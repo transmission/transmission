237b4aeec983f5ccba1e77e402b594a136dc155d
Mitchell Livingston <livings124@transmissionbt.com>
2006-07-15 20:39:59 +0000

    Use .png image.

--- macosx/FilterBarView.m
+++ macosx/FilterBarView.m
@@ -31,7 +31,7 @@
 	if ((self = [super initWithFrame: frameRect]))
     {
 		fBackgroundColor = [[NSColor colorWithPatternImage:
-                            [NSImage imageNamed: @"FilterBarBackground.tiff"]] retain];
+                            [NSImage imageNamed: @"FilterBarBackground.png"]] retain];
 	}
 	return self;
 }
