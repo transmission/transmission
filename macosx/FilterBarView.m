c34434d7bd40a25f63fb479db582b66eab507685
Mitchell Livingston <livings124@transmissionbt.com>
2006-07-15 20:33:53 +0000

    Filter bar background is now from an image.

--- macosx/FilterBarView.m
+++ macosx/FilterBarView.m
@@ -30,7 +30,8 @@
 {
 	if ((self = [super initWithFrame: frameRect]))
     {
-		fBackgroundColor = [[NSColor colorWithCalibratedRed: 0.7137 green: 0.7529 blue: 0.8118 alpha: 1.0] retain];
+		fBackgroundColor = [[NSColor colorWithPatternImage:
+                            [NSImage imageNamed: @"FilterBarBackground.tiff"]] retain];
 	}
 	return self;
 }
@@ -44,6 +45,7 @@
 - (void) drawRect: (NSRect) rect
 {
     [fBackgroundColor set];
+    [[NSGraphicsContext currentContext] setPatternPhase: [self frame].origin];
     NSRectFill([self bounds]);
 }
 
