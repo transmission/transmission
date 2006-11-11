406600e49143866d24c1b7583a7611b3ffda8259
Mitchell Livingston <livings124@transmissionbt.com>
2006-11-11 20:00:50 +0000

    If the folder doesn't exist, still show a folder icon for the download/incomplete folder prefs.

--- macosx/ExpandedPathToIconTransformer.m
+++ macosx/ExpandedPathToIconTransformer.m
@@ -18,7 +18,14 @@
     if (!value)
         return nil;
     
-    NSImage * icon = [[NSWorkspace sharedWorkspace] iconForFile: [value stringByExpandingTildeInPath]];
+    NSString * path = [value stringByExpandingTildeInPath];
+    NSImage * icon;
+    //show a folder icon if the folder doesn't exist
+    if (![[NSFileManager defaultManager] fileExistsAtPath: path] && [[path pathExtension] isEqualToString: @""])
+        icon = [[NSWorkspace sharedWorkspace] iconForFileType: NSFileTypeForHFSTypeCode('fldr')];
+    else
+        icon = [[NSWorkspace sharedWorkspace] iconForFile: [value stringByExpandingTildeInPath]];
+    
     [icon setScalesWhenResized: YES];
     [icon setSize: NSMakeSize(16.0, 16.0)];
     
