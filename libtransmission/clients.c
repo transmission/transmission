f79b141ab18b91eda006827cb841557a9aee7672
Mitchell Livingston <livings124@transmissionbt.com>
2006-12-16 20:05:20 +0000

    formatting

--- libtransmission/clients.c
+++ libtransmission/clients.c
@@ -160,7 +160,7 @@ char * tr_clientForId( uint8_t * id )
 
     if( !ret )
     {
-        if (id[0] != 0)
+        if( id[0] != 0 )
         {
             asprintf( &ret, "unknown client (%c%c%c%c%c%c%c%c)",
                   id[0], id[1], id[2], id[3], id[4], id[5], id[6], id[7] );
