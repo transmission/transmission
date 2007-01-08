e5b24d929ef96617ce8045a1931f97c82c5b9f09
Mitchell Livingston <livings124@transmissionbt.com>
2007-01-08 04:41:18 +0000

    add bittyrant to client id's

--- libtransmission/clients.c
+++ libtransmission/clients.c
@@ -207,6 +207,11 @@ char * tr_clientForId( uint8_t * id )
         asprintf( &ret, "MLDonkey %c%c%c%c%c",
                   id[3], id[4], id[5], id[6], id[7] );
     }
+    else if( !memcmp( id, "AZ", 2 ) && !memcmp( &id[6], "BT", 2 ) )
+    {
+        asprintf( &ret, "BitTyrant (%c.%c.%c.%c)",
+                    id[2], id[3], id[4], id[5] );
+    }
     else if( !memcmp( id, "-FG", 3 ) )
     {
         asprintf( &ret, "FlashGet %d.%c%c",
