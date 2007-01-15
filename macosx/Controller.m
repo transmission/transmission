dc68861f60e1a6d8fbe3ed7f5e7662cc0d8c9ffb
Mitchell Livingston <livings124@transmissionbt.com>
2007-01-15 18:53:35 +0000

    one more localization

--- macosx/Controller.m
+++ macosx/Controller.m
@@ -706,7 +706,7 @@ static void sleepCallBack(void * controller, io_service_t y, natural_t messageTy
 
     NSOpenPanel * panel = [NSOpenPanel openPanel];
 
-    [panel setPrompt: @"Select"];
+    [panel setPrompt: NSLocalizedString(@"Select", "Open torrent -> prompt")];
     [panel setAllowsMultipleSelection: NO];
     [panel setCanChooseFiles: NO];
     [panel setCanChooseDirectories: YES];
