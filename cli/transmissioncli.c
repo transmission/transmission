1da1a9902380806b0e6518ad7daa9529686de2c7
Mitchell Livingston <livings124@transmissionbt.com>
2007-04-21 12:37:12 +0000

    get the cli compiling again

--- cli/transmissioncli.c
+++ cli/transmissioncli.c
@@ -70,8 +70,6 @@ char * getStringRatio( float ratio )
 
     if( ratio == TR_RATIO_NA )
         return "n/a";
-    if( ratio == TR_RATIO_INF )
-        return "inf";
     snprintf( string, sizeof string, "%.3f", ratio );
     return string;
 }
