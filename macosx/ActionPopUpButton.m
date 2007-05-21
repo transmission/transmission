2f3b43ff8e3157889013c07402b71fbbeddc19c4
Mitchell Livingston <livings124@transmissionbt.com>
2007-05-21 04:38:05 +0000

    adjust the action button's image placement

--- macosx/ActionPopUpButton.m
+++ macosx/ActionPopUpButton.m
@@ -41,8 +41,7 @@
 - (void) drawRect: (NSRect) rect
 {
     NSImage * image = [[self cell] isHighlighted] ? fImagePressed : fImage;
-	[image drawInRect: rect fromRect: NSMakeRect(0.0, 0.0, [image size].width, [image size].height)
-            operation: NSCompositeSourceOver fraction: 1.0];
+	[image drawInRect: [self bounds] fromRect: NSZeroRect operation: NSCompositeSourceOver fraction: 1.0];
 }
 
 @end
