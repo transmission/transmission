6f09fbe18d886d4cc737830d084a5f6852400f69
Charles Kerr <charles@transmissionbt.com>
2007-07-13 00:15:45 +0000

    add #include files for stat()

--- libtransmission/fdlimit.c
+++ libtransmission/fdlimit.c
@@ -23,6 +23,8 @@
  *****************************************************************************/
 
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include "transmission.h"
 
