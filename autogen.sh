2fcce9ca70e47e458968bfb0d6b8393482957b98
Josh Elsasser <josh@elsasser.org>
2007-08-16 15:48:07 +0000

    Fix autogen.sh on osx (and maybe elsewhere too).

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 LIBTOOLIZE=libtoolize
-if [ "$(uname)" == "Darwin" ] ; then
+if ! libtoolize --help >/dev/null 2>&1 && glibtoolize --help >/dev/null 2>&1
+then
   LIBTOOLIZE=glibtoolize
 fi
+export LIBTOOLIZE
 
 autoreconf -fiv -I config -I m4
