36faadd42b8159f887547631bc8c982fb774bb4c
Mitchell Livingston <livings124@transmissionbt.com>
2007-10-24 02:38:04 +0000

    ready for 0.90

--- mk/version.sh
+++ mk/version.sh
@@ -5,8 +5,8 @@
 # convention: -TR MAJOR MINOR MAINT BETA - (each a single char)
 # BETA: "Z" for beta, "0" for stable 
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR082Z-"
-USERAGENT_PREFIX="0.82+"
+PEERID_PREFIX="-TR0900-"
+USERAGENT_PREFIX="0.90"
 
 
 SVN_REVISION=`find ./ -name "*\.[ch]" -o -name "*\.cpp" -o -name "*\.po" | \
