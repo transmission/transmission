6776413a197a0301e65c1986cff0e1d8b9208fb4
Mitchell Livingston <livings124@transmissionbt.com>
2007-12-05 03:48:25 +0000

    merge 0.95 commit to trunk

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR094Z-"
-USERAGENT_PREFIX="0.94+"
+PEERID_PREFIX="-TR095Z-"
+USERAGENT_PREFIX="0.95+"
 
 
 SVN_REVISION=`find ./ -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
