2a98bf39d1fbaa47bc1dc922a5dbd749f064f7f3
Mitchell Livingston <livings124@transmissionbt.com>
2007-12-11 02:48:12 +0000

    trunk: get ready for .96

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR095Z-"
-USERAGENT_PREFIX="0.95+"
+PEERID_PREFIX="-TR096Z-"
+USERAGENT_PREFIX="0.96+"
 
 
 SVN_REVISION=`find ./ -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
