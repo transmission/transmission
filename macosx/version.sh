4ee8ea8e6b1f16002f1e91976ae41c32209bff19
Charles Kerr <charles@transmissionbt.com>
2008-01-10 20:41:25 +0000

    get ready for 1.01 Test Release 1

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR1000-"
-USERAGENT_PREFIX="1.00"
+PEERID_PREFIX="-TR101X-"
+USERAGENT_PREFIX="1.01 Test Release 1"
 
 
 SVN_REVISION=`find ./ -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
