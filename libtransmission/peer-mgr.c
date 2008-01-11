90f5c613279b13268e1c4e38df75e4eb75fc6f89
Charles Kerr <charles@transmissionbt.com>
2008-01-11 19:44:45 +0000

    tweak the conncetions a little bit

--- libtransmission/peer-mgr.c
+++ libtransmission/peer-mgr.c
@@ -87,14 +87,14 @@ enum
     MAX_UPLOAD_IDLE_SECS = (60 * 10),
 
     /* how frequently to decide which peers live and die */
-    RECONNECT_PERIOD_MSEC = (4 * 1000),
+    RECONNECT_PERIOD_MSEC = (2 * 1000),
 
     /* max # of peers to ask fer per torrent per reconnect pulse */
-    MAX_RECONNECTIONS_PER_PULSE = 16,
+    MAX_RECONNECTIONS_PER_PULSE = 8,
 
     /* max number of peers to ask for per second overall.
      * this throttle is to avoid overloading the router */
-    MAX_CONNECTIONS_PER_SECOND = 64,
+    MAX_CONNECTIONS_PER_SECOND = 48,
 
     /* corresponds to ut_pex's added.f flags */
     ADDED_F_ENCRYPTION_FLAG = 1,
