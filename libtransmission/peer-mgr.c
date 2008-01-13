b6df35748cc558db497d7304ef2adfbdc1fca7f5
Charles Kerr <charles@transmissionbt.com>
2008-01-13 21:04:46 +0000

    throttle the number of new connections per second down even further from 48 to 32 because of the continuing "kills my router" reports

--- libtransmission/peer-mgr.c
+++ libtransmission/peer-mgr.c
@@ -94,7 +94,7 @@ enum
 
     /* max number of peers to ask for per second overall.
      * this throttle is to avoid overloading the router */
-    MAX_CONNECTIONS_PER_SECOND = 48,
+    MAX_CONNECTIONS_PER_SECOND = 32,
 
     /* corresponds to ut_pex's added.f flags */
     ADDED_F_ENCRYPTION_FLAG = 1,
