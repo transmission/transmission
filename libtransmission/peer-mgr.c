52c3243b4bb426c658e42b0bd356b254623c17cf
Charles Kerr <charles@transmissionbt.com>
2008-01-17 03:43:08 +0000

    more test code to address "router death".  Based on maxintech's feedback in the forums, try making the peer manager's reconnect intervals and volume more like 1.00

--- libtransmission/peer-mgr.c
+++ libtransmission/peer-mgr.c
@@ -87,14 +87,14 @@ enum
     MAX_UPLOAD_IDLE_SECS = (60 * 10),
 
     /* how frequently to decide which peers live and die */
-    RECONNECT_PERIOD_MSEC = (3 * 1000),
+    RECONNECT_PERIOD_MSEC = (1000),
 
     /* max # of peers to ask fer per torrent per reconnect pulse */
-    MAX_RECONNECTIONS_PER_PULSE = 4,
+    MAX_RECONNECTIONS_PER_PULSE = 1,
 
     /* max number of peers to ask for per second overall.
      * this throttle is to avoid overloading the router */
-    MAX_CONNECTIONS_PER_SECOND = 16,
+    MAX_CONNECTIONS_PER_SECOND = 12,
 
     /* corresponds to ut_pex's added.f flags */
     ADDED_F_ENCRYPTION_FLAG = 1,
