d63d4e5a46a4da36d9764fdf46bb9df3982912f0
Mitchell Livingston <livings124@transmissionbt.com>
2008-01-22 20:48:46 +0000

    updates for 1.02

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR1010-"
-USERAGENT_PREFIX="1.01"
+PEERID_PREFIX="-TR1020-"
+USERAGENT_PREFIX="1.02"
 
 
 SVN_REVISION=`find ./ -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
