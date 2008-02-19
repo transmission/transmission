d4eb3c018ee16dfc08cd72c69e96ea463605575d
Mitchell Livingston <livings124@transmissionbt.com>
2008-02-19 05:09:52 +0000

    remove cache file when removing a transfer

--- macosx/Torrent.m
+++ macosx/Torrent.m
@@ -202,8 +202,7 @@ void completenessChangeCallback(tr_torrent * torrent, cp_status_t status, void *
     //allow the file to be index by Time Machine
     [self setTimeMachineExclude: NO forPath: [[self downloadFolder] stringByAppendingPathComponent: [self name]]];
     
-    tr_torrentRemoveSaved(fHandle);
-    tr_torrentClose(fHandle);
+    tr_torrentDelete(fHandle);
 }
 
 - (void) changeIncompleteDownloadFolder: (NSString *) folder
