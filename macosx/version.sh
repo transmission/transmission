14edc2cfe85ccd63d3ca638a888a3dbfd88e27f3
Mitchell Livingston <livings124@transmissionbt.com>
2008-02-27 01:17:21 +0000

    update trunk to 1.06+

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR105Z-"
-USERAGENT_PREFIX="1.05+"
+PEERID_PREFIX="-TR106Z-"
+USERAGENT_PREFIX="1.06+"
 
 
 SVN_REVISION=`find ./ -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
