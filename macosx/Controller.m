457585252a0acdb91ac61b7eea1e52a85049943b
Mitchell Livingston <livings124@transmissionbt.com>
2008-06-08 22:59:27 +0000

    #990 - add hidden pref for DSCP support

--- macosx/Controller.m
+++ macosx/Controller.m
@@ -216,7 +216,7 @@ static void sleepCallback(void * controller, io_service_t y, natural_t messageTy
                                 [fDefaults integerForKey: @"MessageLevel"],
                                 YES,
                                 [fDefaults boolForKey: @"Blocklist"],
-                                TR_DEFAULT_PEER_SOCKET_TOS,
+                                [fDefaults integerForKey: @"PeerSocketTOS"], /* hidden - default should be TR_DEFAULT_PEER_SOCKET_TOS */
                                 [fDefaults boolForKey: @"RPC"],
                                 [fDefaults integerForKey: @"RPCPort"],
                                 NULL, /* reset in prefs */
