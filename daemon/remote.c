d2e1049c78114aec25d4360474f05f6da7ca700d
Charles Kerr <charles@transmissionbt.com>
2008-07-07 03:38:22 +0000

    (daemon) add file-listing capabilities to transmission-remote

--- daemon/remote.c
+++ daemon/remote.c
@@ -31,7 +31,7 @@
 #define DEFAULT_HOST "localhost"
 #define DEFAULT_PORT TR_DEFAULT_RPC_PORT
 
-enum { TAG_LIST };
+enum { TAG_LIST, TAG_DETAILS, TAG_FILES, TAG_PEERS };
 
 static void
 showUsage( void )
@@ -45,30 +45,31 @@ showUsage( void )
             "\n"
             "Options:\n"
             "  -a --add <torrent>        Add a torrent\n"
-            "  -c --encryption required  Require encryption for all peers\n"
-            "  -c --encryption preferred Prefer peers to use encryption\n"
-            "  -c --encryption tolerated Prefer peers to use plaintext\n"
             "  -d --download-limit <int> Max download rate in KiB/s\n"
             "  -D --download-unlimited   No download rate limit\n"
-            "  -e --enable-pex           Enable peer exchange\n"
-            "  -E --disable-pex          Disable peer exchange\n"
-            "  -f --folder <path>        Folder to set for new torrents\n"
+            "  -e --encryption required  Require encryption for all peers\n"
+            "  -e --encryption preferred Prefer peers to use encryption\n"
+            "  -e --encryption tolerated Prefer peers to use plaintext\n"
+            "  -f --files <id>           Get a file list for the specified torrent\n"
             "  -g --debug                Print debugging information\n"
             "  -h --help                 Display this message and exit\n"
             "  -l --list                 Long list of all torrent and status\n"
             "  -m --port-mapping         Automatic port mapping via NAT-PMP or UPnP\n"
             "  -M --no-port-mapping      Disable automatic port mapping\n"
-            "  -p --port <int>           Port to listen for incoming peers\n"
-            "  -r --remove <int>         Remove the torrent with the given ID\n"
+            "  -p --port <id>            Port to listen for incoming peers\n"
+            "  -r --remove <id>          Remove the torrent with the given ID\n"
             "  -r --remove all           Remove all torrents\n"
-            "  -s --start <int>          Start the torrent with the given ID\n"
+            "  -s --start <id>           Start the torrent with the given ID\n"
             "  -s --start all            Start all stopped torrents\n"
-            "  -S --stop <int>           Stop the torrent with the given ID\n"
+            "  -S --stop <id>            Stop the torrent with the given ID\n"
             "  -S --stop all             Stop all running torrents\n"
             "  -t --auth <user>:<pass>   Username and password for authentication\n"
             "  -u --upload-limit <int>   Max upload rate in KiB/s\n"
             "  -U --upload-unlimited     No upload rate limit\n"
-            "  -v --verify <id>          Verify the torrent's local data\n" );
+            "  -v --verify <id>          Verify the torrent's local data\n"
+            "  -w --download-dir <path>  Folder to set for new torrents\n"
+            "  -x --enable-pex           Enable peer exchange\n"
+            "  -X --disable-pex          Disable peer exchange\n" );
     exit( 0 );
 }
 
@@ -116,17 +117,15 @@ static void
 readargs( int argc, char ** argv )
 {
     int opt;
-    char optstr[] = "a:c:d:DeEf:ghlmMp:r:s:S:t:u:Uv:";
+    char optstr[] = "a:d:De:f:ghlmMp:r:s:S:t:u:Uv:w:xX";
     
     const struct option longopts[] =
     {
         { "add",                required_argument, NULL, 'a' },
-        { "encryption",         required_argument, NULL, 'c' },
         { "download-limit",     required_argument, NULL, 'd' },
         { "download-unlimited", no_argument,       NULL, 'D' },
-        { "enable-pex",         no_argument,       NULL, 'e' },
-        { "disable-pex",        no_argument,       NULL, 'E' },
-        { "folder",             required_argument, NULL, 'f' },
+        { "encryption",         required_argument, NULL, 'e' },
+        { "files",              required_argument, NULL, 'f' },
         { "debug",              no_argument,       NULL, 'g' },
         { "help",               no_argument,       NULL, 'h' },
         { "list",               no_argument,       NULL, 'l' },
@@ -140,6 +139,9 @@ readargs( int argc, char ** argv )
         { "upload-limit",       required_argument, NULL, 'u' },
         { "upload-unlimited",   no_argument,       NULL, 'U' },
         { "verify",             required_argument, NULL, 'v' },
+        { "download-dir",       required_argument, NULL, 'w' },
+        { "enable-pex",         no_argument,       NULL, 'x' },
+        { "disable-pex",        no_argument,       NULL, 'X' },
         { NULL, 0, NULL, 0 }
     };
 
@@ -168,8 +170,16 @@ readargs( int argc, char ** argv )
                       tr_bencDictAddStr( args, "metainfo", ((tmp=getEncodedMetainfo(optarg))) );
                       tr_free( tmp );
                       break;
-            case 'c': tr_bencDictAddStr( &top, "method", "session-set" );
+            case 'e': tr_bencDictAddStr( &top, "method", "session-set" );
                       tr_bencDictAddStr( args, "encryption", optarg );
+                      break;
+            case 'f': tr_bencDictAddStr( &top, "method", "torrent-get" );
+                      tr_bencDictAddInt( &top, "tag", TAG_FILES );
+                      tr_rpc_parse_list_str( tr_bencDictAdd( args, "ids" ), optarg, strlen(optarg) );
+                      fields = TR_RPC_TORRENT_FIELD_ID
+                             | TR_RPC_TORRENT_FIELD_FILES
+                             | TR_RPC_TORRENT_FIELD_PRIORITIES;
+                      tr_bencDictAddInt( args, "fields", fields );
                       break;
             case 'd': tr_bencDictAddStr( &top, "method", "session-set" );
                       tr_bencDictAddInt( args, "speed-limit-down", numarg( optarg ) );
@@ -184,15 +194,6 @@ readargs( int argc, char ** argv )
                       break;
             case 'U': tr_bencDictAddStr( &top, "method", "session-set" );
                       tr_bencDictAddInt( args, "speed-limit-up-enabled", 0 );
-                      break;
-            case 'e': tr_bencDictAddStr( &top, "method", "session-set" );
-                      tr_bencDictAddInt( args, "pex-allowed", 1 );
-                      break;
-            case 'E': tr_bencDictAddStr( &top, "method", "session-set" );
-                      tr_bencDictAddInt( args, "pex-allowed", 0 );
-                      break;
-            case 'f': tr_bencDictAddStr( &top, "method", "session-set" );
-                      tr_bencDictAddStr( args, "download-dir", absolutify(buf,sizeof(buf),optarg) );
                       break;
             case 'l': tr_bencDictAddStr( &top, "method", "torrent-get" );
                       tr_bencDictAddInt( &top, "tag", TAG_LIST );
@@ -225,6 +226,15 @@ readargs( int argc, char ** argv )
             case 'v': tr_bencDictAddStr( &top, "method", "torrent-verify" );
                       if( strcmp( optarg, "all" ) )
                           tr_rpc_parse_list_str( tr_bencDictAdd( args, "ids" ), optarg, strlen(optarg) );
+                      break;
+            case 'w': tr_bencDictAddStr( &top, "method", "session-set" );
+                      tr_bencDictAddStr( args, "download-dir", absolutify(buf,sizeof(buf),optarg) );
+                      break;
+            case 'x': tr_bencDictAddStr( &top, "method", "session-set" );
+                      tr_bencDictAddInt( args, "pex-allowed", 1 );
+                      break;
+            case 'X': tr_bencDictAddStr( &top, "method", "session-set" );
+                      tr_bencDictAddInt( args, "pex-allowed", 0 );
                       break;
             default:
                       showUsage( );
@@ -283,6 +293,33 @@ etaToString( char * buf, size_t buflen, int64_t eta )
     else                         snprintf( buf, buflen, "%"PRId64" days", eta/(60*60*24) );
 }
 
+#define KILOBYTE_FACTOR 1024.0
+#define MEGABYTE_FACTOR (1024.0 * 1024.0)
+#define GIGABYTE_FACTOR (1024.0 * 1024.0 * 1024.0)
+
+static char*
+strlsize( char * buf, int64_t size, size_t buflen )
+{
+    if( !size )
+        tr_strlcpy( buf, "None", buflen );
+    else if( size < (int64_t)KILOBYTE_FACTOR )
+        snprintf( buf, buflen, "%'"PRId64" bytes", (int64_t)size );
+    else {
+        double displayed_size;
+        if (size < (int64_t)MEGABYTE_FACTOR) {
+            displayed_size = (double) size / KILOBYTE_FACTOR;
+            snprintf( buf, buflen, "%'.1f KB", displayed_size );
+        } else if (size < (int64_t)GIGABYTE_FACTOR) {
+            displayed_size = (double) size / MEGABYTE_FACTOR;
+            snprintf( buf, buflen, "%'.1f MB", displayed_size );
+        } else {
+            displayed_size = (double) size / GIGABYTE_FACTOR;
+            snprintf( buf, buflen, "%'.1f GB", displayed_size );
+        }
+    }
+    return buf;
+}
+
 static const char*
 torrentStatusToString( int i )
 {
@@ -294,6 +331,100 @@ torrentStatusToString( int i )
         case TR_STATUS_SEED:       return "Seeding";
         case TR_STATUS_STOPPED:    return "Stopped";
         default:                   return "Error";
+    }
+}
+
+static void
+printFileList( tr_benc * top )
+{
+    tr_benc *args, *torrents;
+
+    if( ( tr_bencDictFindDict( top, "arguments", &args ) ) &&
+        ( tr_bencDictFindList( args, "torrents", &torrents ) ) )
+    {
+        int i, in;
+        for( i=0, in=tr_bencListSize( torrents ); i<in; ++i )
+        {
+            tr_benc * d = tr_bencListChild( torrents, i );
+            tr_benc *files, *priorities, *wanteds;
+            const char * name;
+            if( tr_bencDictFindStr( d, "name", &name ) &&
+                tr_bencDictFindList( d, "files", &files ) &&
+                tr_bencDictFindList( d, "priorities", &priorities ) &&
+                tr_bencDictFindList( d, "wanted", &wanteds ) )
+            {
+                int j=0, jn=tr_bencListSize(files);
+                printf( "%s (%d files):\n", name, jn );
+                printf("%3s  %8s %3s %9s  %s\n", "#", "Priority", "Get", "Size", "Name" );
+                for( j=0, jn=tr_bencListSize( files ); j<jn; ++j )
+                {
+                    int64_t length;
+                    int64_t priority;
+                    int64_t wanted;
+                    const char * filename;
+                    tr_benc * file = tr_bencListChild( files, j );
+                    if( tr_bencDictFindInt( file, "length", &length ) &&
+                        tr_bencDictFindStr( file, "name", &filename ) &&
+                        tr_bencGetInt( tr_bencListChild( priorities, j ), &priority ) &&
+                        tr_bencGetInt( tr_bencListChild( wanteds, j ), &wanted ) )
+                    {
+                        char sizestr[64];
+                        strlsize( sizestr, length, sizeof( sizestr ) );
+                        const char * pristr;
+                        switch( priority ) {
+                            case TR_PRI_LOW:    pristr = "Low"; break;
+                            case TR_PRI_HIGH:   pristr = "High"; break;
+                            default:            pristr = "Normal"; break;
+                        }
+                        printf( "%3d: %-8s %-3s %9s  %s\n", (j+1), pristr, (wanted?"Yes":"No"), sizestr, filename );
+                    }
+                }
+            }
+        }
+    }
+}
+
+static void
+printTorrentList( tr_benc * top )
+{
+    tr_benc *args, *list;
+
+    if( ( tr_bencDictFindDict( top, "arguments", &args ) ) &&
+        ( tr_bencDictFindList( args, "torrents", &list ) ) )
+    {
+        int i, n;
+        printf( "%-3s  %-4s  %-8s  %-5s  %-5s  %-5s  %-11s  %s\n",
+                "ID", "Done", "ETA", "Up", "Down", "Ratio", "Status", "Name" );
+        for( i=0, n=tr_bencListSize( list ); i<n; ++i )
+        {
+            int64_t id, eta, status, up, down, sizeWhenDone, leftUntilDone;
+            const char *name;
+            tr_benc * d = tr_bencListChild( list, i );
+            if(    tr_bencDictFindInt( d, "eta", &eta )
+                && tr_bencDictFindInt( d, "id", &id )
+                && tr_bencDictFindInt( d, "leftUntilDone", &leftUntilDone )
+                && tr_bencDictFindStr( d, "name", &name )
+                && tr_bencDictFindInt( d, "rateDownload", &down )
+                && tr_bencDictFindInt( d, "rateUpload", &up )
+                && tr_bencDictFindInt( d, "sizeWhenDone", &sizeWhenDone )
+                && tr_bencDictFindInt( d, "status", &status ) )
+            {
+                char etaStr[16];
+                if( leftUntilDone )
+                    etaToString( etaStr, sizeof( etaStr ), eta );
+                else
+                    snprintf( etaStr, sizeof( etaStr ), "Done" );
+                printf( "%3d  %3d%%  %-8s  %5.1f  %5.1f  %5.1f  %-11s  %s\n",
+                        (int)id,
+                        (int)(100.0*(sizeWhenDone-leftUntilDone)/sizeWhenDone),
+                        etaStr,
+                        up / 1024.0,
+                        down / 1024.0,
+                        (double)(sizeWhenDone-leftUntilDone)/sizeWhenDone,
+                        torrentStatusToString( status ),
+                        name );
+            }
+        }
     }
 }
 
@@ -311,52 +442,16 @@ processResponse( const char * host, int port,
        tr_nerr( MY_NAME, "Unable to parse response \"%*.*s\"", (int)len, (int)len, (char*)response );
     else
     {
-        tr_benc *args, *list;
         int64_t tag = -1;
         const char * str;
         tr_bencDictFindInt( &top, "tag", &tag );
 
         if( tr_bencDictFindStr( &top, "result", &str ) )
             printf( "%s:%d responded: \"%s\"\n", host, port, str );
-
-        if( ( tag == TAG_LIST ) &&
-            ( tr_bencDictFindDict( &top, "arguments", &args ) ) &&
-            ( tr_bencDictFindList( args, "torrents", &list ) ) )
-        {
-            int i, n;
-            printf( "%-3s  %-4s  %-8s  %-5s  %-5s  %-5s  %-11s  %s\n",
-                    "ID", "Done", "ETA", "Up", "Down", "Ratio", "Status", "Name" );
-            for( i=0, n=tr_bencListSize( list ); i<n; ++i )
-            {
-                int64_t id, eta, status, up, down, sizeWhenDone, leftUntilDone;
-                const char *name;
-                tr_benc * d = tr_bencListChild( list, i );
-                if(    tr_bencDictFindInt( d, "eta", &eta )
-                    && tr_bencDictFindInt( d, "id", &id )
-                    && tr_bencDictFindInt( d, "leftUntilDone", &leftUntilDone )
-                    && tr_bencDictFindStr( d, "name", &name )
-                    && tr_bencDictFindInt( d, "rateDownload", &down )
-                    && tr_bencDictFindInt( d, "rateUpload", &up )
-                    && tr_bencDictFindInt( d, "sizeWhenDone", &sizeWhenDone )
-                    && tr_bencDictFindInt( d, "status", &status ) )
-                {
-                    char etaStr[16];
-                    if( leftUntilDone )
-                        etaToString( etaStr, sizeof( etaStr ), eta );
-                    else
-                        snprintf( etaStr, sizeof( etaStr ), "Done" );
-                    printf( "%3d  %3d%%  %-8s  %5.1f  %5.1f  %5.1f  %-11s  %s\n",
-                            (int)id,
-                            (int)(100.0*(sizeWhenDone-leftUntilDone)/sizeWhenDone),
-                            etaStr,
-                            up / 1024.0,
-                            down / 1024.0,
-                            (double)(sizeWhenDone-leftUntilDone)/sizeWhenDone,
-                            torrentStatusToString( status ),
-                            name );
-                }
-            }
-        }
+        if( tag == TAG_FILES )
+            printFileList( &top );
+        if( tag == TAG_LIST )
+            printTorrentList( &top );
 
         tr_bencFree( &top );
     }
