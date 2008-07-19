da14f40e5ef6fa2a95f4a6194d84056834450316
Mitchell Livingston <livings124@transmissionbt.com>
2008-07-19 14:49:53 +0000

    mark the web ui as beta

--- macosx/PortChecker.m
+++ macosx/PortChecker.m
@@ -126,7 +126,7 @@
             [self callBackWithStatus: PORT_STATUS_CLOSED];
         else
         {
-            NSLog(@"Unable to get port status: invalid response");
+            NSLog([NSString stringWithFormat: @"Unable to get port status: invalid response (%@)", probeString]);
             [self callBackWithStatus: PORT_STATUS_ERROR];
         }
         [probeString release];
