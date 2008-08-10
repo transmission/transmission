81d4e5887d8364dd0403dabd45f5fe79b2af98fa
Mitchell Livingston <livings124@transmissionbt.com>
2008-08-10 17:59:16 +0000

    show "remaining time unknown" instead of "data not fully available" (sigh...)

--- macosx/Torrent.m
+++ macosx/Torrent.m
@@ -1927,7 +1927,6 @@ void completenessChangeCallback(tr_torrent * torrent, cp_status_t status, void *
     switch (eta)
     {
         case TR_ETA_NOT_AVAIL:
-            return NSLocalizedString(@"data not fully available", "Torrent -> eta string");
         case TR_ETA_UNKNOWN:
             return NSLocalizedString(@"remaining time unknown", "Torrent -> eta string");
         default:
