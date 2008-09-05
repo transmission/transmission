6d24dcb5eee8ef4b8d70443f613588dfa37ede89
Mukund Sivaraman <muks@mukund.org>
2008-09-05 14:32:20 +0000

    (win32) Don't use dllimport/dllexport (part 3)

--- third-party/miniupnp/declspec.h
+++ third-party/miniupnp/declspec.h
@@ -1,6 +1,7 @@
 #ifndef __DECLSPEC_H__
 #define __DECLSPEC_H__
 
+/*
 #if defined(WIN32) && !defined(STATICLIB)
 	#ifdef MINIUPNP_EXPORTS
 		#define LIBSPEC __declspec(dllexport)
@@ -8,8 +9,11 @@
 		#define LIBSPEC __declspec(dllimport)
 	#endif
 #else
+*/
 	#define LIBSPEC
+/*
 #endif
+*/
 
 #endif
 
