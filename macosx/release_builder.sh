e854dfebd9817fe80a6f81f1e33e2c5f4e48a2bc
Mitchell Livingston <livings124@transmissionbt.com>
2008-11-01 17:08:49 +0000

    remove some pretty pointless steps in the release builder

--- macosx/release_builder.sh
+++ macosx/release_builder.sh
@@ -6,17 +6,6 @@ echo CLEANING TRANSMISSION
 xcodebuild -project Transmission.xcodeproj clean
 echo BUILDING TRANSMISSION
 xcodebuild -project Transmission.xcodeproj -target Transmission -configuration Release
-echo DELETING INTERFACE BUILDER DATA
-rm -f macosx/Transmission.app/Contents/Resources/*.nib/info.nib
-rm -f macosx/Transmission.app/Contents/Resources/*.nib/classes.nib
-rm -f macosx/Transmission.app/Contents/Resources/*.lproj/*.nib/info.nib
-rm -f macosx/Transmission.app/Contents/Resources/*.lproj/*.nib/classes.nib
-rm -f macosx/Transmission.app/Contents/Frameworks/*.framework/Versions/[A-Z]/Resources/*.nib/info.nib
-rm -f macosx/Transmission.app/Contents/Frameworks/*.framework/Versions/[A-Z]/Resources/*.nib/classes.nib
-rm -f macosx/Transmission.app/Contents/Frameworks/*.framework/Versions/[A-Z]/Resources/*.lproj/*.nib/info.nib
-rm -f macosx/Transmission.app/Contents/Frameworks/*.framework/Versions/[A-Z]/Resources/*.lproj/*.nib/classes.nib
-echo DELETING FRAMEWORK HEADERS
-rm -rf macosx/Transmission.app/Contents/Frameworks/*.framework/Versions/[A-Z]/Headers/*
 echo CREATING RELEASE DIR
 rm -rf release
 mkdir -p release/Transmission
