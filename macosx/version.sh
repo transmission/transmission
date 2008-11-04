85b0495e52bef614fce0d1cd10e37c4be2e23510
Charles Kerr <charles@transmissionbt.com>
2008-11-04 06:26:54 +0000

    rewind the trunk builds back to -TR134Z- as they should be

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR140X-"
-USERAGENT_PREFIX="1.40b1"
+PEERID_PREFIX="-TR134Z-"
+USERAGENT_PREFIX="1.34+"
 
 SVN_REVISION=`find ./macosx ./libtransmission -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
               xargs grep "\$Id:" | \
