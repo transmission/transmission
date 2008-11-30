ee4f7374f8fc37e49fdc105755ba3c8bd834b0ff
Mitchell Livingston <livings124@transmissionbt.com>
2008-11-30 18:02:18 +0000

    whoops trunk version number

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR1400-"
-USERAGENT_PREFIX="1.40"
+PEERID_PREFIX="-TR140Z-"
+USERAGENT_PREFIX="1.40+"
 
 SVN_REVISION=`find ./libtransmission -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
               xargs grep "\$Id:" | \
