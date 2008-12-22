9cf24ce2de073a8d606c2b82ec52fa95519f04be
Mitchell Livingston <livings124@transmissionbt.com>
2008-12-22 12:36:50 +0000

    (1.4x) mac build compiles

--- macosx/version.sh
+++ macosx/version.sh
@@ -8,7 +8,7 @@
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
 PEERID_PREFIX="-TR141X-"
-USERAGENT_PREFIX="1.41b4"
+USERAGENT_PREFIX="1.41b5"
 
 SVN_REVISION=`find ./libtransmission -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
               xargs grep "\$Id:" | \
