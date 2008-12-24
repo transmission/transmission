1d51d28198ac88319c033cba68d42c6d35786897
Mitchell Livingston <livings124@transmissionbt.com>
2008-12-24 17:54:30 +0000

    #1626 update group rules sheet to use NSPredicateEditor - main commit in r7493

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR141Z-"
-USERAGENT_PREFIX="1.41+"
+PEERID_PREFIX="-TR142Z-"
+USERAGENT_PREFIX="1.42+"
 
 SVN_REVISION=`find -E ./libtransmission ./macosx                     \
                   -regex ".*\.([chmp]|cpp|po|sh)"                    \
