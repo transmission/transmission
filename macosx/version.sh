2408423c3356729583755f66ef056566efeeb851
Mitchell Livingston <livings124@transmissionbt.com>
2008-12-24 17:41:10 +0000

    set missing properties

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR1410-"
-USERAGENT_PREFIX="1.41"
+PEERID_PREFIX="-TR1420-"
+USERAGENT_PREFIX="1.42"
 
 SVN_REVISION=`find ./libtransmission -name "*\.[chmp]" -o -name "*\.cpp" -o -name "*\.po" -o -name "*\.sh" | \
               xargs grep "\$Id:" | \
