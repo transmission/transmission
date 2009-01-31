44201123211cd9d16a0b3040e4c9dd3370ff8821
Charles Kerr <charles@transmissionbt.com>
2009-01-31 14:42:01 +0000

    (trunk libT) #1777: apply turbo's transmissio-remote.2.diff patch for formatting the -l function a little better

--- daemon/remote.c
+++ daemon/remote.c
@@ -1077,7 +1077,7 @@ printTorrentList( tr_benc * top )
       && ( tr_bencDictFindList( args, "torrents", &list ) ) )
     {
         int i, n;
-        printf( "%-4s  %-4s  %-8s  %-8s  %-6s  %-6s  %-5s  %-11s  %s\n",
+        printf( "%-4s  %-4s  %8s  %-8s  %6s  %6s  %-5s  %-11s  %s\n",
                 "ID", "Done", "Have", "ETA", "Up", "Down", "Ratio", "Status",
                 "Name" );
         for( i = 0, n = tr_bencListSize( list ); i < n; ++i )
@@ -1109,7 +1109,7 @@ printTorrentList( tr_benc * top )
                 else
                     tr_snprintf( etaStr, sizeof( etaStr ), "Done" );
                 printf(
-                    "%4d  %3d%%  %-8s  %8s  %6.1f  %6.1f  %5s  %-11s  %s\n",
+                    "%4d  %3d%%  %8s  %-8s  %6.1f  %6.1f  %5s  %-11s  %s\n",
                     (int)id,
                     (int)( 100.0 * ( sizeWhenDone - leftUntilDone ) / sizeWhenDone ),
                     haveStr,
