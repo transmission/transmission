a5be0fad051ebee66c0fbb7f3bc1d4c09fc01cd0
Mitchell Livingston <livings124@transmissionbt.com>
2009-02-03 00:51:16 +0000

    add a script to build beta releases

--- macosx/scripts/release_builder.sh
+++ macosx/scripts/release_builder.sh
@@ -1,6 +1,6 @@
 #Transmission OS X Release Builder (Universal)
 #/bin/sh
-cd ../
+cd ../../
 echo STARTING OS X RELEASE BUILDER
 echo CLEANING TRANSMISSION
 xcodebuild -project Transmission.xcodeproj clean
@@ -8,12 +8,12 @@ echo BUILDING TRANSMISSION
 xcodebuild -project Transmission.xcodeproj -target Transmission -configuration Release
 echo CREATING RELEASE DIR
 rm -rf release
-mkdir -p release/Transmission
+mkdir -p release
 echo COPYING TRANSMISSION.APP
-cp -R build/Release/Transmission.app ./release/Transmission/
+cp -R build/Release/Transmission.app ./release/
 echo CREATING DMG
 #create a BZ2 Compressed DMG
-hdiutil create -srcfolder release/Transmission/ -format UDBZ -noanyowners -fs HFS+ release/Transmission.dmg
+hdiutil create -srcfolder release/ -format UDBZ -noanyowners -fs HFS+ release/Transmission.dmg
 echo MAKING THE DMG INTERNET-ENABLED
 hdiutil internet-enable -yes release/Transmission.dmg
 cd macosx
