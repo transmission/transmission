0a41302dba5a91e3620283d4dd3e963940dc0d67
Mitchell Livingston <livings124@transmissionbt.com>
2009-02-23 01:22:51 +0000

    update the release scripts to name the disk image "Transmission"

--- macosx/scripts/release_builder.sh
+++ macosx/scripts/release_builder.sh
@@ -6,14 +6,14 @@ echo CLEANING TRANSMISSION
 xcodebuild -project Transmission.xcodeproj clean
 echo BUILDING TRANSMISSION
 xcodebuild -project Transmission.xcodeproj -target Transmission -configuration Release
-echo CREATING RELEASE DIR
+echo CREATING RELEASE DIRECTORY
 rm -rf release
 mkdir -p release
 echo COPYING TRANSMISSION.APP
-cp -R build/Release/Transmission.app ./release/
+cp -R build/Release/Transmission.app ./release/Transmission/
 echo CREATING DMG
 #create a BZ2 Compressed DMG
-hdiutil create -srcfolder release/ -format UDBZ -noanyowners -fs HFS+ release/Transmission.dmg
+hdiutil create -srcfolder release/Transmission/ -format UDBZ -noanyowners -fs HFS+ release/Transmission.dmg
 echo MAKING THE DMG INTERNET-ENABLED
 hdiutil internet-enable -yes release/Transmission.dmg
 cd macosx
