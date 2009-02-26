03e21fadb27ed92a1a15b4940c9a86367a2f8158
Charles Kerr <charles@transmissionbt.com>
2009-02-26 19:23:56 +0000

    (1.5x) prep for 1.51

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR1500-"
-USERAGENT_PREFIX="1.50"
+PEERID_PREFIX="-TR1510-"
+USERAGENT_PREFIX="1.51"
 
 SVN_REVISION=`find -E ./libtransmission ./macosx                     \
                   -regex ".*\.([chmp]|cpp|po|sh)"                    \
