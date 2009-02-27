703da57b8ac1c3e228b4a98a416e8178b22b1854
Mitchell Livingston <livings124@transmissionbt.com>
2009-02-27 00:42:11 +0000

    (1.5x) prepare for 1.51

--- macosx/scripts/beta_builder.sh
+++ macosx/scripts/beta_builder.sh
@@ -6,14 +6,14 @@ echo CLEANING TRANSMISSION
 xcodebuild -project Transmission.xcodeproj clean
 echo BUILDING TRANSMISSION
 xcodebuild -project Transmission.xcodeproj -target Transmission -configuration Release\ -\ Debug
-echo CREATING BETA DIR
+echo CREATING BETA DIRECTORY
 rm -rf beta
 mkdir -p beta
 echo COPYING TRANSMISSION.APP
 cp -R build/Release\ -\ Debug/Transmission.app ./beta/
 echo CREATING DMG
 #create a BZ2 Compressed DMG
-hdiutil create -srcfolder beta/ -format UDBZ -noanyowners -fs HFS+ beta/Transmission-b.dmg
+hdiutil create -volname Transmission -srcfolder beta/ -format UDBZ -noanyowners -fs HFS+ beta/Transmission-b.dmg
 echo MAKING THE DMG INTERNET-ENABLED
 hdiutil internet-enable -yes beta/Transmission-b.dmg
 cd macosx
