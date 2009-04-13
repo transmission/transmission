a47b5b3498b64c4edabdcdcb5a633ef510aca012
Mitchell Livingston <livings124@transmissionbt.com>
2009-04-13 01:19:40 +0000

    (1.5x) update version to 1.52

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR1510-"
-USERAGENT_PREFIX="1.51"
+PEERID_PREFIX="-TR1520-"
+USERAGENT_PREFIX="1.52"
 
 SVN_REVISION=`find -E ./libtransmission ./macosx                     \
                   -regex ".*\.([chmp]|cpp|po|sh)"                    \
