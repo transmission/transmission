472849d47f77ddec206c9b002552bf75bb5f07e6
Charles Kerr <charles@transmissionbt.com>
2009-06-04 20:28:10 +0000

    (1.5x) update news and bump version to 1.54

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR1530-"
-USERAGENT_PREFIX="1.53"
+PEERID_PREFIX="-TR1540-"
+USERAGENT_PREFIX="1.54"
 
 SVN_REVISION=`find -E ./libtransmission ./macosx                     \
                   -regex ".*\.([chmp]|cpp|po|sh)"                    \
