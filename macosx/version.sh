f3f075cdc174653645473fbb0e7b62b8bfa1a679
Mitchell Livingston <livings124@transmissionbt.com>
2009-06-05 12:13:40 +0000

    update trunk to 1.70+

--- macosx/version.sh
+++ macosx/version.sh
@@ -7,8 +7,8 @@
 #         "Z" for unsupported trunk builds,
 #         "0" for stable, supported releases
 # these should be the only two lines you need to change
-PEERID_PREFIX="-TR1700-"
-USERAGENT_PREFIX="1.70"
+PEERID_PREFIX="-TR170Z-"
+USERAGENT_PREFIX="1.70+"
 
 SVN_REVISION=`find -E ./libtransmission ./macosx                     \
                   -regex ".*\.([chmp]|cpp|po|sh)"                    \
