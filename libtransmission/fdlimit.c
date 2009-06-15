1c7ac5bea081288bbe95197a650954dd4e8ce4f1
Erick Turnquist <jhujhiti@adjectivism.org>
2009-06-15 05:16:36 +0000

    (trunk libT) fix "dereferencing pointer does brea strict-aliasing rules" warning

--- libtransmission/fdlimit.c
+++ libtransmission/fdlimit.c
@@ -611,18 +611,24 @@ tr_fdSocketAccept( int           b,
         /* "The ss_family field of the sockaddr_storage structure will always 
          * align with the family field of any protocol-specific structure." */ 
         if( sock.ss_family == AF_INET ) 
-        { 
-            struct sockaddr_in * sock4 = (struct sockaddr_in *)&sock; 
+        {
+            struct sockaddr_in *si;
+            union { struct sockaddr_storage dummy; struct sockaddr_in si; } s;
+            s.dummy = sock;
+            si = &s.si;
             addr->type = TR_AF_INET; 
-            addr->addr.addr4.s_addr = sock4->sin_addr.s_addr; 
-            *port = sock4->sin_port; 
+            addr->addr.addr4.s_addr = si->sin_addr.s_addr; 
+            *port = si->sin_port; 
         } 
         else 
         { 
-            struct sockaddr_in6 * sock6 = (struct sockaddr_in6 *)&sock; 
+            struct sockaddr_in6 *si;
+            union { struct sockaddr_storage dummy; struct sockaddr_in6 si; } s;
+            s.dummy = sock;
+            si = &s.si;
             addr->type = TR_AF_INET6; 
-            addr->addr.addr6 = sock6->sin6_addr;
-            *port = sock6->sin6_port; 
+            addr->addr.addr6 = si->sin6_addr;
+            *port = si->sin6_port; 
         } 
         ++gFd->socketCount;
     }
