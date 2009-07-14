1df06d12a170a557b3c80f396650a181dc61e880
Mitchell Livingston <livings124@transmissionbt.com>
2009-07-14 00:13:10 +0000

    finish #2273 and probably break non-Mac builds in the process

---
+++ third-party/miniupnp/updateminiupnpcstrings.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+# $Id: updateminiupnpcstrings.sh,v 1.3 2009/07/09 16:13:31 nanard Exp $
+
+FILE=miniupnpcstrings.h
+
+# detecting the OS name and version
+OS_NAME=`uname -s`
+OS_VERSION=`uname -r`
+if [ -f /etc/debian_version ]; then
+	OS_NAME=Debian
+	OS_VERSION=`cat /etc/debian_version`
+fi
+# use lsb_release (Linux Standard Base) when available
+LSB_RELEASE=`which lsb_release`
+if [ 0 -eq $? ]; then
+	OS_NAME=`${LSB_RELEASE} -i -s`
+	OS_VERSION=`${LSB_RELEASE} -r -s`
+	case $OS_NAME in
+		Debian)
+			#OS_VERSION=`${LSB_RELEASE} -c -s`
+			;;
+		Ubuntu)
+			#OS_VERSION=`${LSB_RELEASE} -c -s`
+			;;
+	esac
+fi
+
+echo "Detected OS [$OS_NAME] version [$OS_VERSION]"
+
+EXPR="s/OS_STRING \".*\"/OS_STRING \"${OS_NAME}\/${OS_VERSION}\"/"
+#echo $EXPR
+#echo "Backuping $FILE to $FILE.bak."
+#cp $FILE $FILE.bak
+test -f ${FILE}.in
+echo "setting OS_STRING macro value to ${OS_NAME}/${OS_VERSION} in $FILE."
+cat ${FILE}.in | sed -e "$EXPR" > $FILE
+
