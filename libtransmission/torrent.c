b32e5cc25dec1f3addb04c854ac405e76b90b173
Charles Kerr <charles@transmissionbt.com>
2009-09-27 18:48:00 +0000

    (trunk libT) #2450: --find should not delete original

--- libtransmission/torrent.c
+++ libtransmission/torrent.c
@@ -2379,7 +2379,8 @@ setLocation( void * vdata )
         if( !err )
         {
             /* blow away the leftover subdirectories in the old location */
-            tr_torrentDeleteLocalData( tor, remove );
+            if( do_move )
+                tr_torrentDeleteLocalData( tor, remove );
 
             /* set the new location and reverify */
             tr_torrentSetDownloadDir( tor, location );
