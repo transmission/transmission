da62e4945fc8285b7be51b29f179836236b908e6
Charles Kerr <charles@transmissionbt.com>
2009-10-02 22:10:55 +0000

    (trunk, gtk) #2478: revise the popup dialog text

--- gtk/main.c
+++ gtk/main.c
@@ -549,11 +549,7 @@ appsetup( TrWindow *      wind,
                                                 GTK_MESSAGE_INFO,
                                                 GTK_BUTTONS_NONE,
                                                 "%s",
-            _( "Transmission is a file-sharing program.  When you run a torrent "
-               "in Transmission, its files are made available for copying to "
-               "other computers.  And, of course, any content you share is your "
-               "sole responsibility.\n\n"
-               "You probably knew this, so we won't tell you again." ) );
+            _( "Transmission is a file sharing program.  When you run a torrent, its data will be made available to others by means of upload.  And of course, any content you share is your sole responsibility.\n\nYou probably knew this, so we won't tell you again." ) );
         gtk_dialog_add_button( GTK_DIALOG( w ), GTK_STOCK_QUIT, GTK_RESPONSE_REJECT );
         gtk_dialog_add_button( GTK_DIALOG( w ), _( "I Accept" ), GTK_RESPONSE_ACCEPT );
         gtk_dialog_set_default_response( GTK_DIALOG( w ), GTK_RESPONSE_ACCEPT );
