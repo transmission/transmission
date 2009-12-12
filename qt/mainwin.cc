4d9912fcb14a01a98c2d98e589988b82224330f8
Charles Kerr <charles@transmissionbt.com>
2009-12-12 15:38:14 +0000

    (trunk qt) fix qt build broken by r9719; error reported by Lacrocivious

--- qt/mainwin.cc
+++ qt/mainwin.cc
@@ -26,6 +26,8 @@
 #include <QSystemTrayIcon>
 #include <QUrl>
 
+#include <libtransmission/transmission.h>
+#include <libtransmission/utils.h>
 #include <libtransmission/version.h>
 
 #include "about.h"
