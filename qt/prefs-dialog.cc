5c26afdb52e90c4c51352a903c2bb567f21924ca
Charles Kerr <charles@transmissionbt.com>
2010-01-04 21:00:47 +0000

    happy new year!

--- qt/prefs-dialog.cc
+++ qt/prefs-dialog.cc
@@ -1,5 +1,5 @@
 /*
- * This file Copyright (C) 2009 Mnemosyne LLC
+ * This file Copyright (C) 2009-2010 Mnemosyne LLC
  *
  * This file is licensed by the GPL version 2.  Works owned by the
  * Transmission project are granted a special exemption to clause 2(b)
