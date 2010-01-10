5af45426d42bd7144413ad9475d7ae92ec4866d7
Mitchell Livingston <livings124@transmissionbt.com>
2010-01-10 14:56:04 +0000

    random code optimization: when escaping http, use an if with a lot less comparisons rather than a switch

--- libtransmission/web.c
+++ libtransmission/web.c
@@ -514,32 +514,14 @@ tr_http_escape( struct evbuffer  * out,
         len = strlen( str );
 
     for( i = 0; i < len; i++ ) {
-        switch( str[i] ) {
-        case ',': case '-': case '.':
-        case '0': case '1': case '2': case '3': case '4':
-        case '5': case '6': case '7': case '8': case '9':
-        case 'a': case 'b': case 'c': case 'd': case 'e':
-        case 'f': case 'g': case 'h': case 'i': case 'j':
-        case 'k': case 'l': case 'm': case 'n': case 'o':
-        case 'p': case 'q': case 'r': case 's': case 't':
-        case 'u': case 'v': case 'w': case 'x': case 'y': case 'z':
-        case 'A': case 'B': case 'C': case 'D': case 'E':
-        case 'F': case 'G': case 'H': case 'I': case 'J':
-        case 'K': case 'L': case 'M': case 'N': case 'O':
-        case 'P': case 'Q': case 'R': case 'S': case 'T':
-        case 'U': case 'V': case 'W': case 'X': case 'Y': case 'Z':
+        if( str[i] == ',' || str[i] == '-' || str[i] == '.'
+            || ( '0' <= str[i] && str[i] <= '9' )
+            || ( 'a' <= str[i] && str[i] <= 'z' )
+            || ( 'A' <= str[i] && str[i] <= 'Z' )
+            || ( str[i] == '/' && !escape_slashes ) )
             evbuffer_add( out, &str[i], 1 );
-            break;
-        case '/':
-            if(!escape_slashes) {
-                evbuffer_add( out, &str[i], 1 );
-                break;
-            }
-            /* Fall through. */
-        default:
+        else
             evbuffer_add_printf( out, "%%%02X", (unsigned)(str[i]&0xFF) );
-            break;
-        }
     }
 }
 
