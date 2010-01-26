6e8cc99374f7577903eecc31d41a762a9776b476
Charles Kerr <charles@transmissionbt.com>
2010-01-26 20:38:04 +0000

    (trunk libT) #2800 "crashing during operation" -- if a peer sends an out-of-bounds "have piece" message, drop the connection

--- libtransmission/peer-msgs.c
+++ libtransmission/peer-msgs.c
@@ -1425,7 +1425,14 @@ readBtMessage( tr_peermsgs * msgs, struct evbuffer * inbuf, size_t inlen )
         case BT_HAVE:
             tr_peerIoReadUint32( msgs->peer->io, inbuf, &ui32 );
             dbgmsg( msgs, "got Have: %u", ui32 );
-            if( tr_bitsetAdd( &msgs->peer->have, ui32 ) ) {
+            if( tr_torrentHasMetadata( msgs->torrent )
+                    && ( ui32 >= msgs->torrent->info.pieceCount ) )
+            {
+                fireError( msgs, ERANGE );
+                return READ_ERR;
+            }
+            if( tr_bitsetAdd( &msgs->peer->have, ui32 ) )
+            {
                 fireError( msgs, ERANGE );
                 return READ_ERR;
             }
