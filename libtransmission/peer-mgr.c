bdb706edb9b1f89cea1e31dd69ff8ccb2bf470ca
Charles Kerr <charles@transmissionbt.com>
2010-02-03 00:28:07 +0000

    (trunk libT) this is an experimental commit for a debugging session with people who don't have access to build tools.  unless you're one of those testers, you might not want to use this revision.

--- libtransmission/peer-mgr.c
+++ libtransmission/peer-mgr.c
@@ -62,7 +62,7 @@ enum
 
     /* max number of peers to ask for per second overall.
     * this throttle is to avoid overloading the router */
-    MAX_CONNECTIONS_PER_SECOND = 16,
+    MAX_CONNECTIONS_PER_SECOND = 4,
 
     /* number of bad pieces a peer is allowed to send before we ban them */
     MAX_BAD_PIECES_PER_PEER = 5,
