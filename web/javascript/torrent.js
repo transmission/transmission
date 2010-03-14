baab619c934fbae2098bf36041d9f8c12369a544
Charles Kerr <charles@transmissionbt.com>
2010-03-14 05:28:25 +0000

    (trunk web) #3043 "show eta for seeding torrents in webui" -- imlemented in trunk for 2.00

--- web/javascript/torrent.js
+++ web/javascript/torrent.js
@@ -545,6 +545,17 @@ Torrent.prototype =
 		}
 		else
 		{
+			var eta = '';
+
+			if( this.isActive( ) && this.seedRatioLimit( ) > 0 )
+			{
+				eta = ' - ';
+				if (this._eta < 0 || this._eta >= Torrent._InfiniteTimeRemaining )
+					eta += 'remaining time unknown';
+				else
+					eta += Math.formatSeconds(this._eta) + ' remaining';
+			}
+
 			// Create the 'progress details' label
 			// Eg: '698.05 MB, uploaded 8.59 GB (Ratio: 12.3)'
 			c = Math.formatBytes( this._size );
@@ -558,6 +569,7 @@ Torrent.prototype =
 			else
 				c += '0';
 			c += ')';
+			c += eta;
 			progress_details = c;
 
 			var status = this.isActive() ? 'complete' : 'complete_stopped';
