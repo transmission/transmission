c4d76cd8d0acee5d1f28ff5854bf127858b848c1
Charles Kerr <charles@transmissionbt.com>
2010-04-25 15:19:08 +0000

    (trunk libT) slightly better comment i tr_torrentFileCompleted()

--- libtransmission/torrent.c
+++ libtransmission/torrent.c
@@ -2657,9 +2657,9 @@ tr_torrentFileCompleted( tr_torrent * tor, tr_file_index_t fileNum )
     /* close the file so that we can reopen in read-only mode as needed */
     tr_fdFileClose( tor->session, tor, fileNum );
 
-    /* if the torrent's filename on disk isn't the same as the one in the metadata,
-     * then it's been modified to denote that it was a partial file.
-     * Now that it's complete, use the proper filename. */
+    /* if the torrent's current filename isn't the same as the one in the
+     * metadata -- for example, if it had the ".part" suffix appended to
+     * it until now -- then rename it to match the one in the metadata */
     if( tr_torrentFindFile2( tor, fileNum, &base, &sub ) )
     {
         const tr_file * file = &tor->info.files[fileNum];
