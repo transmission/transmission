7da3b81cda8ad37ee2ea3aa59aa39cffd00f48e2
Charles Kerr <charles@transmissionbt.com>
2010-04-30 00:04:15 +0000

    (trunk libT) #2551 "when uploading to peers, prefetch local data from disk" -- commit jch patch #2, which prefetches before checksumming an individual piece

--- libtransmission/inout.c
+++ libtransmission/inout.c
@@ -308,6 +308,8 @@ recalculateHash( tr_torrent       * tor,
     SHA1_Init( &sha );
     bytesLeft = tr_torPieceCountBytes( tor, pieceIndex );
 
+    tr_ioPrefetch( tor, pieceIndex, offset, bytesLeft );
+
     while( bytesLeft )
     {
         const int len = MIN( bytesLeft, buflen );
