cf3417b2cdcf3a0f5a64b655beb9ecfe46ecec8f
Charles Kerr <charles@transmissionbt.com>
2010-06-04 16:48:36 +0000

    (trunk libT) lower the max # of outbound peer connections a little

--- libtransmission/peer-mgr.c
+++ libtransmission/peer-mgr.c
@@ -62,7 +62,7 @@ enum
 
     /* max number of peers to ask for per second overall.
     * this throttle is to avoid overloading the router */
-    MAX_CONNECTIONS_PER_SECOND = 12,
+    MAX_CONNECTIONS_PER_SECOND = 8,
 
     MAX_CONNECTIONS_PER_PULSE = (int)(MAX_CONNECTIONS_PER_SECOND * (RECONNECT_PERIOD_MSEC/1000.0)),
 
