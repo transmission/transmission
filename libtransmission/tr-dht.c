8e548aafcb9beba4dfa6fd0b5fae5b6e33e84e4c
Charles Kerr <charles@transmissionbt.com>
2010-10-30 01:48:45 +0000

    (trunk libT) #3687 "The V=XXXX key/val pair in our DHT messages seems to have no purpose" -- removed.

--- libtransmission/tr-dht.c
+++ libtransmission/tr-dht.c
@@ -58,7 +58,6 @@ THE SOFTWARE.
 #include "tr-dht.h"
 #include "trevent.h" /* tr_runInEventThread() */
 #include "utils.h"
-#include "version.h"
 
 static int dht_socket = -1, dht6_socket = -1;
 static struct event dht_event, dht6_event;
@@ -325,7 +324,6 @@ tr_dhtInit(tr_session *ss, const tr_address * tr_addr)
     uint8_t * nodes = NULL, * nodes6 = NULL;
     const uint8_t * raw;
     size_t len, len6;
-    char v[5];
     struct bootstrap_closure * cl;
 
     if( session ) /* already initialized */
@@ -387,12 +385,8 @@ tr_dhtInit(tr_session *ss, const tr_address * tr_addr)
         tr_cryptoRandBuf( myid, 20 );
     }
 
-    v[0] = 'T';
-    v[1] = 'R';
-    v[2] = MAJOR_VERSION;
-    v[3] = MINOR_VERSION;
-    rc = dht_init( dht_socket, dht6_socket, myid, (const unsigned char*)v );
-    if(rc < 0)
+    rc = dht_init( dht_socket, dht6_socket, myid, NULL );
+    if( rc < 0 )
         goto fail;
 
     session = ss;
