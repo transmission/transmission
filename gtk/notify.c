865c0853d5dce3de3327c10fcb634c7ea5dea163
Charles Kerr <charles@transmissionbt.com>
2010-11-06 14:37:34 +0000

    (trunk gtk) #3710 "Handle API changes to libnotify 0.7" -- fixed.  The cpp hack is borrowed from qbittorrent and torium... :)

--- gtk/notify.c
+++ gtk/notify.c
@@ -129,8 +129,12 @@ tr_notify_send( TrTorrent *tor )
         NotifyNotification * n;
 
         n = notify_notification_new( _( "Torrent Complete" ),
-                                     info->name,
-                                     NULL, NULL );
+                                     info->name, NULL
+/* the fourth argument was removed in libnotify 0.7.0 */
+#if !defined(NOTIFY_VERSION_MINOR) || (NOTIFY_VERSION_MAJOR == 0 && NOTIFY_VERSION_MINOR < 7)
+                                                     , NULL
+#endif
+                                                            );
         addIcon( n );
 
         if( can_support_actions( ) )
