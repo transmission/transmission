63d3e19ddeabb9f928313ae74d011a7202eac40b
Charles Kerr <charles@transmissionbt.com>
2010-12-05 18:29:13 +0000

    (2.0x gtk) backport r11259 for #3619 "memory leak that may cause events to be fired to invalid listeners"

--- gtk/details.c
+++ gtk/details.c
@@ -2005,6 +2005,9 @@ refreshTracker( struct DetailsImpl * di, tr_torrent ** torrents, int n )
         di->trackers_filtered = filter;
 
         gtk_tree_view_set_model( GTK_TREE_VIEW( di->tracker_view ), filter );
+
+        g_object_unref( filter );
+        g_object_unref( store );
     }
 
     if( ( di->tracker_buffer == NULL ) && ( n == 1 ) )
