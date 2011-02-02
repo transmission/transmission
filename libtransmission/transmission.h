859570345dd20f946745a115799c6ecd6e15ad81
Jordan Lee <jordan@transmissionbt.com>
2011-02-02 17:15:41 +0000

    (trunk libT) tweak the documentation on a couple of structs

--- libtransmission/transmission.h
+++ libtransmission/transmission.h
@@ -1612,25 +1612,23 @@ void tr_torrentVerify( tr_torrent * torrent );
 /** @brief a part of tr_info that represents a single file of the torrent's content */
 typedef struct tr_file
 {
-    uint64_t            length;    /* Length of the file, in bytes */
-    char *              name;      /* Path to the file */
-    int8_t              priority;  /* TR_PRI_HIGH, _NORMAL, or _LOW */
-    int8_t              dnd;       /* nonzero if the file shouldn't be
-                                     downloaded */
-    tr_piece_index_t    firstPiece; /* We need pieces [firstPiece... */
-    tr_piece_index_t    lastPiece; /* ...lastPiece] to dl this file */
-    uint64_t            offset;    /* file begins at the torrent's nth byte */
+    uint64_t          length;      /* Length of the file, in bytes */
+    char *            name;        /* Path to the file */
+    int8_t            priority;    /* TR_PRI_HIGH, _NORMAL, or _LOW */
+    int8_t            dnd;         /* "do not download" flag */
+    tr_piece_index_t  firstPiece;  /* We need pieces [firstPiece... */
+    tr_piece_index_t  lastPiece;   /* ...lastPiece] to dl this file */
+    uint64_t          offset;      /* file begins at the torrent's nth byte */
 }
 tr_file;
 
 /** @brief a part of tr_info that represents a single piece of the torrent's content */
 typedef struct tr_piece
 {
-    time_t     timeChecked;             /* the last time we tested this piece */
-    uint8_t    hash[SHA_DIGEST_LENGTH]; /* pieces hash */
-    int8_t     priority;               /* TR_PRI_HIGH, _NORMAL, or _LOW */
-    int8_t     dnd;                    /* nonzero if the piece shouldn't be
-                                         downloaded */
+    time_t   timeChecked;              /* the last time we tested this piece */
+    uint8_t  hash[SHA_DIGEST_LENGTH];  /* pieces hash */
+    int8_t   priority;                 /* TR_PRI_HIGH, _NORMAL, or _LOW */
+    int8_t   dnd;                      /* "do not download" flag */
 }
 tr_piece;
 
