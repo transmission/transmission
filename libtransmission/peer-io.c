5563729d61e40f84299a18fec524ebc3bd58a2de
Juliusz Chroboczek <jch@pps.univ-paris-diderot.fr>
2011-02-18 00:24:15 +0000

    Add assertions to libevent callbacks.
    They should never be called with a UTP socket.

--- libtransmission/peer-io.c
+++ libtransmission/peer-io.c
@@ -218,6 +218,7 @@ event_read_cb( int fd, short event UNUSED, void * vio )
     const unsigned int max = 256 * 1024;
 
     assert( tr_isPeerIo( io ) );
+    assert( io->socket >= 0 );
 
     io->pendingEvents &= ~EV_READ;
 
@@ -293,6 +294,7 @@ event_write_cb( int fd, short event UNUSED, void * vio )
     const tr_direction dir = TR_UP;
 
     assert( tr_isPeerIo( io ) );
+    assert( io->socket >= 0 );
 
     io->pendingEvents &= ~EV_WRITE;
 
