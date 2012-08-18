9221f8a2481170553b5bb843047f9aff164e2a0b
Jordan Lee <jordan@transmissionbt.com>
2012-08-18 14:59:09 +0000

    (trunk, web) #5008 'html entitiy not correctly rendered' -- use patch submitted by jfc

--- web/javascript/file-row.js
+++ web/javascript/file-row.js
@@ -137,7 +137,7 @@ function FileRow(torrent, i)
 
 		name = file.name || 'Unknown';
 		name = name.substring(name.lastIndexOf('/')+1);
-		name = name.replace(/([\/_\.])/g, "$1&#8203;");
+		name = name.replace(/([\/_\.])/g, "$1​");
 		e = document.createElement('div');
 		e.className = "inspector_torrent_file_list_entry_name";
 		setTextContent(e, name);
