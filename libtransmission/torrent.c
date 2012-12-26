7081027041c4f18cba61b334d882536d1e5956fb
Jordan Lee <jordan@transmissionbt.com>
2012-12-26 23:34:32 +0000

    #5191 'using a webseed doesn't update the remaining time' -- fixed with patch by cfpp2p.

--- libtransmission/torrent.c
+++ libtransmission/torrent.c
@@ -1256,7 +1256,7 @@ tr_torrentStat (tr_torrent * tor)
             tor->etaDLSpeedCalculatedAt = now;
           }
 
-        if (s->leftUntilDone > s->desiredAvailable)
+        if ((s->leftUntilDone > s->desiredAvailable) && (tor->info.webseedCount < 0))
           s->eta = TR_ETA_NOT_AVAIL;
         else if (tor->etaDLSpeed_KBps < 1)
           s->eta = TR_ETA_UNKNOWN;
