00a5e869394eac2f240f721143368a3dcbb86b02
Jordan Lee <jordan@transmissionbt.com>
2013-02-09 18:59:05 +0000

    (qt) #5282 "Formatter's sizeToString() and memToString() return wrong text for 0 bytes": fixed.

--- qt/formatter.h
+++ qt/formatter.h
@@ -1,5 +1,5 @@
 /*
- * This file Copyright (C) Mnemosyne LLC
+ * This file Copyright(C) Mnemosyne LLC
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2
@@ -23,34 +23,34 @@ class Speed;
 
 class Formatter: public QObject
 {
-        Q_OBJECT
+    Q_OBJECT
 
-    public:
+  public:
 
-        Formatter( ) { }
-        virtual ~Formatter( ) { }
+    Formatter() {}
+    virtual ~Formatter() {}
 
-    public:
+  public:
 
-        static QString memToString( int64_t bytes );
-        static QString sizeToString( int64_t bytes );
-        static QString speedToString( const Speed& speed );
-        static QString percentToString( double x );
-        static QString ratioToString( double ratio );
-        static QString timeToString( int seconds );
-        static QString uploadSpeedToString (const Speed& up);
-        static QString downloadSpeedToString (const Speed& down);
+    static QString memToString (int64_t bytes);
+    static QString sizeToString (int64_t bytes);
+    static QString speedToString (const Speed& speed);
+    static QString percentToString (double x);
+    static QString ratioToString (double ratio);
+    static QString timeToString (int seconds);
+    static QString uploadSpeedToString(const Speed& up);
+    static QString downloadSpeedToString(const Speed& down);
 
-    public:
+  public:
 
-        typedef enum { B, KB, MB, GB, TB } Size;
-        typedef enum { SPEED, SIZE, MEM } Type;
-        static QString unitStr( Type t, Size s ) { return unitStrings[t][s]; }
-        static void initUnits( );
+    typedef enum { B, KB, MB, GB, TB } Size;
+    typedef enum { SPEED, SIZE, MEM } Type;
+    static QString unitStr (Type t, Size s) { return unitStrings[t][s]; }
+    static void initUnits ();
 
-    private:
+  private:
 
-        static QString unitStrings[3][5];
+    static QString unitStrings[3][5];
 };
 
 #endif
