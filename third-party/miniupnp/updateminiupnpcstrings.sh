3d497891ee115e6751f8abd8c56279df1083c707
Mike Gelfand <mikedld@mikedld.com>
2015-10-08 19:12:03 +0000

    #5867: Fix miniupnpc strings script re. r14269 (patch by demosdemon)

--- third-party/miniupnp/updateminiupnpcstrings.sh
+++ third-party/miniupnp/updateminiupnpcstrings.sh
@@ -38,7 +38,7 @@ if [ "$OS_NAME" = "AmigaOS" ]; then
 fi
 
 echo "Detected OS [$OS_NAME] version [$OS_VERSION]"
-MINIUPNPC_VERSION=`cat VERSION`
+MINIUPNPC_VERSION=`cat ${VERSION_FILE}`
 echo "MiniUPnPc version [${MINIUPNPC_VERSION}]"
 
 EXPR="s|OS_STRING \".*\"|OS_STRING \"${OS_NAME}/${OS_VERSION}\"|"
