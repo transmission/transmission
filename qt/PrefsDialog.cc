8579732a3ebe2307dd04ec90011a3e818bba4250
Tomaz Canabrava <tomaz.canabrava@intel.com>
2016-02-23 16:55:43 -0300

    use initializer-lists instead of populating the vector on runtime
    Signed-off-by: Tomaz Canabrava <tomaz.canabrava@intel.com>

--- qt/PrefsDialog.cc
+++ qt/PrefsDialog.cc
@@ -524,17 +524,18 @@ PrefsDialog::PrefsDialog (Session& session, Prefs& prefs, QWidget * parent):
 
   connect (&mySession, SIGNAL (sessionUpdated ()), SLOT (sessionUpdated ()));
 
-  QList<int> keys;
-  keys << Prefs::RPC_ENABLED
-       << Prefs::ALT_SPEED_LIMIT_ENABLED
-       << Prefs::ALT_SPEED_LIMIT_TIME_ENABLED
-       << Prefs::ENCRYPTION
-       << Prefs::BLOCKLIST_ENABLED
-       << Prefs::DIR_WATCH
-       << Prefs::DOWNLOAD_DIR
-       << Prefs::INCOMPLETE_DIR
-       << Prefs::INCOMPLETE_DIR_ENABLED
-       << Prefs::SCRIPT_TORRENT_DONE_FILENAME;
+  QList<int> keys = {
+    Prefs::RPC_ENABLED,
+    Prefs::ALT_SPEED_LIMIT_ENABLED,
+    Prefs::ALT_SPEED_LIMIT_TIME_ENABLED,
+    Prefs::ENCRYPTION,
+    Prefs::BLOCKLIST_ENABLED,
+    Prefs::DIR_WATCH,
+    Prefs::DOWNLOAD_DIR,
+    Prefs::INCOMPLETE_DIR,
+    Prefs::INCOMPLETE_DIR_ENABLED,
+    Prefs::SCRIPT_TORRENT_DONE_FILENAME};
+
   foreach (const int key, keys)
     refreshPref (key);
 
