66285a525ba8c78b73c7c7fd340a314757624c50
Mike Gelfand <mikedld@mikedld.com>
2016-04-12 19:03:01 +0000

    Web UI: main() refactoring (patch by skybon @ GH-9)
    Less jQuery, more Web API. Moved main() away from common.

--- web/javascript/common.js
+++ web/javascript/common.js
@@ -1,5 +1,5 @@
 /**
- * Copyright © Dave Perrett and Malcolm Jarvis
+ * Copyright © Dave Perrett, Malcolm Jarvis and Artem Vorotnikov
  *
  * This file is licensed under the GPLv2.
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -9,18 +9,6 @@ var transmission,
     dialog,
     isMobileDevice = RegExp("(iPhone|iPod|Android)").test(navigator.userAgent),
     scroll_timeout;
-
-if (!Array.indexOf) {
-    Array.prototype.indexOf = function (obj) {
-        var i, len;
-        for (i = 0, len = this.length; i < len; i++) {
-            if (this[i] == obj) {
-                return i;
-            };
-        };
-        return -1;
-    };
-};
 
 // http://forum.jquery.com/topic/combining-ui-dialog-and-tabs
 $.fn.tabbedDialog = function (dialog_opts) {
@@ -43,57 +31,6 @@ $.fn.tabbedDialog = function (dialog_opts) {
     this.siblings('.ui-dialog-titlebar').remove();
     tabul.addClass('ui-dialog-titlebar');
 }
-
-$(document).ready(function () {
-
-    // IE8 and below don’t support ES5 Date.now()
-    if (!Date.now) {
-        Date.now = function () {
-            return +new Date();
-        };
-    };
-
-    // IE specific fixes here
-    if ($.browser.msie) {
-        try {
-            document.execCommand("BackgroundImageCache", false, true);
-        } catch (err) {};
-        $('.dialog_container').css('height', $(window).height() + 'px');
-    };
-
-    if ($.browser.safari) {
-        // Move search field's margin down for the styled input
-        $('#torrent_search').css('margin-top', 3);
-    };
-
-    if (isMobileDevice) {
-        window.onload = function () {
-            setTimeout(function () {
-                window.scrollTo(0, 1);
-            }, 500);
-        };
-        window.onorientationchange = function () {
-            setTimeout(function () {
-                window.scrollTo(0, 1);
-            }, 100);
-        };
-        if (window.navigator.standalone) {
-            // Fix min height for isMobileDevice when run in full screen mode from home screen
-            // so the footer appears in the right place
-            $('body div#torrent_container').css('min-height', '338px');
-        };
-        $("label[for=torrent_upload_url]").text("URL: ");
-    } else {
-        // Fix for non-Safari-3 browsers: dark borders to replace shadows.
-        $('div.dialog_container div.dialog_window').css('border', '1px solid #777');
-    };
-
-    // Initialise the dialog controller
-    dialog = new Dialog();
-
-    // Initialise the main Transmission controller
-    transmission = new Transmission();
-});
 
 /**
  * Checks to see if the content actually changed before poking the DOM.
