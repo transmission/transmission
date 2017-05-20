11d29ff62d528319f9b9596fd746dd7795aff140
Niklas Haas <git@haasn.xyz>
2017-05-20 07:16:53 +0200

    LibreSSL compatibility
    This seems to be the idiomatic way to fix libressl compatibility issues,
    judging by what most other open source projects seem to be doing.
    
    I've confirmed that transmission builds with libressl for me after this
    patch is applied.

--- libtransmission/crypto-utils-openssl.c
+++ libtransmission/crypto-utils-openssl.c
@@ -50,7 +50,7 @@ log_openssl_error (const char * file,
       static bool strings_loaded = false;
       if (!strings_loaded)
         {
-#if OPENSSL_VERSION_NUMBER < 0x10100000
+#if OPENSSL_VERSION_NUMBER < 0x10100000 || defined (LIBRESSL_VERSION_NUMBER)
           ERR_load_crypto_strings ();
 #else
           OPENSSL_init_crypto (OPENSSL_INIT_LOAD_CRYPTO_STRINGS, NULL);
@@ -236,7 +236,7 @@ tr_rc4_process (tr_rc4_ctx_t   handle,
 ****
 ***/
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000
+#if OPENSSL_VERSION_NUMBER < 0x10100000 || defined (LIBRESSL_VERSION_NUMBER)
 
 static inline int
 DH_set0_pqg (DH     * dh,
