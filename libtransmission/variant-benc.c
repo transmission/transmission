8c53b4a5ff24f6719ab04cc371374b4cafadb975
Charles Kerr <ckerr@github.com>
2019-02-17 11:16:53 +0000

    fix: better fix to serializing benc strings
    The approach in 33e2ece7e5bc261566ae9e8db57be0b3247508d1 was
    a little problematic: GetString() shouldn't fail here; but if
    it somehow did, we still want to encode a zero-length benc string here.

--- libtransmission/variant-benc.c
+++ libtransmission/variant-benc.c
@@ -351,11 +351,13 @@ static void saveStringFunc(tr_variant const* v, void* evbuf)
 {
     size_t len;
     char const* str;
-    if (tr_variantGetStr(v, &str, &len))
+    if (!tr_variantGetStr(v, &str, &len))
     {
-        evbuffer_add_printf(evbuf, "%zu:", len);
-        evbuffer_add(evbuf, str, len);
+        len = 0;
+        str = NULL;
     }
+    evbuffer_add_printf(evbuf, "%zu:", len);
+    evbuffer_add(evbuf, str, len);
 }
 
 static void saveDictBeginFunc(tr_variant const* val UNUSED, void* evbuf)
