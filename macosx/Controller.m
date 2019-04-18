4331d9f0c7f4099743e1d3a57744492687fc0552
Dmitry Serov <barbari100@gmail.com>
2019-04-19 02:28:45 +0700

    Ignore non-URL strings when opening URLs

--- macosx/Controller.m
+++ macosx/Controller.m
@@ -1269,8 +1269,14 @@ static void removeKeRangerRansomware()
             else
                 urlString = [@"http://" stringByAppendingString: urlString];
         }
+        
+        NSURL * url = [NSURL URLWithString: urlString];
+        if (url == nil)
+        {
+            return;
+        }
 
-        NSURLRequest * request = [NSURLRequest requestWithURL: [NSURL URLWithString: urlString]
+        NSURLRequest * request = [NSURLRequest requestWithURL: url
                                     cachePolicy: NSURLRequestReloadIgnoringLocalAndRemoteCacheData timeoutInterval: 60];
 
         if (fPendingTorrentDownloads[[request URL]])
