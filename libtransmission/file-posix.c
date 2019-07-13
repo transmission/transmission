16d385e4c7c8a8a424a44f46f81087572921d351
Mike Gelfand <mikedld@mikedld.com>
2019-07-13 23:02:27 +0300

    Fixup code style

--- libtransmission/file-posix.c
+++ libtransmission/file-posix.c
@@ -988,7 +988,7 @@ bool tr_sys_file_lock(tr_sys_file_t handle, int operation, tr_error** error)
 
 #if defined(F_OFD_SETLK)
 
-    struct flock fl = {0};
+    struct flock fl = { 0 };
 
     switch (operation & (TR_SYS_FILE_LOCK_SH | TR_SYS_FILE_LOCK_EX | TR_SYS_FILE_LOCK_UN))
     {
