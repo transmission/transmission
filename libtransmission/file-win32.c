49740c2025f61fffe695542fa0d0c9676fc3be11
Robert Crowston <crowston@protonmail.com>
2020-01-12 02:12:18 +0000

    tr_sys_path_copy: handle win32 wide characters in paths.

--- libtransmission/file-win32.c
+++ libtransmission/file-win32.c
@@ -735,15 +735,33 @@ bool tr_sys_path_rename(char const* src_path, char const* dst_path, tr_error** e
 
 bool tr_sys_path_copy(char const* src_path, char const* dst_path, tr_error** error)
 {
-    LPBOOL cancel = FALSE;
-    DWORD const flags = COPY_FILE_ALLOW_DECRYPTED_DESTINATION | COPY_FILE_FAIL_IF_EXISTS;
-    if (CopyFileExW(src_path, dst_path, NULL, NULL, &cancel, flags) == 0)
+    bool ret = false;
+
+    wchar_t* wide_src_path = path_to_native_path(src_path);
+    wchar_t* wide_dst_path = path_to_native_path(dst_path);
+
+    if (wide_src_path == NULL || wide_dst_path == NULL)
     {
-        set_system_error(error, GetLastError());
-        return false;
+        goto out;
     }
 
-    return true;
+    LPBOOL cancel = FALSE;
+    DWORD const flags = COPY_FILE_ALLOW_DECRYPTED_DESTINATION | COPY_FILE_FAIL_IF_EXISTS;
+    if (CopyFileExW(wide_src_path, wide_dst_path, NULL, NULL, &cancel, flags) == 0)
+    {
+        set_system_error(error, GetLastError());
+        goto out;
+    }
+    else
+    {
+        ret = true;
+    }
+
+out:
+    tr_free(wide_src_path);
+    tr_free(wide_dst_path);
+
+    return ret;
 }
 
 bool tr_sys_path_remove(char const* path, tr_error** error)
