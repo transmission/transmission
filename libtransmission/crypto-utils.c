7f3ee1c92e565a0b9e86768091af9e4f11518474
Mingye Wang <arthur200126@gmail.com>
2020-05-01 21:08:04 +0800

    crypto-utils: do tr_rand_int without abs
    There is really not much point in trying to abs a random int when you can just interpret it as unsigned in the first place.

--- libtransmission/crypto-utils.c
+++ libtransmission/crypto-utils.c
@@ -89,17 +89,11 @@ int tr_rand_int(int upper_bound)
 {
     TR_ASSERT(upper_bound > 0);
 
-    int noise;
+    unsigned noise;
 
-    while (tr_rand_buffer(&noise, sizeof(noise)))
-    {
-        noise = abs(noise) % upper_bound;
-
-        /* abs(INT_MIN) is undefined and could return negative value */
-        if (noise >= 0)
-        {
-            return noise;
-        }
+    if (tr_rand_buffer(&noise, sizeof(noise)))
+    {       
+        return noise % upper_bound;
     }
 
     /* fall back to a weaker implementation... */
