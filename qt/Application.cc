7ee72f3406afe6703e169215b3e66ec47e297fbc
Charles Kerr <charles@charleskerr.com>
2020-06-02 15:36:47 -0500

    fixup! chore: shut up tr_main argv avoid-c-arrays clang-tidy warning

--- qt/Application.cc
+++ qt/Application.cc
@@ -626,7 +626,7 @@ FaviconCache& Application::faviconCache()
 ****
 ***/
 
-// NOLINTNEXTLINE(*-avoid-c-arrays)
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays)
 int tr_main(int argc, char* argv[])
 {
     InteropHelper::initialize();
