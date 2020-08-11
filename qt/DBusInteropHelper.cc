ac2222b2e6e185483c6554f03300152e5481fb52
Charles Kerr <charles@charleskerr.com>
2020-08-11 13:28:11 -0500

    refactor: remove exit-time destructors in DBusInteropHelper

--- qt/DBusInteropHelper.cc
+++ qt/DBusInteropHelper.cc
@@ -17,15 +17,6 @@
 #include "DBusInteropHelper.h"
 #include "InteropObject.h"
 
-namespace
-{
-
-auto const DBusService = QStringLiteral("com.transmissionbt.Transmission");
-auto const DBusObjectPath = QStringLiteral("/com/transmissionbt/Transmission");
-auto const DBusInterface = QStringLiteral("com.transmissionbt.Transmission");
-
-} // namespace
-
 bool DBusInteropHelper::isConnected() const
 {
     return QDBusConnection::sessionBus().isConnected();
@@ -33,8 +24,12 @@ bool DBusInteropHelper::isConnected() const
 
 QVariant DBusInteropHelper::addMetainfo(QString const& metainfo)
 {
-    QDBusMessage request = QDBusMessage::createMethodCall(DBusService, DBusObjectPath, DBusInterface,
-        QStringLiteral("AddMetainfo"));
+    auto request = QDBusMessage::createMethodCall(
+        QStringLiteral("com.transmissionbt.Transmission"),
+        QStringLiteral("com/transmissionbt/Transmission"),
+        QStringLiteral("com.transmissionbt.Transmission"),
+        QStringLiteral("AddMetainfo")
+    );
     request.setArguments(QVariantList() << metainfo);
 
     QDBusReply<bool> const response = QDBusConnection::sessionBus().call(request);
@@ -43,20 +38,21 @@ QVariant DBusInteropHelper::addMetainfo(QString const& metainfo)
 
 void DBusInteropHelper::registerObject(QObject* parent)
 {
-    QDBusConnection bus = QDBusConnection::sessionBus();
-
+    auto bus = QDBusConnection::sessionBus();
     if (!bus.isConnected())
     {
         return;
     }
 
-    if (!bus.registerService(DBusService))
+    auto const service_name = QStringLiteral("com.transmissionbt.Transmission");
+    if (!bus.registerService(service_name))
     {
-        std::cerr << "couldn't register " << qPrintable(DBusService) << std::endl;
+        std::cerr << "couldn't register " << qPrintable(service_name) << std::endl;
     }
 
-    if (!bus.registerObject(DBusObjectPath, new InteropObject(parent), QDBusConnection::ExportAllSlots))
+    auto const object_path = QStringLiteral("com/transmissionbt/Transmission");
+    if (!bus.registerObject(object_path, new InteropObject(parent), QDBusConnection::ExportAllSlots))
     {
-        std::cerr << "couldn't register " << qPrintable(DBusObjectPath) << std::endl;
+        std::cerr << "couldn't register " << qPrintable(object_path) << std::endl;
     }
 }
