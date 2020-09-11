e253705d19f20d232aff82302b90f64b34a6b620
Charles Kerr <charles@charleskerr.com>
2020-09-10 19:36:18 -0500

    chore: remove unused variable
    found by sanity

--- libtransmission/magnet.c
+++ libtransmission/magnet.c
@@ -38,8 +38,6 @@ static int const base32Lookup[] =
     0x17, 0x18, 0x19, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF /* 'x', 'y', 'z', '{', '|', '}', '~', 'DEL' */
 };
 
-static int const base32LookupLen = TR_N_ELEMENTS(base32Lookup);
-
 static void base32_to_sha1(uint8_t* out, char const* in, size_t const inlen)
 {
     TR_ASSERT(inlen == 32);
