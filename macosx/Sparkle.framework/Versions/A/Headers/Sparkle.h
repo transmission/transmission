08381e4ba3e013e2aab1527ab516630067c1b871
Mitchell Livingston <livings124@mac.com>
2020-11-15 10:19:32 -0500

    Update Sparkle to 1.24.0

--- macosx/Sparkle.framework/Versions/A/Headers/Sparkle.h
+++ macosx/Sparkle.framework/Versions/A/Headers/Sparkle.h
@@ -12,6 +12,10 @@
 // This list should include the shared headers. It doesn't matter if some of them aren't shared (unless
 // there are name-space collisions) so we can list all of them to start with:
 
+#pragma clang diagnostic push
+// Do not use <> style includes since 2.x has two frameworks that need to work: Sparkle and SparkleCore
+#pragma clang diagnostic ignored "-Wquoted-include-in-framework-header"
+
 #import "SUAppcast.h"
 #import "SUAppcastItem.h"
 #import "SUStandardVersionComparator.h"
@@ -29,5 +33,7 @@
 #import "SPUDownloaderSession.h"
 #import "SPUURLRequest.h"
 #import "SUCodeSigningVerifier.h"
+
+#pragma clang diagnostic pop
 
 #endif
