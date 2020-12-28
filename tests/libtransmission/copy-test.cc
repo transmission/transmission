3db96944bf470beab781953d6d85ae33cb7353f9
Robert Crowston <crowston@protonmail.com>
2020-12-28 01:54:20 +0000

    Acceptable coding style has changed again.
    Now it's camelCase. Also use nullptr instead of NULL, etc.

--- tests/libtransmission/copy-test.cc
+++ tests/libtransmission/copy-test.cc
@@ -23,35 +23,34 @@ class CopyTest : public SandboxedTest
 protected:
     void testImpl(char const* filename1, char const* filename2, const size_t file_length)
     {
-        auto const path1 = tr_buildPath(sandboxDir().data(), filename1, NULL);
+        auto const path1 = tr_buildPath(sandboxDir().data(), filename1, nullptr);
 
         /* Create a file. */
-        char* file_content = (char*)tr_malloc(file_length);
+        char* file_content = static_cast<char*>(tr_malloc(file_length));
         tr_rand_buffer(file_content, file_length);
         createFileWithContents(path1, file_content, file_length);
         tr_free(file_content);
 
-        auto const path2 = tr_buildPath(sandboxDir().data(), filename2, NULL);
+        auto const path2 = tr_buildPath(sandboxDir().data(), filename2, nullptr);
 
         tr_error* err = nullptr;
         /* Copy it. */
         EXPECT_TRUE(tr_sys_path_copy(path1, path2, &err));
-
         EXPECT_EQ(nullptr, err);
         tr_error_clear(&err);
 
-        EXPECT_TRUE(files_are_identical(path1, path2));
+        EXPECT_TRUE(filesAreIdentical(path1, path2));
 
         /* Dispose of those files that we created. */
-        tr_sys_path_remove(path1, NULL);
+        tr_sys_path_remove(path1, nullptr);
         tr_free(path1);
 
-        tr_sys_path_remove(path2, NULL);
+        tr_sys_path_remove(path2, nullptr);
         tr_free(path2);
     }
 
 private:
-    uint64_t fill_buffer_from_fd(tr_sys_file_t fd, uint64_t bytes_remaining, char* buf, size_t buf_len)
+    uint64_t fillBufferFromFd(tr_sys_file_t fd, uint64_t bytes_remaining, char* buf, size_t buf_len)
     {
         memset(buf, 0, buf_len);
 
@@ -61,7 +60,7 @@ private:
             uint64_t const chunk_size = MIN(buf_len - buf_pos, bytes_remaining);
             uint64_t bytes_read = 0;
 
-            tr_sys_file_read(fd, buf + buf_pos, chunk_size, &bytes_read, NULL);
+            tr_sys_file_read(fd, buf + buf_pos, chunk_size, &bytes_read, nullptr);
 
             EXPECT_LE(buf_pos + bytes_read, buf_len);
             EXPECT_LE(bytes_read, bytes_remaining);
@@ -72,29 +71,30 @@ private:
         return bytes_remaining;
     }
 
-    bool files_are_identical(char const* fn1, char const* fn2)
+    bool filesAreIdentical(char const* fn1, char const* fn2)
     {
-        tr_sys_file_t fd1 = tr_sys_file_open(fn1, TR_SYS_FILE_READ | TR_SYS_FILE_SEQUENTIAL, 0, NULL);
-        tr_sys_file_t fd2 = tr_sys_file_open(fn2, TR_SYS_FILE_READ | TR_SYS_FILE_SEQUENTIAL, 0, NULL);
+        tr_sys_file_t fd1 = tr_sys_file_open(fn1, TR_SYS_FILE_READ | TR_SYS_FILE_SEQUENTIAL, 0, nullptr);
+        tr_sys_file_t fd2 = tr_sys_file_open(fn2, TR_SYS_FILE_READ | TR_SYS_FILE_SEQUENTIAL, 0, nullptr);
         EXPECT_NE(fd1, TR_BAD_SYS_FILE);
         EXPECT_NE(fd2, TR_BAD_SYS_FILE);
 
-        tr_sys_path_info info1, info2;
-        tr_sys_file_get_info(fd1, &info1, NULL);
-        tr_sys_file_get_info(fd2, &info2, NULL);
+        tr_sys_path_info info1;
+        tr_sys_path_info info2;
+        tr_sys_file_get_info(fd1, &info1, nullptr);
+        tr_sys_file_get_info(fd2, &info2, nullptr);
         EXPECT_EQ(info1.size, info2.size);
 
         uint64_t bytes_left1 = info1.size;
         uint64_t bytes_left2 = info2.size;
 
         size_t const buflen = 2 * 1024 * 1024; /* 2 MiB buffer */
-        char* readbuf1 = (char*)tr_malloc(buflen);
-        char* readbuf2 = (char*)tr_malloc(buflen);
+        char* readbuf1 = static_cast<char*>(tr_malloc(buflen));
+        char* readbuf2 = static_cast<char*>(tr_malloc(buflen));
 
         while (bytes_left1 > 0 || bytes_left2 > 0)
         {
-            bytes_left1 = fill_buffer_from_fd(fd1, bytes_left1, readbuf1, buflen);
-            bytes_left2 = fill_buffer_from_fd(fd2, bytes_left2, readbuf2, buflen);
+            bytes_left1 = fillBufferFromFd(fd1, bytes_left1, readbuf1, buflen);
+            bytes_left2 = fillBufferFromFd(fd2, bytes_left2, readbuf2, buflen);
 
             if (bytes_left1 != bytes_left2)
             {
@@ -109,8 +109,8 @@ private:
 
         tr_free(readbuf1);
         tr_free(readbuf2);
-        tr_sys_file_close(fd1, NULL);
-        tr_sys_file_close(fd2, NULL);
+        tr_sys_file_close(fd1, nullptr);
+        tr_sys_file_close(fd2, nullptr);
 
         return true;
     }
