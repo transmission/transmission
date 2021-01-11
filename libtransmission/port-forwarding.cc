50cbe390ad3e3cc5afe405d20c24f59933cca8c0
Mickael Hoerdt <mickael.hoerdt@protonmail.ch>
2021-01-11 12:09:26 +0100

    NAT-PMP private/public port and lifetime fix

--- libtransmission/port-forwarding.cc
+++ libtransmission/port-forwarding.cc
@@ -68,6 +68,9 @@ static char const* getNatStateStr(int state)
 
 static void natPulse(tr_shared* s, bool do_check)
 {
+    int oldStatus;
+    int newStatus;
+    tr_port received_private_port;
     tr_port const private_peer_port = s->session->private_peer_port;
     bool const is_enabled = s->isEnabled && !s->isShuttingDown;
 
@@ -84,11 +87,14 @@ static void natPulse(tr_shared* s, bool do_check)
     auto const old_status = tr_sharedTraversalStatus(s);
 
     auto public_peer_port = tr_port{};
-    s->natpmpStatus = tr_natpmpPulse(s->natpmp, private_peer_port, is_enabled, &public_peer_port);
+    s->natpmpStatus = tr_natpmpPulse(s->natpmp, private_peer_port, is_enabled, &public_peer_port, &received_private_port);
 
     if (s->natpmpStatus == TR_PORT_MAPPED)
     {
         s->session->public_peer_port = public_peer_port;
+        s->session->private_peer_port = received_private_port;
+        tr_logAddNamedInfo(
+            getKey(), "public peer port %d (private %d) ", s->session->public_peer_port, s->session->private_peer_port);
     }
 
     s->upnpStatus = tr_upnpPulse(s->upnp, private_peer_port, is_enabled, do_check);
@@ -117,7 +123,8 @@ static void set_evtimer_from_status(tr_shared* s)
         /* if we're mapped, everything is fine... check back in 20 minutes
          * to renew the port forwarding if it's expired */
         s->doPortCheck = true;
-        sec = 60 * 20;
+        sec = s->natpmp->renew_time - time(NULL);
+
         break;
 
     case TR_PORT_ERROR:
