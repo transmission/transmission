2b76444b7310969517269bd3afc21ed99fc2bcac
Charles Kerr <charles@charleskerr.com>
2021-09-17 20:27:14 -0500

    try using gulrak/filesystem as a fallback

--- libtransmission/file-posix.cc
+++ libtransmission/file-posix.cc
@@ -25,7 +25,21 @@
 #include <unistd.h> /* lseek(), write(), ftruncate(), pread(), pwrite(), pathconf(), etc */
 #include <vector>
 
-#include <ghc/filesystem>
+#ifdef __APPLE__
+#include <Availability.h> // for deployment target to support pre-catalina targets without std::fs
+#endif
+#if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || (defined(__cplusplus) && __cplusplus >= 201703L)) && \
+    defined(__has_include)
+#if __has_include(<filesystem>) && (!defined(__MAC_OS_X_VERSION_MIN_REQUIRED) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)
+#define GHC_USE_STD_FS
+#include <filesystem>
+namespace fs = std::filesystem;
+#endif
+#endif
+#ifndef GHC_USE_STD_FS
+#include <ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
+#endif
 
 #ifdef HAVE_XFS_XFS_H
 #include <xfs/xfs.h>
--- libtransmission/file.cc
+++ libtransmission/file.cc
@@ -7,7 +7,22 @@
  */
 
 #include <string.h> /* strlen() */
+
+#ifdef __APPLE__
+#include <Availability.h> // for deployment target to support pre-catalina targets without std::fs
+#endif
+#if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || (defined(__cplusplus) && __cplusplus >= 201703L)) && \
+    defined(__has_include)
+#if __has_include(<filesystem>) && (!defined(__MAC_OS_X_VERSION_MIN_REQUIRED) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 101500)
+#define GHC_USE_STD_FS
+#include <filesystem>
+namespace fs = std::filesystem;
+#endif
+#endif
+#ifndef GHC_USE_STD_FS
 #include <ghc/filesystem.hpp>
+namespace fs = ghc::filesystem;
+#endif
 
 #include "transmission.h"
 #include "error.h"
