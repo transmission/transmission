d21606ec30db33d2f15c031a172d018992026696
Charles Kerr <charles@charleskerr.com>
2021-10-10 11:00:42 -0500

    refactor: use [[maybe_unused]] in libtransmission/watchdir-inotify.cc

--- libtransmission/watchdir-inotify.cc
+++ libtransmission/watchdir-inotify.cc
@@ -55,11 +55,8 @@ struct tr_watchdir_inotify
 ****
 ***/
 
-static void tr_watchdir_inotify_on_first_scan(evutil_socket_t fd, short type, void* context)
+static void tr_watchdir_inotify_on_first_scan([[maybe_unused]] evutil_socket_t fd, [[maybe_unused]] short type, void* context)
 {
-    TR_UNUSED(fd);
-    TR_UNUSED(type);
-
     auto const handle = static_cast<tr_watchdir_t>(context);
 
     tr_watchdir_scan(handle, nullptr);
