76da26a1f77ecfa35e6b6a304713f9cf89500d5f
Charles Kerr <charles@charleskerr.com>
2021-10-22 09:01:37 -0500

    fixup! refactor: uninit vars in cache.cc

--- libtransmission/cache.cc
+++ libtransmission/cache.cc
@@ -422,16 +422,15 @@ int tr_cacheFlushDone(tr_cache* cache)
 
 int tr_cacheFlushFile(tr_cache* cache, tr_torrent* torrent, tr_file_index_t i)
 {
-    int pos;
-    int err = 0;
-    tr_block_index_t first;
-    tr_block_index_t last;
-
+    auto first = tr_block_index_t{};
+    auto last = tr_block_index_t{};
     tr_torGetFileBlockRange(torrent, i, &first, &last);
-    pos = findBlockPos(cache, torrent, first);
+
+    int pos = findBlockPos(cache, torrent, first);
     dbgmsg("flushing file %d from cache to disk: blocks [%zu...%zu]", (int)i, (size_t)first, (size_t)last);
 
     /* flush out all the blocks in that file */
+    int err = 0;
     while (err == 0 && pos < tr_ptrArraySize(&cache->blocks))
     {
         auto const* b = static_cast<struct cache_block const*>(tr_ptrArrayNth(&cache->blocks, pos));
