5c8bab7496859230c1a26e3959888f6e660be227
Mike Gelfand <mikedld@mikedld.com>
2021-10-31 12:32:50 +0300

    Don't use QStringRef (and associated methods)
    While still available, QStringRef has been moved to Qt6::Core5Compat
    module and is not part of Qt6::Core. Related method (e.g.
    QString::midRef) have been removed in Qt 6.

--- qt/FileTreeModel.cc
+++ qt/FileTreeModel.cc
@@ -53,7 +53,7 @@ public:
     {
         int new_slash_index = path_.lastIndexOf(SlashChar, slash_index_);
         token_.truncate(0);
-        token_ += path_.midRef(new_slash_index + 1, slash_index_ - new_slash_index);
+        token_.append(&path_.data()[new_slash_index + 1], slash_index_ - new_slash_index);
         slash_index_ = new_slash_index - 1;
         return token_;
     }
@@ -82,7 +82,7 @@ public:
         }
 
         token_.truncate(0);
-        token_ += path_.midRef(slash_index_, new_slash_index - slash_index_);
+        token_.append(&path_.data()[slash_index_], new_slash_index - slash_index_);
         slash_index_ = new_slash_index + 1;
         return token_;
     }
