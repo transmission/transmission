1757bebc112ac4a25a625e797c473d700135b0ab
Mike Gelfand <mikedld@mikedld.com>
2021-10-31 13:03:03 +0300

    Use QDateTime's {to,from}SecsSinceEpoch instead of {to,from}Time_t
    The latter was deprecated in Qt 5.8 and removed in Qt 6.

--- qt/Prefs.cc
+++ qt/Prefs.cc
@@ -324,7 +324,11 @@ Prefs::Prefs(QString config_dir)
                 auto const value = getValue<time_t>(b);
                 if (value)
                 {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 8, 0)
+                    values_[i].setValue(QDateTime::fromSecsSinceEpoch(*value));
+#else
                     values_[i].setValue(QDateTime::fromTime_t(*value));
+#endif
                 }
             }
             break;
@@ -405,7 +409,11 @@ Prefs::~Prefs()
             break;
 
         case QVariant::DateTime:
+#if QT_VERSION >= QT_VERSION_CHECK(5, 8, 0)
+            dictAdd(&current_settings, key, int64_t{ val.toDateTime().toSecsSinceEpoch() });
+#else
             dictAdd(&current_settings, key, val.toDateTime().toTime_t());
+#endif
             break;
 
         default:
