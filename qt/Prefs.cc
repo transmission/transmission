0fde95e543f6125d130bc6094f4d07eab1cd7503
Mike Gelfand <mikedld@mikedld.com>
2021-10-31 12:56:12 +0300

    Use QStringDecoder instead of QTextCodec
    While still available, QTextCodec has been moved to Qt6::Core5Compat
    module and is not part of Qt6::Core.

--- qt/Prefs.cc
+++ qt/Prefs.cc
@@ -14,7 +14,11 @@
 #include <QDateTime>
 #include <QDir>
 #include <QFile>
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+#include <QStringDecoder>
+#else
 #include <QTextCodec>
+#endif
 
 #include <libtransmission/transmission.h>
 #include <libtransmission/utils.h>
@@ -180,10 +184,20 @@ auto const SortModes = std::array<std::pair<int, std::string_view>, SortMode::NU
 
 bool isValidUtf8(QByteArray const& byteArray)
 {
+#if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
+
+    auto decoder = QStringDecoder(QStringConverter::Utf8, QStringConverter::Flag::Stateless);
+    auto const text = QString(decoder.decode(byteArray));
+    return !decoder.hasError() && !text.contains(QChar::ReplacementCharacter);
+
+#else
+
     auto const* const codec = QTextCodec::codecForName("UTF-8");
     auto state = QTextCodec::ConverterState{};
     auto const text = codec->toUnicode(byteArray.constData(), byteArray.size(), &state);
     return state.invalidChars == 0;
+
+#endif
 }
 
 } // namespace
