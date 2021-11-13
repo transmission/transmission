a8f640c559df07d56b2e5751fcbab98b37288ccc
Charles Kerr <charles@charleskerr.com>
2021-11-13 12:09:14 -0600

    refactor: tr_dirSpace() (#2143)
    * refactor/tr_dirSpace

--- libtransmission/platform-quota.h
+++ libtransmission/platform-quota.h
@@ -8,9 +8,9 @@
 
 #pragma once
 
-#ifndef __TRANSMISSION__
-#error only libtransmission should #include this header.
-#endif
+#include <cstdint>
+#include <string>
+#include <string_view>
 
 /**
  * @addtogroup tr_session Session
@@ -19,9 +19,9 @@
 
 struct tr_device_info
 {
-    char* path;
-    char* device;
-    char* fstype;
+    std::string path;
+    std::string device;
+    std::string fstype;
 };
 
 struct tr_disk_space
@@ -30,13 +30,11 @@ struct tr_disk_space
     int64_t total;
 };
 
-struct tr_device_info* tr_device_info_create(char const* path);
+tr_device_info tr_device_info_create(std::string_view path);
 
 /** Values represents the total space on disk.
     If the disk quota (free space) is enabled and readable, this returns how much is available in the quota.
     Otherwise, it returns how much is available on the disk, or { -1, -1 } on error. */
-struct tr_disk_space tr_device_info_get_disk_space(struct tr_device_info const* info);
-
-void tr_device_info_free(struct tr_device_info* info);
+tr_disk_space tr_device_info_get_disk_space(tr_device_info const& info);
 
 /** @} */
