03ff05bf7bfbf03ebc766c4f999a9410a89684a0
Charles Kerr <charles@charleskerr.com>
2021-11-27 18:45:14 -0600

    fix use-init-statement warning
    Xref: https://sonarcloud.io/project/issues\?id\=transmission_transmission\&issues\=AX0rAQvnfJ-O-YIDS9xG\&open\=AX0rAQvnfJ-O-YIDS9xG

--- libtransmission/torrent-ctor.cc
+++ libtransmission/torrent-ctor.cc
@@ -140,9 +140,7 @@ int tr_ctorSetMetainfoFromFile(tr_ctor* ctor, char const* filename)
     setSourceFile(ctor, filename);
 
     /* if no `name' field was set, then set it from the filename */
-    tr_variant* info = nullptr;
-
-    if (tr_variantDictFindDict(&ctor->metainfo, TR_KEY_info, &info))
+    if (tr_variant* info = nullptr; tr_variantDictFindDict(&ctor->metainfo, TR_KEY_info, &info))
     {
         auto name = std::string_view{};
 
