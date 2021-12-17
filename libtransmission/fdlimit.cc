a3c1ceba80b92967cfbb3492d87e91eab44e9cf1
Charles Kerr <charles@charleskerr.com>
2021-12-16 21:40:02 -0600

    fix: sonarcloud warning - use std::array

--- libtransmission/fdlimit.cc
+++ libtransmission/fdlimit.cc
@@ -7,6 +7,7 @@
  */
 
 #include <algorithm>
+#include <array>
 #include <cerrno>
 #include <cinttypes>
 #include <cstring>
@@ -85,18 +86,17 @@ static bool preallocate_file_full(tr_sys_file_t fd, uint64_t length, tr_error** 
 
     if (!TR_ERROR_IS_ENOSPC(my_error->code))
     {
-        uint8_t buf[4096];
+        auto buf = std::array<uint8_t, 4096>{};
         bool success = true;
 
-        memset(buf, 0, sizeof(buf));
         tr_error_clear(&my_error);
 
         /* fallback: the old-fashioned way */
         while (success && length > 0)
         {
-            uint64_t const thisPass = std::min(length, uint64_t{ sizeof(buf) });
+            uint64_t const thisPass = std::min(length, uint64_t{ std::size(buf) });
             uint64_t bytes_written = 0;
-            success = tr_sys_file_write(fd, buf, thisPass, &bytes_written, &my_error);
+            success = tr_sys_file_write(fd, std::data(buf), thisPass, &bytes_written, &my_error);
             length -= bytes_written;
         }
 
