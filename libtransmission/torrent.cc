4bf613804c5cff769e0228b0bd46afdde3bd96d6
Charles Kerr <charles@charleskerr.com>
2022-02-21 14:20:35 -0600

    fix: bitfield assertion failure on zero-byte file
    Fixes #2667.

--- libtransmission/torrent.cc
+++ libtransmission/torrent.cc
@@ -2002,9 +2002,9 @@ tr_block_span_t tr_torGetFileBlockSpan(tr_torrent const* tor, tr_file_index_t i)
     auto const [begin_byte, end_byte] = tor->fpm_.byteSpan(i);
 
     auto const begin_block = tor->byteLoc(begin_byte).block;
-    if (begin_byte >= end_byte)
+    if (begin_byte >= end_byte) // 0-byte file
     {
-        return { begin_block, begin_block };
+        return { begin_block, begin_block + 1};
     }
 
     auto const final_block = tor->byteLoc(end_byte - 1).block;
