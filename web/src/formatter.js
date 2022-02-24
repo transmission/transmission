4e03e58c66c4b2ad8aaa3cce20cac035f4c0c479
Frank Aurich <1100101@gmail.com>
2022-02-24 09:49:37 +0100

    Fix display of size values in web interface: The web interface rounded large number values down using Math.floor(), causing the web interface to display faulty torrent/download sizes. A torrent of size 1,92 GB (915 pieces @ 2,1 MB) was shown as "1GB". After about half the download was done, the UI showed "1GB of 1GB (55%)", which is rather irritating.
    The fix removes the `Math.floor()` and adds fractions to the larger size values.

--- web/src/formatter.js
+++ web/src/formatter.js
@@ -9,21 +9,23 @@ const number_format = new Intl.NumberFormat(current_locale);
 
 const kilo = 1000;
 const mem_formatters = [
-  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'byte' }),
-  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'kilobyte' }),
-  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'megabyte' }),
-  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'gigabyte' }),
-  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'terabyte' }),
-  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'petabyte' }),
+  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'byte', maximumFractionDigits: 0 }),
+  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'kilobyte', maximumFractionDigits: 0 }),
+  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'megabyte', maximumFractionDigits: 0 }),
+  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'gigabyte', maximumFractionDigits: 2 }),
+  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'terabyte', maximumFractionDigits: 2 }),
+  new Intl.NumberFormat(current_locale, { style: 'unit', unit: 'petabyte', maximumFractionDigits: 2 }),
 ];
 
 const fmt_kBps = new Intl.NumberFormat(current_locale, {
   style: 'unit',
   unit: 'kilobyte-per-second',
+  maximumFractionDigits: 2,
 });
 const fmt_MBps = new Intl.NumberFormat(current_locale, {
   style: 'unit',
   unit: 'megabyte-per-second',
+  maximumFractionDigits: 2,
 });
 
 export const Formatter = {
@@ -51,7 +53,7 @@ export const Formatter = {
     let size = bytes;
     for (const nf of mem_formatters) {
       if (size < kilo) {
-        return nf.format(Math.floor(size));
+        return nf.format(size);
       }
       size /= kilo;
     }
