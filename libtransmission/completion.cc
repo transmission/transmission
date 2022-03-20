09deb56f5648974939e12908b3b769a97095d17e
SweetPPro <sweetppro@users.noreply.github.com>
2022-03-20 14:25:32 +0100

    Update completion.cc
    improved magnet link checking logic

--- libtransmission/completion.cc
+++ libtransmission/completion.cc
@@ -119,7 +119,7 @@ tr_completeness tr_completion::status() const
         return TR_SEED;
     }
 
-    if (size_now_ == sizeWhenDone() && sizeWhenDone() != 0)
+    if (!hasNone() && size_now_ == sizeWhenDone())
     {
         return TR_PARTIAL_SEED;
     }
