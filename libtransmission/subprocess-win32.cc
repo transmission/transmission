2c0e038b1865686612145a66f9eec2fae0105d6d
Charles Kerr <charles@charleskerr.com>
2022-03-31 18:08:05 -0500

    fixup! refactor: remove evbuffer_free_to_str() two-arg variant
    fix: string conversion in subprocess-win32.cc

--- libtransmission/subprocess-win32.cc
+++ libtransmission/subprocess-win32.cc
@@ -38,7 +38,7 @@ static void set_system_error(tr_error** error, DWORD code, std::string_view what
 
     if (char* message = tr_win32_format_message(code); message != nullptr)
     {
-        tr_error_set(error, code, fmt::format(FMT_STRING("{:s} failed: {:s}"), what, message);
+        tr_error_set(error, code, fmt::format(FMT_STRING("{:s} failed: {:s}"), what, message));
         tr_free(message);
     }
     else
