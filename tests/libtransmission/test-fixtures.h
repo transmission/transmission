c3d3f52d3060a36f81dc23ea896033bdbb0614dd
Charles Kerr <charles@charleskerr.com>
2022-04-02 15:23:17 -0500

    refactor: use the nullptr literal

--- tests/libtransmission/test-fixtures.h
+++ tests/libtransmission/test-fixtures.h
@@ -88,17 +88,16 @@ public:
 protected:
     static std::string get_default_parent_dir()
     {
-        auto* path = getenv("TMPDIR");
-        if (path != NULL)
+        if (auto* const path = getenv("TMPDIR"); path != nullptr)
         {
             return path;
         }
 
         tr_error* error = nullptr;
-        path = tr_sys_dir_get_current(&error);
-        if (path != nullptr)
+
+        if (auto* path = tr_sys_dir_get_current(&error); path != nullptr)
         {
-            std::string const ret = path;
+            auto ret = std::string{ path };
             tr_free(path);
             return ret;
         }
