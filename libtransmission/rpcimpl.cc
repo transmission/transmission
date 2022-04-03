2bb771c71e62d9434896be5cf5c7499d5d55ac37
Charles Kerr <charles@charleskerr.com>
2022-04-03 12:25:45 -0500

    fixup! refactor: prefer fmt over tr_snprintf in rpcimpl.cc
    improve error messages

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -1405,7 +1405,9 @@ static void onBlocklistFetched(tr_web::FetchResponse const& web_response)
         // we failed to download the blocklist...
         tr_idle_function_done(
             data,
-            fmt::format(FMT_STRING("blocklist http error: {:s} ({:d})"), tr_webGetResponseStr(status), status));
+            fmt::format(_("Couldn't fetch blocklist: {error} ({error_code})"),
+                fmt::arg("error", tr_webGetResponseStr(status)),
+                fmt::arg("error_code", status)));
         return;
     }
 
@@ -1537,7 +1539,7 @@ static void onMetadataFetched(tr_web::FetchResponse const& web_response)
         tr_idle_function_done(
             data->data,
             fmt::format(
-                _("Couldn't fetch page: {error} ({error_code})"),
+                _("Couldn't fetch torrent: {error} ({error_code})"),
                 fmt::arg("error", tr_webGetResponseStr(status)),
                 fmt::arg("error_code", status)));
     }
