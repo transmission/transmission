06c40296779509195739b9cb2f69200bd5b50039
Gary Elshaw <69029666+GaryElshaw@users.noreply.github.com>
2022-05-08 23:35:07 +1200

    Delete qt directory

--- qt/Typedefs.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <unordered_set>
-
-using torrent_ids_t = std::unordered_set<int>;
--- qt/my-valgrind.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#valgrind --tool=cachegrind ./transmission-qt 2>&1 | tee runlog
-#valgrind --tool=massif --threshold=0.2 ./transmission-qt 2>&1 | tee runlog
-valgrind --tool=memcheck --leak-check=full --leak-resolution=high --num-callers=16 --log-file=x-valgrind --show-reachable=no ./transmission-qt 2>&1 | tee runlog
