2f936c1fcb287c3a0e7a63c1949d5e55fce044dd
Gary Elshaw <69029666+GaryElshaw@users.noreply.github.com>
2022-05-08 23:32:12 +1200

    Delete gtk directory

--- gtk/my-valgrind.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export G_SLICE=always-malloc
-export G_DEBUG=gc-friendly
-export GLIBCXX_FORCE_NEW=1
-#valgrind --tool=cachegrind ./transmission-gtk 2>&1 | tee runlog
-#valgrind --tool=cachegrind ./transmission-gtk -p -g /tmp/transmission-test 2>&1 | tee runlog
-valgrind --tool=memcheck --leak-check=full --leak-resolution=high --num-callers=48 --log-file=x-valgrind --show-reachable=no ./transmission-gtk -p 2>&1 | tee runlog
