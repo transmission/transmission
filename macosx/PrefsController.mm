659969a4361ec480116505ce50e1a1e1c9f1d49a
Antoine Cœur <coeur@gmx.fr>
2022-05-14 10:27:31 +0800

    Fix Display window option incorrectly enabled on start (#3056)

--- macosx/PrefsController.mm
+++ macosx/PrefsController.mm
@@ -157,9 +157,6 @@
                                                      notifyingAbout:VDKQueueNotifyAboutWrite];
         }
 
-        //set special-handling of magnet link add window checkbox
-        [self updateShowAddMagnetWindowField];
-
         //set blocklist scheduler
         [BlocklistScheduler.scheduler updateSchedule];
 
@@ -216,6 +213,9 @@
     self.window.toolbar = toolbar;
 
     [self setPrefView:nil];
+
+    //set special-handling of magnet link add window checkbox
+    [self updateShowAddMagnetWindowField];
 
     //set download folder
     [self.fFolderPopUp selectItemAtIndex:[self.fDefaults boolForKey:@"DownloadLocationConstant"] ? DOWNLOAD_FOLDER : DOWNLOAD_TORRENT];
