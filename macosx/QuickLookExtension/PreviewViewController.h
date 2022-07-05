6d43a327543ca49127fd6ee19767d22381c680a9
Dzmitry Neviadomski <nevack.d@gmail.com>
2022-07-05 06:03:54 +0300

    WIP: QuickLook Extension for macOS 10.15+

---
+++ macosx/QuickLookExtension/PreviewViewController.h
@@ -0,0 +1,9 @@
+// This file Copyright © 2010-2022 Transmission authors and contributors.
+// It may be used under the MIT (SPDX: MIT) license.
+// License text can be found in the licenses/ folder.
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreviewViewController : NSViewController
+
+@end
