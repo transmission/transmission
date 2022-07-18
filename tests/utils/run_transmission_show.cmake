53f1ff284a29f39af655b09a3b65b83e2e471b12
Colin B <lanzaa@1101b.com>
2022-07-18 11:45:13 -0700

    Request en_US.UTF-8 during testing

--- tests/utils/run_transmission_show.cmake
+++ tests/utils/run_transmission_show.cmake
@@ -14,6 +14,8 @@ else()
    message(STATUS "      output_file ${output_file}")
    message(STATUS "   reference_file ${reference_file}")
 
+   # We want UTF-8
+   set(ENV{LANG} "en_US.UTF-8")
    # The app's output includes timestamps, so fake our TZ to ensure
    # the test doesn't depend on the physical TZ of the test machine
    set(ENV{TZ} "UTC")
