7617531562ecc20990606a614074d3856d15838d
Charles Kerr <charles@charleskerr.com>
2022-07-30 10:25:04 -0500

    refactor: update polar/mbed

--- libtransmission/crypto-utils-polarssl.cc
+++ libtransmission/crypto-utils-polarssl.cc
@@ -6,8 +6,10 @@
 #include <mutex>
 
 #if defined(POLARSSL_IS_MBEDTLS)
+// NOLINTBEGIN bugprone-macro-parentheses
 #define API_HEADER(x) <mbedtls/x>
 #define API(x) mbedtls_##x
+// NOLINTEND
 #define API_VERSION_NUMBER MBEDTLS_VERSION_NUMBER
 #else
 #define API_HEADER(x) <polarssl/x>
@@ -129,96 +131,131 @@ static std::recursive_mutex rng_mutex_;
 ****
 ***/
 
-tr_sha1_ctx_t tr_sha1_init()
+namespace
 {
-    auto* const handle = new api_sha1_context{};
 
-#if API_VERSION_NUMBER >= 0x01030800
-    API(sha1_init)(handle);
-#endif
-
-    API(sha1_starts)(handle);
-    return handle;
-}
-
-bool tr_sha1_update(tr_sha1_ctx_t raw_handle, void const* data, size_t data_length)
+class Sha1Impl final : public tr_sha1
 {
-    auto* handle = static_cast<api_sha1_context*>(raw_handle);
-    TR_ASSERT(handle != nullptr);
-
-    if (data_length == 0)
+public:
+    Sha1Impl()
     {
-        return true;
+        clear();
     }
 
-    TR_ASSERT(data != nullptr);
+    ~Sha1Impl() override = default;
 
-    API(sha1_update)(handle, static_cast<unsigned char const*>(data), data_length);
-    return true;
-}
-
-std::optional<tr_sha1_digest_t> tr_sha1_final(tr_sha1_ctx_t raw_handle)
-{
-    auto* handle = static_cast<api_sha1_context*>(raw_handle);
-    TR_ASSERT(handle != nullptr);
-
-    auto digest = tr_sha1_digest_t{};
-    auto* const digest_as_uchar = reinterpret_cast<unsigned char*>(std::data(digest));
-    API(sha1_finish)(handle, digest_as_uchar);
-#if API_VERSION_NUMBER >= 0x01030800
-    API(sha1_free)(handle);
-#endif
-
-    delete handle;
-    return digest;
-}
-
-/***
-****
-***/
-
-tr_sha256_ctx_t tr_sha256_init()
-{
-    auto* const handle = new api_sha256_context{};
-
-#if API_VERSION_NUMBER >= 0x01030800
-    API(sha256_init)(handle);
-#endif
-
-    API(sha256_starts)(handle, 0);
-    return handle;
-}
-
-bool tr_sha256_update(tr_sha256_ctx_t raw_handle, void const* data, size_t data_length)
-{
-    auto* handle = static_cast<api_sha256_context*>(raw_handle);
-    TR_ASSERT(handle != nullptr);
-
-    if (data_length == 0)
+    void clear() override
     {
-        return true;
+#if API_VERSION_NUMBER >= 0x01030800
+        API(sha1_init)(&handle_);
+#endif
+
+#if API_VERSION_NUMBER >= 0x02070000
+        mbedtls_sha1_starts_ret(&handle_);
+#else
+        API(sha1_starts)(&handle_);
+#endif
     }
 
-    TR_ASSERT(data != nullptr);
+    void add(void const* data, size_t data_length) override
+    {
+        if (data_length > 0U)
+        {
+#if API_VERSION_NUMBER >= 0x02070000
+            mbedtls_sha1_update_ret(&handle_, static_cast<unsigned char const*>(data), data_length);
+#else
+            API(sha1_update)(&handle_, static_cast<unsigned char const*>(data), data_length);
+#endif
+        }
+    }
 
-    API(sha256_update)(handle, static_cast<unsigned char const*>(data), data_length);
-    return true;
-}
-
-std::optional<tr_sha256_digest_t> tr_sha256_final(tr_sha256_ctx_t raw_handle)
-{
-    auto* handle = static_cast<api_sha256_context*>(raw_handle);
-    TR_ASSERT(handle != nullptr);
-
-    auto digest = tr_sha256_digest_t{};
-    auto* const digest_as_uchar = reinterpret_cast<unsigned char*>(std::data(digest));
-    API(sha256_finish)(handle, digest_as_uchar);
-#if API_VERSION_NUMBER >= 0x01030800
-    API(sha256_free)(handle);
+    [[nodiscard]] tr_sha1_digest_t final() override
+    {
+        auto digest = tr_sha1_digest_t{};
+        auto* const digest_as_uchar = reinterpret_cast<unsigned char*>(std::data(digest));
+#if API_VERSION_NUMBER >= 0x02070000
+        mbedtls_sha1_finish_ret(&handle_, digest_as_uchar);
+#else
+        API(sha1_finish)(&handle_, digest_as_uchar);
 #endif
 
-    delete handle;
-    return digest;
+#if API_VERSION_NUMBER >= 0x01030800
+        API(sha1_free)(&handle_);
+#endif
+
+        return digest;
+    }
+
+private:
+    mbedtls_sha1_context handle_ = {};
+};
+
+class Sha256Impl final : public tr_sha256
+{
+public:
+    Sha256Impl()
+    {
+        clear();
+    }
+
+    ~Sha256Impl() override = default;
+
+    void clear() override
+    {
+#if API_VERSION_NUMBER >= 0x01030800
+        API(sha256_init)(&handle_);
+#endif
+
+#if API_VERSION_NUMBER >= 0x02070000
+        mbedtls_sha256_starts_ret(&handle_, 0);
+#else
+        API(sha256_starts)(&handle_);
+#endif
+    }
+
+    void add(void const* data, size_t data_length) override
+    {
+        if (data_length > 0U)
+        {
+#if API_VERSION_NUMBER >= 0x02070000
+            mbedtls_sha256_update_ret(&handle_, static_cast<unsigned char const*>(data), data_length);
+#else
+            API(sha256_update)(&handle_, static_cast<unsigned char const*>(data), data_length);
+#endif
+        }
+    }
+
+    [[nodiscard]] tr_sha256_digest_t final() override
+    {
+        auto digest = tr_sha256_digest_t{};
+        auto* const digest_as_uchar = reinterpret_cast<unsigned char*>(std::data(digest));
+#if API_VERSION_NUMBER >= 0x02070000
+        mbedtls_sha256_finish_ret(&handle_, digest_as_uchar);
+#else
+        API(sha256_finish)(&handle_, digest_as_uchar);
+#endif
+
+#if API_VERSION_NUMBER >= 0x01030800
+        API(sha256_free)(&handle_);
+#endif
+
+        return digest;
+    }
+
+private:
+    mbedtls_sha256_context handle_ = {};
+};
+
+} // namespace
+
+std::unique_ptr<tr_sha1> tr_sha1::create()
+{
+    return std::make_unique<Sha1Impl>();
+}
+
+std::unique_ptr<tr_sha256> tr_sha256::create()
+{
+    return std::make_unique<Sha256Impl>();
 }
 
 /***
