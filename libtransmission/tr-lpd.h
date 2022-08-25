4c0f2dd91bb9ed76132cb8c31458e1e590d5060a
Charles Kerr <charles@charleskerr.com>
2022-08-24 20:23:33 -0500

    chore: fix copyright of lpd header file

--- libtransmission/tr-lpd.h
+++ libtransmission/tr-lpd.h
@@ -1,5 +1,6 @@
-// This file Copyright © 2010 Johannes Lieder.
-// It may be used under the MIT (SPDX: MIT) license.
+// This file Copyright © 2022 Mnemosyne LLC.
+// It may be used under GPLv2 (SPDX: GPL-2.0-only), GPLv3 (SPDX: GPL-3.0-only),
+// or any future license endorsed by Mnemosyne LLC.
 // License text can be found in the licenses/ folder.
 
 #pragma once
