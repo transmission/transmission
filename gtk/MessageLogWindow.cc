transmission/transmission

commit 7c5862a5f5db422896a708f77d681687659fa3c1

refactor: remove tr_new, tr_free pt. 1 (#3626)
* refactor: tr_torrentGetMetadataPiece() returns an optional vector

* refactor: use tr_pathbuf in create_temp_path()

* refactor: use tr_pathbuf in win32 create_temp_path()

* refactor: use std::vector in isPeerInteresting()

* refactor: remove tr_new0 from tr_peerMgrPeerStats()

* refactor: remove tr_new0 from rechokeUploads()

* refactor: silence clang nullptr dereference warning

* refactor: make tr_natpmp a C++ class

* refactor: use std::string in tr_log_message

gtk/MessageLogWindow.cc
@@ -208,13 +208,7 @@ void MessageLogWindow::Impl::doSave(Gtk::Window& parent, Glib::ustring const& fi
             auto const it = level_names_.find(node->level);
             auto const* const level_str = it != std::end(level_names_) ? it->second : "???";
 
-            fprintf(
-                fp,
-                "%s\t%s\t%s\t%s\n",
-                date.c_str(),
-                level_str,
-                node->name != nullptr ? node->name : "",
-                node->message != nullptr ? node->message : "");
+            fprintf(fp, "%s\t%s\t%s\t%s\n", date.c_str(), level_str, node->name.c_str(), node->message.c_str());
         }
 
         fclose(fp);
@@ -356,7 +350,7 @@ tr_log_message* addMessages(Glib::RefPtr<Gtk::ListStore> const& store, tr_log_me
 
     for (i = head; i != nullptr && i->next != nullptr; i = i->next)
     {
-        char const* name = i->name != nullptr ? i->name : default_name.c_str();
+        char const* name = !std::empty(i->name) ? i->name.c_str() : default_name.c_str();
 
         auto const row = *store->prepend();
         row[message_log_cols.tr_msg] = i;
@@ -369,9 +363,9 @@ tr_log_message* addMessages(Glib::RefPtr<Gtk::ListStore> const& store, tr_log_me
         {
             auto gstr = gtr_sprintf("%s:%d %s", i->file, i->line, i->message);
 
-            if (i->name != nullptr)
+            if (!std::empty(i->name))
             {
-                gstr += gtr_sprintf(" (%s)", i->name);
+                gstr += gtr_sprintf(" (%s)", i->name.c_str());
             }
 
             g_warning("%s", gstr.c_str());

daemon/daemon.cc
@@ -396,8 +396,7 @@ static void pumpLogMessages(tr_sys_file_t file)
 
     for (tr_log_message const* l = list; l != nullptr; l = l->next)
     {
-        auto const name = std::string_view{ l->name != nullptr ? l->name : "" };
-        printMessage(file, l->level, name, l->message, l->file, l->line);
+        printMessage(file, l->level, l->name, l->message, l->file, l->line);
     }
 
     if (file != TR_BAD_SYS_FILE)

libtransmission/file-posix.cc
@@ -59,7 +59,7 @@
 #include "log.h"
 #include "tr-assert.h"
 #include "tr-strbuf.h"
-#include "utils.h"
+#include "utils.h" // for _(), tr_strerror()
 
 #ifndef O_LARGEFILE
 #define O_LARGEFILE 0
@@ -192,10 +192,10 @@ static bool create_path_require_dir(char const* path, tr_error** error)
 static bool create_path(char const* path_in, int permissions, tr_error** error)
 {
     /* make a temporary copy of path */
-    char* const path = tr_strdup(path_in);
+    auto path = tr_pathbuf{ path_in };
 
     /* walk past the root */
-    char* p = path;
+    char* p = std::data(path);
 
     while (*p == TR_PATH_DELIMITER)
     {
@@ -283,7 +283,6 @@ CLEANUP:
 
     TR_ASSERT(my_error == nullptr);
 
-    tr_free(path);
     return ret;
 }
 

libtransmission/file-win32.cc
@@ -15,6 +15,7 @@
 #include <fmt/format.h>
 
 #include "transmission.h"
+
 #include "crypto-utils.h" /* tr_rand_int() */
 #include "error.h"
 #include "file.h"
@@ -298,8 +299,8 @@ static void create_temp_path(
     TR_ASSERT(path_template != nullptr);
     TR_ASSERT(callback != nullptr);
 
-    char* path = tr_strdup(path_template);
-    size_t path_size = strlen(path);
+    auto path = std::string{ path_template };
+    auto path_size = std::size(path);
 
     TR_ASSERT(path_size > 0);
 
@@ -320,7 +321,7 @@ static void create_temp_path(
 
         tr_error_clear(&my_error);
 
-        (*callback)(path, callback_param, &my_error);
+        (*callback)(path.c_str(), callback_param, &my_error);
 
         if (my_error == nullptr)
         {
@@ -334,10 +335,8 @@ static void create_temp_path(
     }
     else
     {
-        memcpy(path_template, path, path_size);
+        std::copy_n(std::begin(path), path_size, path_template);
     }
-
-    tr_free(path);
 }
 
 bool tr_sys_path_exists(char const* path, tr_error** error)

libtransmission/log.cc
@@ -133,13 +133,13 @@ void logAddImpl(
 
     if (tr_logGetQueueEnabled())
     {
-        auto* const newmsg = tr_new0(tr_log_message, 1);
+        auto* const newmsg = new tr_log_message{};
         newmsg->level = level;
         newmsg->when = tr_time();
-        newmsg->message = tr_strvDup(msg);
+        newmsg->message = msg;
         newmsg->file = file;
         newmsg->line = line;
-        newmsg->name = tr_strvDup(name);
+        newmsg->name = name;
 
         *log_state.queue_tail_ = newmsg;
         log_state.queue_tail_ = &newmsg->next;
@@ -221,9 +221,7 @@ void tr_logFreeQueue(tr_log_message* freeme)
     while (freeme != nullptr)
     {
         auto* const next = freeme->next;
-        tr_free(freeme->message);
-        tr_free(freeme->name);
-        tr_free(freeme);
+        delete freeme;
         freeme = next;
     }
 }

libtransmission/log.h
@@ -8,6 +8,7 @@
 #include <cstddef>
 #include <ctime>
 #include <optional>
+#include <string>
 #include <string_view>
 
 ///
@@ -50,17 +51,17 @@ struct tr_log_message
     tr_log_level level;
 
     // location in the source code
-    char const* file;
-    int line;
+    std::string_view file;
+    size_t line;
 
     // when the message was generated
     time_t when;
 
     // torrent name or code module name associated with the message
-    char* name;
+    std::string name;
 
     // the message
-    char* message;
+    std::string message;
 
     // linked list of messages
     struct tr_log_message* next;

libtransmission/natpmp.cc
@@ -21,13 +21,6 @@
 #include "port-forwarding.h"
 #include "utils.h"
 
-static auto constexpr LifetimeSecs = uint32_t{ 3600 };
-static auto constexpr CommandWaitSecs = time_t{ 8 };
-
-/**
-***
-**/
-
 static void logVal(char const* func, int ret)
 {
     if (ret == NATPMP_TRYAGAIN)
@@ -51,57 +44,33 @@ static void logVal(char const* func, int ret)
     }
 }
 
-struct tr_natpmp* tr_natpmpInit()
+bool tr_natpmp::canSendCommand() const
 {
-    auto* const nat = tr_new0(struct tr_natpmp, 1);
-    nat->state = TR_NATPMP_DISCOVER;
-    nat->public_port.clear();
-    nat->private_port.clear();
-    nat->natpmp.s = TR_BAD_SOCKET; /* socket */
-    return nat;
+    return tr_time() >= command_time_;
 }
 
-void tr_natpmpClose(tr_natpmp* nat)
+void tr_natpmp::setCommandTime()
 {
-    if (nat != nullptr)
+    command_time_ = tr_time() + CommandWaitSecs;
+}
+
+tr_port_forwarding tr_natpmp::pulse(tr_port private_port, bool is_enabled, tr_port* public_port, tr_port* real_private_port)
+{
+    if (is_enabled && state_ == TR_NATPMP_DISCOVER)
     {
-        closenatpmp(&nat->natpmp);
-        tr_free(nat);
-    }
-}
-
-static bool canSendCommand(struct tr_natpmp const* nat)
-{
-    return tr_time() >= nat->command_time;
-}
-
-static void setCommandTime(struct tr_natpmp* nat)
-{
-    nat->command_time = tr_time() + CommandWaitSecs;
-}
-
-tr_port_forwarding tr_natpmpPulse(
-    struct tr_natpmp* nat,
-    tr_port private_port,
-    bool is_enabled,
-    tr_port* public_port,
-    tr_port* real_private_port)
-{
-    if (is_enabled && nat->state == TR_NATPMP_DISCOVER)
-    {
-        int val = initnatpmp(&nat->natpmp, 0, 0);
+        int val = initnatpmp(&natpmp_, 0, 0);
         logVal("initnatpmp", val);
-        val = sendpublicaddressrequest(&nat->natpmp);
+        val = sendpublicaddressrequest(&natpmp_);
         logVal("sendpublicaddressrequest", val);
-        nat->state = val < 0 ? TR_NATPMP_ERR : TR_NATPMP_RECV_PUB;
-        nat->has_discovered = true;
-        setCommandTime(nat);
+        state_ = val < 0 ? TR_NATPMP_ERR : TR_NATPMP_RECV_PUB;
+        has_discovered_ = true;
+        setCommandTime();
     }
 
-    if (nat->state == TR_NATPMP_RECV_PUB && canSendCommand(nat))
+    if (state_ == TR_NATPMP_RECV_PUB && canSendCommand())
     {
         natpmpresp_t response;
-        int const val = readnatpmpresponseorretry(&nat->natpmp, &response);
+        int const val = readnatpmpresponseorretry(&natpmp_, &response);
         logVal("readnatpmpresponseorretry", val);
 
         if (val >= 0)
@@ -109,37 +78,31 @@ tr_port_forwarding tr_natpmpPulse(
             char str[128];
             evutil_inet_ntop(AF_INET, &response.pnu.publicaddress.addr, str, sizeof(str));
             tr_logAddInfo(fmt::format(_("Found public address '{address}'"), fmt::arg("address", str)));
-            nat->state = TR_NATPMP_IDLE;
+            state_ = TR_NATPMP_IDLE;
         }
         else if (val != NATPMP_TRYAGAIN)
         {
-            nat->state = TR_NATPMP_ERR;
+            state_ = TR_NATPMP_ERR;
         }
     }
 
-    if ((nat->state == TR_NATPMP_IDLE || nat->state == TR_NATPMP_ERR) && (nat->is_mapped) &&
-        (!is_enabled || nat->private_port != private_port))
+    if ((state_ == TR_NATPMP_IDLE || state_ == TR_NATPMP_ERR) && is_mapped_ && (!is_enabled || private_port_ != private_port))
     {
-        nat->state = TR_NATPMP_SEND_UNMAP;
+        state_ = TR_NATPMP_SEND_UNMAP;
     }
 
-    if (nat->state == TR_NATPMP_SEND_UNMAP && canSendCommand(nat))
+    if (state_ == TR_NATPMP_SEND_UNMAP && canSendCommand())
     {
-        int const val = sendnewportmappingrequest(
-            &nat->natpmp,
-            NATPMP_PROTOCOL_TCP,
-            nat->private_port.host(),
-            nat->public_port.host(),
-            0);
+        int const val = sendnewportmappingrequest(&natpmp_, NATPMP_PROTOCOL_TCP, private_port_.host(), public_port_.host(), 0);
         logVal("sendnewportmappingrequest", val);
-        nat->state = val < 0 ? TR_NATPMP_ERR : TR_NATPMP_RECV_UNMAP;
-        setCommandTime(nat);
+        state_ = val < 0 ? TR_NATPMP_ERR : TR_NATPMP_RECV_UNMAP;
+        setCommandTime();
     }
 
-    if (nat->state == TR_NATPMP_RECV_UNMAP)
+    if (state_ == TR_NATPMP_RECV_UNMAP)
     {
         natpmpresp_t resp;
-        int const val = readnatpmpresponseorretry(&nat->natpmp, &resp);
+        int const val = readnatpmpresponseorretry(&natpmp_, &resp);
         logVal("readnatpmpresponseorretry", val);
 
         if (val >= 0)
@@ -148,72 +111,72 @@ tr_port_forwarding tr_natpmpPulse(
 
             tr_logAddInfo(fmt::format(_("Port {port} is no longer forwarded"), fmt::arg("port", unmapped_port.host())));
 
-            if (nat->private_port == unmapped_port)
+            if (private_port_ == unmapped_port)
             {
-                nat->private_port.clear();
-                nat->public_port.clear();
-                nat->state = TR_NATPMP_IDLE;
-                nat->is_mapped = false;
+                private_port_.clear();
+                public_port_.clear();
+                state_ = TR_NATPMP_IDLE;
+                is_mapped_ = false;
             }
         }
         else if (val != NATPMP_TRYAGAIN)
         {
-            nat->state = TR_NATPMP_ERR;
+            state_ = TR_NATPMP_ERR;
         }
     }
 
-    if (nat->state == TR_NATPMP_IDLE)
+    if (state_ == TR_NATPMP_IDLE)
     {
-        if (is_enabled && !nat->is_mapped && nat->has_discovered)
+        if (is_enabled && !is_mapped_ && has_discovered_)
         {
-            nat->state = TR_NATPMP_SEND_MAP;
+            state_ = TR_NATPMP_SEND_MAP;
         }
-        else if (nat->is_mapped && tr_time() >= nat->renew_time)
+        else if (is_mapped_ && tr_time() >= renew_time_)
         {
-            nat->state = TR_NATPMP_SEND_MAP;
+            state_ = TR_NATPMP_SEND_MAP;
         }
     }
 
-    if (nat->state == TR_NATPMP_SEND_MAP && canSendCommand(nat))
+    if (state_ == TR_NATPMP_SEND_MAP && canSendCommand())
     {
         int const val = sendnewportmappingrequest(
-            &nat->natpmp,
+            &natpmp_,
             NATPMP_PROTOCOL_TCP,
             private_port.host(),
             private_port.host(),
             LifetimeSecs);
         logVal("sendnewportmappingrequest", val);
-        nat->state = val < 0 ? TR_NATPMP_ERR : TR_NATPMP_RECV_MAP;
-        setCommandTime(nat);
+        state_ = val < 0 ? TR_NATPMP_ERR : TR_NATPMP_RECV_MAP;
+        setCommandTime();
     }
 
-    if (nat->state == TR_NATPMP_RECV_MAP)
+    if (state_ == TR_NATPMP_RECV_MAP)
     {
         natpmpresp_t resp;
-        int const val = readnatpmpresponseorretry(&nat->natpmp, &resp);
+        int const val = readnatpmpresponseorretry(&natpmp_, &resp);
         logVal("readnatpmpresponseorretry", val);
 
         if (val >= 0)
         {
-            nat->state = TR_NATPMP_IDLE;
-            nat->is_mapped = true;
-            nat->renew_time = tr_time() + (resp.pnu.newportmapping.lifetime / 2);
-            nat->private_port = tr_port::fromHost(resp.pnu.newportmapping.privateport);
-            nat->public_port = tr_port::fromHost(resp.pnu.newportmapping.mappedpublicport);
-            tr_logAddInfo(fmt::format(_("Port {port} forwarded successfully"), fmt::arg("port", nat->private_port.host())));
+            state_ = TR_NATPMP_IDLE;
+            is_mapped_ = true;
+            renew_time_ = tr_time() + (resp.pnu.newportmapping.lifetime / 2);
+            private_port_ = tr_port::fromHost(resp.pnu.newportmapping.privateport);
+            public_port_ = tr_port::fromHost(resp.pnu.newportmapping.mappedpublicport);
+            tr_logAddInfo(fmt::format(_("Port {port} forwarded successfully"), fmt::arg("port", private_port_.host())));
         }
         else if (val != NATPMP_TRYAGAIN)
         {
-            nat->state = TR_NATPMP_ERR;
+            state_ = TR_NATPMP_ERR;
         }
     }
 
-    switch (nat->state)
+    switch (state_)
     {
     case TR_NATPMP_IDLE:
-        *public_port = nat->public_port;
-        *real_private_port = nat->private_port;
-        return nat->is_mapped ? TR_PORT_MAPPED : TR_PORT_UNMAPPED;
+        *public_port = public_port_;
+        *real_private_port = private_port_;
+        return is_mapped_ ? TR_PORT_MAPPED : TR_PORT_UNMAPPED;
 
     case TR_NATPMP_DISCOVER:
         return TR_PORT_UNMAPPED;

libtransmission/natpmp_local.h
@@ -9,46 +9,62 @@
 #error only libtransmission should #include this header.
 #endif
 
-/**
- * @addtogroup port_forwarding Port Forwarding
- * @{
- */
-
 #include <ctime> // time_t
+
+#include "transmission.h" // tr_port_forwarding
 
 #include "natpmp.h"
 #include "net.h" // tr_port
 
-enum tr_natpmp_state
+class tr_natpmp
 {
-    TR_NATPMP_IDLE,
-    TR_NATPMP_ERR,
-    TR_NATPMP_DISCOVER,
-    TR_NATPMP_RECV_PUB,
-    TR_NATPMP_SEND_MAP,
-    TR_NATPMP_RECV_MAP,
-    TR_NATPMP_SEND_UNMAP,
-    TR_NATPMP_RECV_UNMAP
+public:
+    tr_natpmp()
+    {
+        natpmp_.s = TR_BAD_SOCKET; /* socket */
+    }
+
+    ~tr_natpmp()
+    {
+        closenatpmp(&natpmp_);
+    }
+
+    [[nodiscard]] constexpr auto renewTime() const noexcept
+    {
+        return renew_time_;
+    }
+
+    tr_port_forwarding pulse(tr_port port, bool is_enabled, tr_port* public_port, tr_port* real_private_port);
+
+private:
+    enum tr_natpmp_state
+    {
+        TR_NATPMP_IDLE,
+        TR_NATPMP_ERR,
+        TR_NATPMP_DISCOVER,
+        TR_NATPMP_RECV_PUB,
+        TR_NATPMP_SEND_MAP,
+        TR_NATPMP_RECV_MAP,
+        TR_NATPMP_SEND_UNMAP,
+        TR_NATPMP_RECV_UNMAP
+    };
+
+    static constexpr auto LifetimeSecs = uint32_t{ 3600 };
+    static constexpr auto CommandWaitSecs = time_t{ 8 };
+
+    [[nodiscard]] bool canSendCommand() const;
+
+    void setCommandTime();
+
+    natpmp_t natpmp_;
+
+    tr_port public_port_ = {};
+    tr_port private_port_ = {};
+
+    time_t renew_time_ = 0;
+    time_t command_time_ = 0;
+    tr_natpmp_state state_ = TR_NATPMP_DISCOVER;
+
+    bool has_discovered_ = false;
+    bool is_mapped_ = false;
 };
-
-struct tr_natpmp
-{
-    bool has_discovered;
-    bool is_mapped;
-
-    tr_port public_port;
-    tr_port private_port;
-
-    time_t renew_time;
-    time_t command_time;
-    tr_natpmp_state state;
-    natpmp_t natpmp;
-};
-
-tr_natpmp* tr_natpmpInit(void);
-
-void tr_natpmpClose(tr_natpmp*);
-
-tr_port_forwarding tr_natpmpPulse(tr_natpmp*, tr_port port, bool is_enabled, tr_port* public_port, tr_port* real_private_port);
-
-/* @} */

commit dffd822b84d9c63d28935c668e18fb5b044cc06c

Fix GTK client crash opening message log containing errors
`Glib::ustring::sprintf()` doesn't support formatting `std::string_view`.
Use `fmt::format()` instead.

Broken in #3626 (7c5862a5f5).

gtk/MessageLogWindow.cc
@@ -362,11 +362,11 @@ tr_log_message* addMessages(Glib::RefPtr<Gtk::ListStore> const& store, tr_log_me
         /* if it's an error message, dump it to the terminal too */
         if (i->level == TR_LOG_ERROR)
         {
-            auto gstr = gtr_sprintf("%s:%d %s", i->file, i->line, i->message);
+            auto gstr = fmt::format("{}:{} {}", i->file, i->line, i->message);
 
             if (!std::empty(i->name))
             {
-                gstr += gtr_sprintf(" (%s)", i->name.c_str());
+                gstr += fmt::format(" ({})", i->name.c_str());
             }
 
             g_warning("%s", gstr.c_str());