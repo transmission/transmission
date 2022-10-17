a39811771efa34dff1003d1e427a8902b155c7b1
Charles Kerr <charles@charleskerr.com>
2022-10-17 01:02:51 -0500

    fixup! refactor: add libtransmission::Dns to tr_session

--- libtransmission/session.cc
+++ libtransmission/session.cc
@@ -2810,8 +2810,8 @@ tr_session::tr_session(std::string_view config_dir)
                        // if zero, active requests will be aborted
                        evdns_base_free(dns, 0);
                    } }
-    , timer_maker_{ std::make_unique<libtransmission::EvTimerMaker>(event_base_) }
-    , dns_{ std::make_unique<libtransmission::EvDns>(event_base_) }
+    , timer_maker_{ std::make_unique<libtransmission::EvTimerMaker>(eventBase()) }
+    , dns_{ std::make_unique<libtransmission::EvDns>(eventBase()) }
     , session_id_{ tr_time }
 {
     now_timer_ = timerMaker().create([this]() { onNowTimer(); });
