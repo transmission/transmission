9d4ecb6e5478d12a208fa127b26c20463a093a47
Charles Kerr <charles@charleskerr.com>
2022-11-15 08:35:00 -0600

    fix: avoid "using namespace" in header

--- daemon/daemon.h
+++ daemon/daemon.h
@@ -15,8 +15,6 @@
 #include <libtransmission/file.h>
 #include <libtransmission/log.h>
 
-using namespace std::literals;
-
 class tr_daemon
 {
 public:
@@ -53,8 +51,8 @@ private:
     char const* log_file_name_ = nullptr;
     struct event_base* ev_base_ = nullptr;
     tr_sys_file_t logfile_ = TR_BAD_SYS_FILE;
-    tr_quark key_pidfile_ = tr_quark_new("pidfile"sv);
-    tr_quark key_watch_dir_force_generic_ = tr_quark_new("watch-dir-force-generic"sv);
+    tr_quark key_pidfile_ = tr_quark_new("pidfile");
+    tr_quark key_watch_dir_force_generic_ = tr_quark_new("watch-dir-force-generic");
 
     bool parse_args(int argc, char const** argv, bool* dump_settings, bool* foreground, int* exit_code);
     bool reopen_log_file(char const* filename);
