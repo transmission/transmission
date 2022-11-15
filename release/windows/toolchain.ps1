297bd3390fbe6749c86e5370008dde65b3153e07
Mike Gelfand <mikedld@mikedld.com>
2022-11-15 22:30:07 +0000

    Add Windows configuration to GHA sanity builds

--- release/windows/toolchain.ps1
+++ release/windows/toolchain.ps1
@@ -12,12 +12,15 @@ $global:LinkerFlags = @(
     '/PDBALTPATH:%_PDB%'
 )
 
-$global:VsInstallPrefix = "$env:VSINSTALLDIR"
-if (-not (Test-Path $global:VsInstallPrefix)) {
-    $global:VsInstallPrefix = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio' 2022 Community
-}
-if (-not (Test-Path $global:VsInstallPrefix)) {
-    $global:VsInstallPrefix = Join-Path ${env:ProgramFiles} 'Microsoft Visual Studio' 2022 Community
+foreach ($VsType in @('Enterprise', 'Professional', 'Community', 'BuildTools')) {
+    $global:VsInstallPrefix = Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio' 2022 $VsType
+    if (Test-Path $global:VsInstallPrefix) {
+        break
+    }
+    $global:VsInstallPrefix = Join-Path ${env:ProgramFiles} 'Microsoft Visual Studio' 2022 $VsType
+    if (Test-Path $global:VsInstallPrefix) {
+        break
+    }
 }
 $global:VsVersion = ((& (Join-Path ${env:ProgramFiles(x86)} 'Microsoft Visual Studio' Installer vswhere) -Property catalog_productSemanticVersion -Path $VsInstallPrefix) -Split '[+]')[0]
 $global:VcVarsScript = Join-Path $VsInstallPrefix VC Auxiliary Build vcvarsall.bat
