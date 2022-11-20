9c29881e0449baf032eff369d25de0875197222e
Cœur <coeur@gmx.fr>
2022-11-20 12:29:26 +0800

    code review

--- libtransmission/crypto-utils-ccrypto.cc
+++ libtransmission/crypto-utils-ccrypto.cc
@@ -119,18 +119,14 @@ public:
 
     void add(void const* data, size_t data_length) override
     {
+        static auto constexpr Max = static_cast<size_t>(std::numeric_limits<CC_LONG>::max());
         auto const* sha_data = static_cast<uint8_t const*>(data);
-#if UINTPTR_MAX > UINT32_MAX
-        while (data_length > UINT32_MAX)
+        while (data_length > 0)
         {
-            CC_SHA1_Update(&handle_, sha_data, UINT32_MAX);
-            data_length -= UINT32_MAX;
-            sha_data += UINT32_MAX;
-        }
-#endif
-        if (data_length > 0U)
-        {
-            CC_SHA1_Update(&handle_, sha_data, static_cast<CC_LONG>(data_length));
+            auto const n_bytes = static_cast<CC_LONG>(std::min(data_length, Max));
+            CC_SHA1_Update(&handle_, sha_data, n_bytes);
+            data_length -= n_bytes;
+            sha_data += n_bytes;
         }
     }
 
@@ -163,18 +159,14 @@ public:
 
     void add(void const* data, size_t data_length) override
     {
+        static auto constexpr Max = static_cast<size_t>(std::numeric_limits<CC_LONG>::max());
         auto const* sha_data = static_cast<uint8_t const*>(data);
-#if UINTPTR_MAX > UINT32_MAX
-        while (data_length > UINT32_MAX)
+        while (data_length > 0)
         {
-            CC_SHA256_Update(&handle_, sha_data, UINT32_MAX);
-            data_length -= UINT32_MAX;
-            sha_data += UINT32_MAX;
-        }
-#endif
-        if (data_length > 0U)
-        {
-            CC_SHA256_Update(&handle_, sha_data, static_cast<CC_LONG>(data_length));
+            auto const n_bytes = static_cast<CC_LONG>(std::min(data_length, Max));
+            CC_SHA256_Update(&handle_, sha_data, n_bytes);
+            data_length -= n_bytes;
+            sha_data += n_bytes;
         }
     }
 
