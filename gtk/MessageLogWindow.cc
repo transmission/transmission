4a0d30f191e5d3ef71bdf7862272130e94e4ea80
Charles Kerr <charles@charleskerr.com>
2022-11-25 20:34:32 -0600

    fixup! Merge branch 'fix/4181-message-log-window-does-not-restore-level-selection' of github.com:transmission/transmission into fix/4181-message-log-window-does-not-restore-level-selection
    fix: another oops :P

--- gtk/MessageLogWindow.cc
+++ gtk/MessageLogWindow.cc
@@ -462,11 +462,13 @@ MessageLogWindow::Impl::Impl(
           sigc::mem_fun(*this, &Impl::onRefresh),
           SECONDARY_WINDOW_REFRESH_INTERVAL_SECONDS))
     , level_names_{
-        { { TR_LOG_CRITICAL, C_("Logging level", "Critical") },
-          { TR_LOG_ERROR, C_("Logging level", "Error") },
-          { TR_LOG_WARN, C_("Logging level", "Warning") },
-          { TR_LOG_INFO, C_("Logging level", "Information") },
-          { TR_LOG_DEBUG, C_("Logging level", "Debug") } },
+        {
+            { TR_LOG_CRITICAL, C_("Logging level", "Critical") },
+            { TR_LOG_ERROR, C_("Logging level", "Error") },
+            { TR_LOG_WARN, C_("Logging level", "Warning") },
+            { TR_LOG_INFO, C_("Logging level", "Information") },
+            { TR_LOG_DEBUG, C_("Logging level", "Debug") },
+        },
     }
 {
     /**
