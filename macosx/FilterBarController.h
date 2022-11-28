9a6fbc9cedcd0a9d9d250c692a8afcb0c7b5b65a
Daniil Subbotin <robots@subdan.ru>
2022-11-28 07:31:26 +0300

    Fix deleting previously selected torrent when trying to clear search field using Command + Delete (#4245)
    * Fix deleting previously selected torrent when trying to clear search field using Command + Delete
    
    Fixes #3599
    
    * Fixes after code review
    
    * Fixes after code review
    
    * chore: make clang-format happy
    
    Co-authored-by: Daniil Subbotin <d.subbotin@opends.tech>

--- macosx/FilterBarController.h
+++ macosx/FilterBarController.h
@@ -33,6 +33,7 @@ extern const NSInteger kGroupFilterAllTag;
 - (IBAction)setGroupFilter:(id)sender;
 - (void)reset:(BOOL)updateUI;
 - (void)focusSearchField;
+- (BOOL)isFocused;
 
 - (void)setCountAll:(NSUInteger)all
              active:(NSUInteger)active
