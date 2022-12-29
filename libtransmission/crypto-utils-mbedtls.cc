10523b29f71eb5f5256b882a224cb5802c557094
Mike Gelfand <mikedld@mikedld.com>
2022-12-29 20:55:18 +0000

    Drop support for PolarSSL (keep MbedTLS)

--- libtransmission/crypto-utils-mbedtls.cc
+++ libtransmission/crypto-utils-mbedtls.cc
@@ -6,24 +6,12 @@
 #include <memory>
 #include <mutex>
 
-#if defined(POLARSSL_IS_MBEDTLS)
-// NOLINTBEGIN bugprone-macro-parentheses
-#define API_HEADER(x) <mbedtls/x>
-#define API(x) mbedtls_##x
-// NOLINTEND
-#define API_VERSION_NUMBER MBEDTLS_VERSION_NUMBER
-#else
-#define API_HEADER(x) <polarssl/x>
-#define API(x) x
-#define API_VERSION_NUMBER POLARSSL_VERSION_NUMBER
-#endif
-
-#include API_HEADER(base64.h)
-#include API_HEADER(ctr_drbg.h)
-#include API_HEADER(error.h)
-#include API_HEADER(sha1.h)
-#include API_HEADER(sha256.h)
-#include API_HEADER(version.h)
+#include <mbedtls/base64.h>
+#include <mbedtls/ctr_drbg.h>
+#include <mbedtls/error.h>
+#include <mbedtls/sha1.h>
+#include <mbedtls/sha256.h>
+#include <mbedtls/version.h>
 
 #include <fmt/core.h>
 
@@ -40,23 +28,12 @@
 ****
 ***/
 
-using api_ctr_drbg_context = API(ctr_drbg_context);
-using api_sha1_context = API(sha1_context);
-using api_sha256_context = API(sha256_context);
-
-static void log_polarssl_error(int error_code, char const* file, int line)
+static void log_mbedtls_error(int error_code, char const* file, int line)
 {
     if (tr_logLevelIsActive(TR_LOG_ERROR))
     {
         char error_message[256];
-
-#if defined(POLARSSL_IS_MBEDTLS)
         mbedtls_strerror(error_code, error_message, sizeof(error_message));
-#elif API_VERSION_NUMBER >= 0x01030000
-        polarssl_strerror(error_code, error_message, sizeof(error_message));
-#else
-        error_strerror(error_code, error_message, sizeof(error_message));
-#endif
 
         tr_logAddMessage(
             file,
@@ -64,28 +41,28 @@ static void log_polarssl_error(int error_code, char const* file, int line)
             TR_LOG_ERROR,
             fmt::format(
                 _("{crypto_library} error: {error} ({error_code})"),
-                fmt::arg("crypto_library", "PolarSSL/MbedTLS"),
+                fmt::arg("crypto_library", "MbedTLS"),
                 fmt::arg("error", error_message),
                 fmt::arg("error_code", error_code)));
     }
 }
 
-#define log_error(error_code) log_polarssl_error((error_code), __FILE__, __LINE__)
+#define log_error(error_code) log_mbedtls_error((error_code), __FILE__, __LINE__)
 
-static bool check_polarssl_result(int result, int expected_result, char const* file, int line)
+static bool check_mbedtls_result(int result, int expected_result, char const* file, int line)
 {
     bool const ret = result == expected_result;
 
     if (!ret)
     {
-        log_polarssl_error(result, file, line);
+        log_mbedtls_error(result, file, line);
     }
 
     return ret;
 }
 
-#define check_result(result) check_polarssl_result((result), 0, __FILE__, __LINE__)
-#define check_result_eq(result, x_result) check_polarssl_result((result), (x_result), __FILE__, __LINE__)
+#define check_result(result) check_mbedtls_result((result), 0, __FILE__, __LINE__)
+#define check_result_eq(result, x_result) check_mbedtls_result((result), (x_result), __FILE__, __LINE__)
 
 /***
 ****
@@ -98,20 +75,20 @@ static int my_rand(void* /*context*/, unsigned char* buffer, size_t buffer_size)
     return 0;
 }
 
-static api_ctr_drbg_context* get_rng()
+static mbedtls_ctr_drbg_context* get_rng()
 {
-    static api_ctr_drbg_context rng;
+    static mbedtls_ctr_drbg_context rng;
     static bool rng_initialized = false;
 
     if (!rng_initialized)
     {
-#if API_VERSION_NUMBER >= 0x02000000
-        API(ctr_drbg_init)(&rng);
+#if MBEDTLS_VERSION_NUMBER >= 0x02000000
+        mbedtls_ctr_drbg_init(&rng);
 
-        if (!check_result(API(ctr_drbg_seed)(&rng, my_rand, nullptr, nullptr, 0)))
+        if (!check_result(mbedtls_ctr_drbg_seed(&rng, my_rand, nullptr, nullptr, 0)))
 #else
 
-        if (!check_result(API(ctr_drbg_init)(&rng, my_rand, nullptr, nullptr, 0)))
+        if (!check_result(mbedtls_ctr_drbg_init(&rng, my_rand, nullptr, nullptr, 0)))
 #endif
         {
             return nullptr;
@@ -144,14 +121,12 @@ public:
 
     void clear() override
     {
-#if API_VERSION_NUMBER >= 0x01030800
-        API(sha1_init)(&handle_);
-#endif
+        mbedtls_sha1_init(&handle_);
 
-#if API_VERSION_NUMBER >= 0x02070000
+#if MBEDTLS_VERSION_NUMBER >= 0x02070000
         mbedtls_sha1_starts_ret(&handle_);
 #else
-        API(sha1_starts)(&handle_);
+        mbedtls_sha1_starts(&handle_);
 #endif
     }
 
@@ -159,10 +134,10 @@ public:
     {
         if (data_length > 0U)
         {
-#if API_VERSION_NUMBER >= 0x02070000
+#if MBEDTLS_VERSION_NUMBER >= 0x02070000
             mbedtls_sha1_update_ret(&handle_, static_cast<unsigned char const*>(data), data_length);
 #else
-            API(sha1_update)(&handle_, static_cast<unsigned char const*>(data), data_length);
+            mbedtls_sha1_update(&handle_, static_cast<unsigned char const*>(data), data_length);
 #endif
         }
     }
@@ -171,16 +146,13 @@ public:
     {
         auto digest = tr_sha1_digest_t{};
         auto* const digest_as_uchar = reinterpret_cast<unsigned char*>(std::data(digest));
-#if API_VERSION_NUMBER >= 0x02070000
+#if MBEDTLS_VERSION_NUMBER >= 0x02070000
         mbedtls_sha1_finish_ret(&handle_, digest_as_uchar);
 #else
-        API(sha1_finish)(&handle_, digest_as_uchar);
+        mbedtls_sha1_finish(&handle_, digest_as_uchar);
 #endif
 
-#if API_VERSION_NUMBER >= 0x01030800
-        API(sha1_free)(&handle_);
-#endif
-
+        mbedtls_sha1_free(&handle_);
         return digest;
     }
 
@@ -200,14 +172,12 @@ public:
 
     void clear() override
     {
-#if API_VERSION_NUMBER >= 0x01030800
-        API(sha256_init)(&handle_);
-#endif
+        mbedtls_sha256_init(&handle_);
 
-#if API_VERSION_NUMBER >= 0x02070000
+#if MBEDTLS_VERSION_NUMBER >= 0x02070000
         mbedtls_sha256_starts_ret(&handle_, 0);
 #else
-        API(sha256_starts)(&handle_);
+        mbedtls_sha256_starts(&handle_);
 #endif
     }
 
@@ -215,10 +185,10 @@ public:
     {
         if (data_length > 0U)
         {
-#if API_VERSION_NUMBER >= 0x02070000
+#if MBEDTLS_VERSION_NUMBER >= 0x02070000
             mbedtls_sha256_update_ret(&handle_, static_cast<unsigned char const*>(data), data_length);
 #else
-            API(sha256_update)(&handle_, static_cast<unsigned char const*>(data), data_length);
+            mbedtls_sha256_update(&handle_, static_cast<unsigned char const*>(data), data_length);
 #endif
         }
     }
@@ -227,16 +197,13 @@ public:
     {
         auto digest = tr_sha256_digest_t{};
         auto* const digest_as_uchar = reinterpret_cast<unsigned char*>(std::data(digest));
-#if API_VERSION_NUMBER >= 0x02070000
+#if MBEDTLS_VERSION_NUMBER >= 0x02070000
         mbedtls_sha256_finish_ret(&handle_, digest_as_uchar);
 #else
-        API(sha256_finish)(&handle_, digest_as_uchar);
+        mbedtls_sha256_finish(&handle_, digest_as_uchar);
 #endif
 
-#if API_VERSION_NUMBER >= 0x01030800
-        API(sha256_free)(&handle_);
-#endif
-
+        mbedtls_sha256_free(&handle_);
         return digest;
     }
 
@@ -270,5 +237,5 @@ bool tr_rand_buffer_crypto(void* buffer, size_t length)
     TR_ASSERT(buffer != nullptr);
 
     auto const lock = std::lock_guard(rng_mutex_);
-    return check_result(API(ctr_drbg_random)(get_rng(), static_cast<unsigned char*>(buffer), length));
+    return check_result(mbedtls_ctr_drbg_random(get_rng(), static_cast<unsigned char*>(buffer), length));
 }
