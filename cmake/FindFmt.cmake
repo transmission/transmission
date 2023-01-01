ffbd9377d99a7eb4404bc99a66afe988311637a2
Mike Gelfand <mikedld@mikedld.com>
2023-01-01 22:16:45 +0000

    Use imported CMake target for fmtlib

--- cmake/FindFmt.cmake
+++ cmake/FindFmt.cmake
@@ -1,1 +1,10 @@
-set(LIBFMT_INCLUDE_DIRS ${CMAKE_SOURCE_DIR}/third-party/fmt/include)
+add_library(fmt::fmt-header-only INTERFACE IMPORTED)
+
+target_include_directories(fmt::fmt-header-only
+    INTERFACE
+        ${CMAKE_CURRENT_LIST_DIR}/../third-party/fmt/include)
+
+target_compile_definitions(fmt::fmt-header-only
+    INTERFACE
+        FMT_EXCEPTIONS=0
+        FMT_HEADER_ONLY=1)
