0e0f83c87228c0207337f913af916707b91648a9
Charles Kerr <charles@charleskerr.com>
2023-01-03 14:35:51 -0600

    fix: -Wredundant-decls gcc warning

--- libtransmission/utils-ev.h
+++ libtransmission/utils-ev.h
@@ -11,72 +11,38 @@
 
 #include <memory>
 
-extern "C"
-{
-    struct evbuffer;
-    struct event;
-    struct event_base;
-    struct evhttp;
-
-    void evbuffer_free(struct evbuffer*);
-    void event_base_free(struct event_base*);
-    int event_del(struct event*);
-    void event_free(struct event*);
-    void evhttp_free(struct evhttp*);
-}
+struct evbuffer;
+struct event;
+struct event_base;
+struct evhttp;
 
 namespace libtransmission::evhelpers
 {
 
 struct BufferDeleter
 {
-    void operator()(struct evbuffer* buf) const noexcept
-    {
-        if (buf != nullptr)
-        {
-            evbuffer_free(buf);
-        }
-    }
+    void operator()(struct evbuffer* buf) const noexcept;
 };
 
 using evbuffer_unique_ptr = std::unique_ptr<struct evbuffer, BufferDeleter>;
 
 struct EventBaseDeleter
 {
-    void operator()(struct event_base* evbase) const noexcept
-    {
-        if (evbase != nullptr)
-        {
-            event_base_free(evbase);
-        }
-    }
+    void operator()(struct event_base* evbase) const noexcept;
 };
 
 using evbase_unique_ptr = std::unique_ptr<struct event_base, EventBaseDeleter>;
 
 struct EventDeleter
 {
-    void operator()(struct event* event) const
-    {
-        if (event != nullptr)
-        {
-            event_del(event);
-            event_free(event);
-        }
-    }
+    void operator()(struct event* event) const;
 };
 
 using event_unique_ptr = std::unique_ptr<struct event, EventDeleter>;
 
 struct EvhttpDeleter
 {
-    void operator()(struct evhttp* evh) const noexcept
-    {
-        if (evh != nullptr)
-        {
-            evhttp_free(evh);
-        }
-    }
+    void operator()(struct evhttp* evh) const noexcept;
 };
 
 using evhttp_unique_ptr = std::unique_ptr<struct evhttp, EvhttpDeleter>;
