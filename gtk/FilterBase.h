d33832b3c35b3645d94f51b372324b34b2603431
Mike Gelfand <mikedld@mikedld.com>
2023-01-06 00:50:26 +0000

    Refactor sorting and filtering compatibility code
    Factor out parts of `TorrentFilter` and `TorrentSorter` classes into
    reusable `FilterBase<>` and `SorterBase<>` templates.
    
    Factor out filter and sort models setup from `FilterBar` and `Session`
    classes into reusable `FilterListModel<>` and `SortListModel<>`
    templates.

---
+++ gtk/FilterBase.h
@@ -0,0 +1,53 @@
+// This file Copyright © 2022-2023 Mnemosyne LLC.
+// It may be used under GPLv2 (SPDX: GPL-2.0-only), GPLv3 (SPDX: GPL-3.0-only),
+// or any future license endorsed by Mnemosyne LLC.
+// License text can be found in the licenses/ folder.
+
+#pragma once
+
+#include "GtkCompat.h"
+
+#if GTKMM_CHECK_VERSION(4, 0, 0)
+#include <gtkmm/filter.h>
+#else
+#include <glibmm/object.h>
+#endif
+
+template<typename T>
+class FilterBase : public IF_GTKMM4(Gtk::Filter, Glib::Object)
+{
+public:
+#if !GTKMM_CHECK_VERSION(4, 0, 0)
+    enum class Change{
+        DIFFERENT,
+        LESS_STRICT,
+        MORE_STRICT,
+    };
+#endif
+
+public:
+    ~FilterBase() override = default;
+
+    virtual bool match(T const& item) const = 0;
+
+    virtual bool matches_all() const;
+    virtual bool matches_none() const;
+
+#if !GTKMM_CHECK_VERSION(4, 0, 0)
+    sigc::signal<void(Change)>& signal_changed();
+#endif
+
+protected:
+#if GTKMM_CHECK_VERSION(4, 0, 0)
+    // Gtk::Filter
+    bool match_vfunc(Glib::RefPtr<Glib::ObjectBase> const& object) override;
+    Match get_strictness_vfunc() override;
+#else
+    void changed(Change change);
+#endif
+
+private:
+#if !GTKMM_CHECK_VERSION(4, 0, 0)
+    sigc::signal<void(Change)> signal_changed_;
+#endif
+};
--- gtk/TorrentSorter.h
+++ gtk/TorrentSorter.h
@@ -1,55 +1,29 @@
-// This file Copyright © 2022 Mnemosyne LLC.
+// This file Copyright © 2022-2023 Mnemosyne LLC.
 // It may be used under GPLv2 (SPDX: GPL-2.0-only), GPLv3 (SPDX: GPL-3.0-only),
 // or any future license endorsed by Mnemosyne LLC.
 // License text can be found in the licenses/ folder.
 
 #pragma once
 
-#include "GtkCompat.h"
+#include "SorterBase.h"
 #include "Torrent.h"
 
 #include <glibmm/refptr.h>
 
-#if GTKMM_CHECK_VERSION(4, 0, 0)
-#include <gtkmm/sorter.h>
-#else
-#include <glibmm/object.h>
-#endif
-
-class TorrentSorter : public IF_GTKMM4(Gtk::Sorter, Glib::Object)
+class TorrentSorter : public SorterBase<Torrent>
 {
     using CompareFunc = int (*)(Torrent const&, Torrent const&);
-
-#if !GTKMM_CHECK_VERSION(4, 0, 0)
-    enum class Change{
-        DIFFERENT,
-        INVERTED,
-        LESS_STRICT,
-        MORE_STRICT,
-    };
-#endif
 
 public:
     void set_mode(std::string_view mode);
     void set_reversed(bool is_reversed);
 
-    int compare(Torrent const& lhs, Torrent const& rhs) const;
+    // SorterBase<Torrent>
+    int compare(Torrent const& lhs, Torrent const& rhs) const override;
 
     void update(Torrent::ChangeFlags changes);
 
-#if !GTKMM_CHECK_VERSION(4, 0, 0)
-    sigc::signal<void()>& signal_changed();
-#endif
-
     static Glib::RefPtr<TorrentSorter> create();
-
-protected:
-#if GTKMM_CHECK_VERSION(4, 0, 0)
-    Gtk::Ordering compare_vfunc(gpointer lhs, gpointer rhs) override;
-    Order get_order_vfunc() override;
-#else
-    void changed(Change change);
-#endif
 
 private:
     TorrentSorter();
@@ -57,8 +31,4 @@ private:
 private:
     CompareFunc compare_func_ = nullptr;
     bool is_reversed_ = false;
-
-#if !GTKMM_CHECK_VERSION(4, 0, 0)
-    sigc::signal<void()> signal_changed_;
-#endif
 };
