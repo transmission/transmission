94239232e4973e294ce5018a567c6ec35da0ffb1
Charles Kerr <charles@charleskerr.com>
2023-01-27 11:53:40 -0600

    chore: inline tr_bitfield::freeArray()
    chore: inline tr_bitfield::rebuildTrueCount()

--- libtransmission/bitfield.cc
+++ libtransmission/bitfield.cc
@@ -228,11 +228,6 @@ bool tr_bitfield::ensureNthBitAlloced(size_t nth)
     return true;
 }
 
-void tr_bitfield::freeArray() noexcept
-{
-    flags_ = std::vector<uint8_t>{};
-}
-
 void tr_bitfield::setTrueCount(size_t n) noexcept
 {
     TR_ASSERT(bit_count_ == 0 || n <= bit_count_);
@@ -247,11 +242,6 @@ void tr_bitfield::setTrueCount(size_t n) noexcept
     }
 
     TR_ASSERT(isValid());
-}
-
-void tr_bitfield::rebuildTrueCount() noexcept
-{
-    setTrueCount(countFlags());
 }
 
 void tr_bitfield::incrementTrueCount(size_t inc) noexcept
