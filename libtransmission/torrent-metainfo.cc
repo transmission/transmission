4374838d9d695b6d2ae120afc84239d68f5bfa88
Charles Kerr <charles@charleskerr.com>
2023-01-27 11:57:22 -0600

    chore: mark tr_torrent_metainfo::pieceHash() TR_CONSTEXPR20

--- libtransmission/torrent-metainfo.cc
+++ libtransmission/torrent-metainfo.cc
@@ -631,11 +631,6 @@ bool tr_torrent_metainfo::parseTorrentFile(std::string_view filename, std::vecto
     return tr_loadFile(filename, *contents, error) && parseBenc({ std::data(*contents), std::size(*contents) }, error);
 }
 
-tr_sha1_digest_t const& tr_torrent_metainfo::pieceHash(tr_piece_index_t piece) const
-{
-    return this->pieces_[piece];
-}
-
 tr_pathbuf tr_torrent_metainfo::makeFilename(
     std::string_view dirname,
     std::string_view name,
