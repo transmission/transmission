61fa6f6088c3bacf22078e3f2f13b32b119a5191
Cœur <coeur@gmx.fr>
2023-02-01 13:51:04 +0800

    Fix WebUtilsTest.urlParse and psl_builtin for Xcode (#4642)

--- libtransmission/web-utils.cc
+++ libtransmission/web-utils.cc
@@ -21,7 +21,9 @@
 
 #include "transmission.h"
 
+#include "log.h"
 #include "net.h"
+#include "tr-assert.h"
 #include "tr-strbuf.h"
 #include "utils.h"
 #include "web-utils.h"
@@ -252,12 +254,20 @@ std::string_view getSiteName(std::string_view host)
         return host;
     }
 
+    TR_ASSERT(psl_builtin() != nullptr);
+    if (psl_builtin() == nullptr)
+    {
+        tr_logAddWarn("psl_builtin is null");
+        return host;
+    }
+
     // psl needs a zero-terminated hostname
     auto const szhost = tr_urlbuf{ host };
 
     // is it a registered name?
     if (isAsciiNonUpperCase(host))
     {
+        // www.example.co.uk -> example.co.uk
         if (char const* const top = psl_registrable_domain(psl_builtin(), std::data(szhost)); top != nullptr)
         {
             host.remove_prefix(top - std::data(szhost));
@@ -265,7 +275,7 @@ std::string_view getSiteName(std::string_view host)
     }
     else if (char* lower = nullptr; psl_str_to_utf8lower(std::data(szhost), nullptr, nullptr, &lower) == PSL_SUCCESS)
     {
-        // www.example.com -> example.com
+        // www.example.co.uk -> example.co.uk
         if (char const* const top = psl_registrable_domain(psl_builtin(), lower); top != nullptr)
         {
             host.remove_prefix(top - lower);
@@ -274,7 +284,7 @@ std::string_view getSiteName(std::string_view host)
         psl_free_string(lower);
     }
 
-    // example.com -> example
+    // example.co.uk -> example
     if (auto const dot_pos = host.find('.'); dot_pos != std::string_view::npos)
     {
         host = host.substr(0, dot_pos);
