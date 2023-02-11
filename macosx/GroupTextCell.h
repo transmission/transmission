c840bbd404d09efe6acf692919f51e04a64c73ab
Charles Kerr <charles@charleskerr.com>
2023-02-11 12:20:18 -0600

    Revert "fix: empty tableview rows on macOS (#4333)"
    This reverts commit 9f2a981c8c40fe5dd4326cc1406b990a480872f2.

--- macosx/GroupTextCell.h
+++
@@ -1,9 +0,0 @@
-// This file Copyright © 2022 Transmission authors and contributors.
-// It may be used under the MIT (SPDX: MIT) license.
-// License text can be found in the licenses/ folder.
-
-#import <AppKit/AppKit.h>
-
-@interface GroupTextCell : NSTextFieldCell
-
-@end
