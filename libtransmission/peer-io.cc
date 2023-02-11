456a98b6fb5ca6033a8ecfb88f0fec57824ec2de
Charles Kerr <charles@charleskerr.com>
2023-02-11 12:20:21 -0600

    Revert "fix: test for tr_peerIo.gotError function pointer before use (#4051)"
    This reverts commit d5ef1ea80a213f9adc6ef897e65d110406084657.

--- libtransmission/peer-io.cc
+++ libtransmission/peer-io.cc
@@ -732,7 +732,10 @@ tr_peerIo::~tr_peerIo()
     auto const lock = session->unique_lock();
     TR_ASSERT(session->events != nullptr);
 
-    clearCallbacks();
+    this->canRead = nullptr;
+    this->didWrite = nullptr;
+    this->gotError = nullptr;
+
     tr_logAddTraceIo(this, "in tr_peerIo destructor");
     event_disable(this, EV_READ | EV_WRITE);
     io_close_socket(this);
@@ -753,7 +756,7 @@ void tr_peerIo::setCallbacks(tr_can_read_cb readcb, tr_did_write_cb writecb, tr_
 
 void tr_peerIo::clear()
 {
-    clearCallbacks();
+    setCallbacks(nullptr, nullptr, nullptr, nullptr);
     setEnabled(TR_UP, false);
     setEnabled(TR_DOWN, false);
     io_close_socket(this);
@@ -993,12 +996,7 @@ static size_t tr_peerIoTryWrite(tr_peerIo* io, size_t howmuch, tr_error** error)
                         What,
                         my_error->code,
                         my_error->message));
-
-                if (io->gotError != nullptr)
-                {
-                    io->gotError(io, What, io->userData);
-                }
-
+                io->gotError(io, What, io->userData);
                 tr_error_propagate(error, &my_error);
             }
         }
