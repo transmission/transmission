67fcd27ce06818cb5330450951a0fedc608a5630
Charles Kerr <charles@charleskerr.com>
2023-02-11 12:20:16 -0600

    Revert "fix: when comparing priority for rpc call, cast to string first (#4610)"
    This reverts commit 506439f099a60adfe202273e64141302a89450f4.

--- web/src/utils.js
+++ web/src/utils.js
@@ -268,10 +268,6 @@ export function setChecked(element, b) {
   setOrDeleteAttribute(element, 'checked', b);
 }
 
-export function addCheckedClass(element, b) {
-  element.classList.toggle('checked', b);
-}
-
 function getBestMenuPos(r, bounds) {
   let { x, y } = r;
   const { width, height } = r;
