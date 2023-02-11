a87f42e2df1d06b6cc7c985d995c4c65e991421e
Charles Kerr <charles@charleskerr.com>
2023-02-11 12:20:17 -0600

    Revert "fix: -Wredundant-decls warning when building with g++ (#4526)"
    This reverts commit 1e521b903030d4f1acc95615a24d37078126e88e.

--- libtransmission/utils-ev.h
+++ libtransmission/utils-ev.h
@@ -11,38 +11,72 @@
 
 #include <memory>
 
-struct evbuffer;
-struct event;
-struct event_base;
-struct evhttp;
+extern "C"
+{
+    struct evbuffer;
+    struct event;
+    struct event_base;
+    struct evhttp;
+
+    void evbuffer_free(struct evbuffer*);
+    void event_base_free(struct event_base*);
+    int event_del(struct event*);
+    void event_free(struct event*);
+    void evhttp_free(struct evhttp*);
+}
 
 namespace libtransmission::evhelpers
 {
 
 struct BufferDeleter
 {
-    void operator()(struct evbuffer* buf) const noexcept;
+    void operator()(struct evbuffer* buf) const noexcept
+    {
+        if (buf != nullptr)
+        {
+            evbuffer_free(buf);
+        }
+    }
 };
 
 using evbuffer_unique_ptr = std::unique_ptr<struct evbuffer, BufferDeleter>;
 
 struct EventBaseDeleter
 {
-    void operator()(struct event_base* evbase) const noexcept;
+    void operator()(struct event_base* evbase) const noexcept
+    {
+        if (evbase != nullptr)
+        {
+            event_base_free(evbase);
+        }
+    }
 };
 
 using evbase_unique_ptr = std::unique_ptr<struct event_base, EventBaseDeleter>;
 
 struct EventDeleter
 {
-    void operator()(struct event* event) const;
+    void operator()(struct event* event) const
+    {
+        if (event != nullptr)
+        {
+            event_del(event);
+            event_free(event);
+        }
+    }
 };
 
 using event_unique_ptr = std::unique_ptr<struct event, EventDeleter>;
 
 struct EvhttpDeleter
 {
-    void operator()(struct evhttp* evh) const noexcept;
+    void operator()(struct evhttp* evh) const noexcept
+    {
+        if (evh != nullptr)
+        {
+            evhttp_free(evh);
+        }
+    }
 };
 
 using evhttp_unique_ptr = std::unique_ptr<struct evhttp, EvhttpDeleter>;
