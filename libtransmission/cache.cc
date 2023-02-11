24722e5e819a1b76c0d5b86d439ba466617becf0
Charles Kerr <charles@charleskerr.com>
2023-02-11 12:20:16 -0600

    Revert "perf: optimize the only block case in Cache::writeContiguous() (#4679)"
    This reverts commit 52d49b84eb58fd9b162eb45d22be6033a744350f.

--- libtransmission/cache.cc
+++ libtransmission/cache.cc
@@ -71,30 +71,21 @@ std::pair<Cache::CIter, Cache::CIter> Cache::findContiguous(CIter const begin, C
 
 int Cache::writeContiguous(CIter const begin, CIter const end) const
 {
-    // The most common case without an extra data copy.
-    auto const* towrite = begin->buf.get();
-
-    // Contiguous area to join more than one block, if any.
+    // join the blocks together into contiguous memory `buf`
     auto buf = std::vector<uint8_t>{};
-
-    if (end - begin > 1)
+    auto const buflen = std::accumulate(
+        begin,
+        end,
+        size_t{},
+        [](size_t sum, auto const& block) { return sum + std::size(*block.buf); });
+    buf.reserve(buflen);
+    for (auto iter = begin; iter != end; ++iter)
     {
-        // Yes, there are.
-        auto const buflen = std::accumulate(
-            begin,
-            end,
-            size_t{},
-            [](size_t sum, auto const& block) { return sum + std::size(*block.buf); });
-        buf.reserve(buflen);
-        for (auto iter = begin; iter != end; ++iter)
-        {
-            TR_ASSERT(begin->key.first == iter->key.first);
-            TR_ASSERT(begin->key.second + std::distance(begin, iter) == iter->key.second);
-            buf.insert(std::end(buf), std::begin(*iter->buf), std::end(*iter->buf));
-        }
-        TR_ASSERT(std::size(buf) == buflen);
-        towrite = &buf;
+        TR_ASSERT(begin->key.first == iter->key.first);
+        TR_ASSERT(begin->key.second + std::distance(begin, iter) == iter->key.second);
+        buf.insert(std::end(buf), std::begin(*iter->buf), std::end(*iter->buf));
     }
+    TR_ASSERT(std::size(buf) == buflen);
 
     // save it
     auto const& [torrent_id, block] = begin->key;
@@ -106,13 +97,13 @@ int Cache::writeContiguous(CIter const begin, CIter const end) const
 
     auto const loc = tor->blockLoc(block);
 
-    if (auto const err = tr_ioWrite(tor, loc, std::size(*towrite), std::data(*towrite)); err != 0)
+    if (auto const err = tr_ioWrite(tor, loc, std::size(buf), std::data(buf)); err != 0)
     {
         return err;
     }
 
     ++disk_writes_;
-    disk_write_bytes_ += std::size(*towrite);
+    disk_write_bytes_ += std::size(buf);
     return {};
 }
 
