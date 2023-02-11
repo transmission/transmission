c839c85fa8cefb75a27e9e9c9cbc58a7a1b74ed0
Charles Kerr <charles@charleskerr.com>
2023-02-11 12:20:16 -0600

    Revert "daemon: check whether log file is regular and may be flushed (#4612)"
    This reverts commit 770da79cf39e41772bc46dfe61f671cd7a1f5de8.

--- daemon/daemon.cc
+++ daemon/daemon.cc
@@ -172,7 +172,6 @@ bool tr_daemon::reopen_log_file(char const* filename)
     }
 
     logfile_ = new_log_file;
-    logfile_flush_ = tr_sys_file_flush_possible(logfile_);
 
     if (old_log_file != TR_BAD_SYS_FILE)
     {
@@ -363,7 +362,7 @@ static void printMessage(
 #endif
 }
 
-static void pumpLogMessages(tr_sys_file_t file, bool flush)
+static void pumpLogMessages(tr_sys_file_t file)
 {
     tr_log_message* list = tr_logGetQueue();
 
@@ -372,7 +371,7 @@ static void pumpLogMessages(tr_sys_file_t file, bool flush)
         printMessage(file, l->level, l->name, l->message, l->file, l->line);
     }
 
-    if (flush && file != TR_BAD_SYS_FILE)
+    if (file != TR_BAD_SYS_FILE)
     {
         tr_sys_file_flush(file);
     }
@@ -397,7 +396,7 @@ void tr_daemon::report_status(void)
 
 void tr_daemon::periodic_update(void)
 {
-    pumpLogMessages(logfile_, logfile_flush_);
+    pumpLogMessages(logfile_);
     report_status();
 }
 
@@ -853,7 +852,7 @@ CLEANUP:
 
     tr_sessionSaveSettings(my_session_, cdir, &settings_);
     tr_sessionClose(my_session_);
-    pumpLogMessages(logfile_, logfile_flush_);
+    pumpLogMessages(logfile_);
     printf(" done.\n");
 
     /* shutdown */
@@ -900,7 +899,6 @@ bool tr_daemon::init(int argc, char const* const argv[], bool* foreground, int* 
     if (*foreground && logfile_ == TR_BAD_SYS_FILE)
     {
         logfile_ = tr_sys_file_get_std(TR_STD_SYS_FILE_ERR);
-        logfile_flush_ = tr_sys_file_flush_possible(logfile_);
     }
 
     if (!loaded)
