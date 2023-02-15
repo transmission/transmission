e156c99298bc6ec01c7067364df7122e81d169cc
Charles Kerr <charles@charleskerr.com>
2023-02-14 23:37:46 -0600

    chore: clang-format

--- libtransmission/peer-io.cc
+++ libtransmission/peer-io.cc
@@ -686,17 +686,17 @@ void tr_peerIo::on_utp_error(int errcode)
     tr_error* error = nullptr;
     switch (errcode)
     {
-        case UTP_ECONNREFUSED:
-            tr_error_set_from_errno(&error, ECONNREFUSED);
-            break;
-        case UTP_ECONNRESET:
-            tr_error_set_from_errno(&error, ECONNRESET);
-            break;
-        case UTP_ETIMEDOUT:
-            tr_error_set_from_errno(&error, ETIMEDOUT);
-            break;
-        default:
-            tr_error_set(&error, errcode, utp_error_code_names[errcode]);
+    case UTP_ECONNREFUSED:
+        tr_error_set_from_errno(&error, ECONNREFUSED);
+        break;
+    case UTP_ECONNRESET:
+        tr_error_set_from_errno(&error, ECONNRESET);
+        break;
+    case UTP_ETIMEDOUT:
+        tr_error_set_from_errno(&error, ETIMEDOUT);
+        break;
+    default:
+        tr_error_set(&error, errcode, utp_error_code_names[errcode]);
     }
     call_error_callback(*error);
     tr_error_clear(&error);
