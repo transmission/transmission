54f091526bea9e767676fa23df674563f51368c0
Roey Fuchs <44167563+RoeyFuchs@users.noreply.github.com>
2023-02-24 22:22:06 +0200

    Update daemon/daemon.cc
    Co-authored-by: Charles Kerr <charles@charleskerr.com>

--- daemon/daemon.cc
+++ daemon/daemon.cc
@@ -631,7 +631,7 @@ bool tr_daemon::parse_args(int argc, char const* const* argv, bool* dump_setting
         case TR_OPT_UNK:
             fprintf(stderr, "Unexpected argument: %s \n", optstr);
             tr_getopt_usage(MyName, Usage, std::data(Options));
-            *exit_code = 0;
+            *exit_code = 1;
             return false;
 
         default:
