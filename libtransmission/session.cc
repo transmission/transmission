5fb772a5c7eac49f9540503b5c52cff15770ea0f
Charles Kerr <charles@charleskerr.com>
2023-02-23 23:40:45 -0600

    fixup! fix: ensure torrent files are loaded before magnets on startup
    chore: code cleanup

--- libtransmission/session.cc
+++ libtransmission/session.cc
@@ -1375,36 +1375,31 @@ namespace load_torrents_helpers
 
 void session_load_torrents(tr_session* session, tr_ctor* ctor, std::promise<size_t>* loaded_promise)
 {
-    constexpr auto const IsTorrentOrMagnetFile = [](std::string_view name)
-    {
-        return tr_strvEndsWith(name, ".torrent"sv) || tr_strvEndsWith(name, ".magnet"sv);
-    };
+    auto const& folder = session->torrentDir();
+    auto names = get_matching_files(
+        folder,
+        [](std::string_view name) { return tr_strvEndsWith(name, ".torrent"sv) || tr_strvEndsWith(name, ".magnet"sv); });
 
     // An infohash *shouldn't* have both a .torrent and .magnet file;
     // but if somehow it does, we want to load the .torrent first to
     // ensure the tr_torrent is constructed with metainfo.
-    constexpr auto const TorrentsFirstCompare = [](auto const& a, auto const& b) // <
-    {
-        constexpr auto const IsTorrentFile = [](std::string_view name)
+    std::sort(
+        std::begin(names),
+        std::end(names),
+        [](auto const& a, auto const& b)
         {
-            return tr_strvEndsWith(name, ".torrent"sv);
-        };
+            auto const a_is_torrent = tr_strvEndsWith(a, ".torrent"sv);
+            auto const b_is_torrent = tr_strvEndsWith(b, ".torrent"sv);
+            if (a_is_torrent != b_is_torrent)
+            {
+                return a_is_torrent;
+            }
 
-        auto const a_is_torrent = IsTorrentFile(a);
-        auto const b_is_torrent = IsTorrentFile(b);
-        if (a_is_torrent != b_is_torrent)
-        {
-            return a_is_torrent;
-        }
+            return a < b;
+        });
 
-        return a < b;
-    };
-
-    auto const& folder = session->torrentDir();
     auto n_torrents = size_t{};
     auto buf = std::vector<char>{};
-    auto names = get_matching_files(folder, IsTorrentOrMagnetFile);
-    std::sort(std::begin(names), std::end(names), TorrentsFirstCompare);
     for (auto const& name : names)
     {
         if (auto const path = tr_pathbuf{ folder, '/', name }; !tr_ctorSetMetainfoFromFile(ctor, path.sv(), nullptr))
