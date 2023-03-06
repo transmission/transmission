39e3e1d87b69a1a5e63a6cc17ef62c3b40034711
Charles Kerr <charles@charleskerr.com>
2023-03-06 15:50:19 -0600

    fix: increase priority of first and last piece of each file (#5167)
    The first and last pieces of a file generally include information needed
    by preview generators, so prioritizing those pieces makes life easier
    for users or tools that look at files while they're being downloaded.
    
    This worked in 3.00 but isn't present in 4.0.0.

--- libtransmission/file-piece-map.h
+++ libtransmission/file-piece-map.h
@@ -52,7 +52,11 @@ public:
     void reset(tr_block_info const& block_info, uint64_t const* file_sizes, size_t n_files);
     void reset(tr_torrent_metainfo const& tm);
 
-    [[nodiscard]] piece_span_t pieceSpan(tr_file_index_t file) const;
+    [[nodiscard]] TR_CONSTEXPR20 piece_span_t pieceSpan(tr_file_index_t file) const noexcept
+    {
+        return file_pieces_[file];
+    }
+
     [[nodiscard]] file_span_t fileSpan(tr_piece_index_t piece) const;
 
     [[nodiscard]] file_offset_t fileOffset(uint64_t offset) const;
@@ -60,6 +64,11 @@ public:
     [[nodiscard]] TR_CONSTEXPR20 size_t size() const
     {
         return std::size(file_pieces_);
+    }
+
+    [[nodiscard]] TR_CONSTEXPR20 bool empty() const noexcept
+    {
+        return std::empty(file_pieces_);
     }
 
     // TODO(ckerr) minor wart here, two identical span types
