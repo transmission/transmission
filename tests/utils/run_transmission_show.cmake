ed43366bc9430191fbb4f295ffc03eeb27d93782
Charles Kerr <charles@charleskerr.com>
2023-03-13 13:04:20 -0500

    chore: reduce diffs to main
    undo whitespace changes

--- tests/utils/run_transmission_show.cmake
+++ tests/utils/run_transmission_show.cmake
@@ -6,49 +6,48 @@ if(CMAKE_VERSION VERSION_LESS 3.14)
     # --ignore-eol was introduced in CMake 3.14
     message(status "skipping transmission-show test; cmake version too old")
 else()
-   get_filename_component(torrent_basename "${torrent_file}" NAME)
-   set(output_file ${CMAKE_CURRENT_BINARY_DIR}/${torrent_basename}.out)
+    get_filename_component(torrent_basename "${torrent_file}" NAME)
+    set(output_file ${CMAKE_CURRENT_BINARY_DIR}/${torrent_basename}.out)
 
-   message(STATUS "transmission_show ${transmission_show}")
-   message(STATUS "       input_file ${torrent_file}")
-   message(STATUS "      output_file ${output_file}")
-   message(STATUS "   reference_file ${reference_file}")
+    message(STATUS "transmission_show ${transmission_show}")
+    message(STATUS "       input_file ${torrent_file}")
+    message(STATUS "      output_file ${output_file}")
+    message(STATUS "   reference_file ${reference_file}")
 
-   # We want UTF-8
-   set(ENV{LC_ALL} "en_US.UTF-8")
-   # The app's output includes timestamps, so fake our TZ to ensure
-   # the test doesn't depend on the physical TZ of the test machine
-   set(ENV{TZ} "UTC")
-   execute_process(
-      COMMAND ${transmission_show} ${torrent_file}
-      OUTPUT_FILE ${output_file}
-   )
+    # We want UTF-8
+    set(ENV{LC_ALL} "en_US.UTF-8")
 
-   execute_process(
-      COMMAND ${CMAKE_COMMAND} -E compare_files --ignore-eol ${reference_file} ${output_file}
-      RESULT_VARIABLE STATUS
-   )
+    # The app's output includes timestamps, so fake our TZ to ensure
+    # the test doesn't depend on the physical TZ of the test machine
+    set(ENV{TZ} "UTC")
 
-   if(STATUS AND NOT STATUS EQUAL 0)
-      file(READ ${reference_file} CONTENTS)
-      message("EXPECTED CONTENTS (${reference_file}):")
-      message(${CONTENTS})
+    execute_process(
+        COMMAND ${transmission_show} ${torrent_file}
+        OUTPUT_FILE ${output_file})
 
-      file(READ ${output_file} CONTENTS)
-      message("RECEIVED CONTENTS (${output_file}):")
-      message(${CONTENTS})
+    execute_process(
+        COMMAND ${CMAKE_COMMAND} -E compare_files --ignore-eol ${reference_file} ${output_file}
+        RESULT_VARIABLE STATUS)
 
-      find_program(DIFF_EXEC diff)
-      if (DIFF_EXEC)
-         message("DIFF:")
-         execute_process(COMMAND ${DIFF_EXEC} -u ${output_file} ${reference_file})
-      endif()
+    if(STATUS AND NOT STATUS EQUAL 0)
+        file(READ ${reference_file} CONTENTS)
+        message("EXPECTED CONTENTS (${reference_file}):")
+        message(${CONTENTS})
 
-      file(REMOVE ${output_file})
-      message(FATAL_ERROR "failed: files '${reference_file}' and '${output_file}' do not match")
-   else()
-      file(REMOVE ${output_file})
-      message("passed")
-   endif()
+        file(READ ${output_file} CONTENTS)
+        message("RECEIVED CONTENTS (${output_file}):")
+        message(${CONTENTS})
 
+        find_program(DIFF_EXEC diff)
+        if(DIFF_EXEC)
+            message("DIFF:")
+            execute_process(COMMAND ${DIFF_EXEC} -u ${output_file} ${reference_file})
+        endif()
+
+        file(REMOVE ${output_file})
+        message(FATAL_ERROR "failed: files '${reference_file}' and '${output_file}' do not match")
+    else()
+        file(REMOVE ${output_file})
+        message("passed")
+    endif()
 endif()
