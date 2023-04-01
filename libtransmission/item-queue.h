d346a285ac18734304f42f82f2c678ee9a9a1b1b
Charles Kerr <charles@charleskerr.com>
2023-04-01 17:32:45 -0500

    chore: savepoint

---
+++ libtransmission/item-queue.h
@@ -0,0 +1,148 @@
+// This file Copyright © 2023 Mnemosyne LLC.
+// It may be used under GPLv2 (SPDX: GPL-2.0-only), GPLv3 (SPDX: GPL-3.0-only),
+// or any future license endorsed by Mnemosyne LLC.
+// License text can be found in the licenses/ folder.
+
+#pragma once
+
+#ifndef __TRANSMISSION__
+#error only libtransmission should #include this header.
+#endif
+
+#include <algorithm>
+#include <cstddef> // for size_t
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <optional>
+#include <set>
+#include <sstream>
+#include <vector>
+
+#include "tr-macros.h" // for TR_CONSTEXPR20
+
+namespace libtransmission
+{
+template<typename Item>
+class ItemQueue
+{
+public:
+    [[nodiscard]] TR_CONSTEXPR20 auto size() const noexcept
+    {
+        return std::size(items_);
+    }
+
+    [[nodiscard]] TR_CONSTEXPR20 auto empty() const noexcept
+    {
+        return std::empty(items_);
+    }
+
+    void move_top(Item const* items, size_t item_count);
+
+    void move_up(Item const* items, size_t item_count)
+    {
+
+        auto const do_move = [items, item_count](auto const& item)
+        {
+            auto const begin = items;
+            auto const end = items + item_count;
+            auto const found = std::find(begin, end, item) != end;
+            std::cerr << "item [" << item << "] found " << found << std::endl;
+            return found;
+        };
+
+        for (auto const& [lpos, litem] : items_) std::cerr << "[pos " << lpos << " item " << litem << ']'; std::cerr << std::endl;
+        for (size_t i = 0, end = std::size(items_); i != end; ++i)
+        {
+            if (auto const& [pos, item] = items_[i]; do_move(item) && pos > 0U)
+            {
+                std::cerr << "due to " << item << " swapping " << items_[i-1U].second << " and " << items_[i].second << std::endl;
+                std::swap(items_[i-1U].second, items_[i].second);
+                for (auto const& [lpos, litem] : items_) std::cerr << "[pos " << lpos << " item " << litem << ']'; std::cerr << std::endl;
+            }
+        }
+        for (auto const& [lpos, litem] : items_) std::cerr << "[pos " << lpos << " item " << litem << ']'; std::cerr << std::endl;
+    }
+
+    void move_down(Item const* items, size_t item_count);
+
+    void move_bottom(Item const* items, size_t item_count);
+
+    void erase(Item const& item)
+    {
+        if (auto old_iter = find(item); old_iter != std::end(items_))
+        {
+            items_.erase(old_iter);
+        }
+    }
+
+    void set(Item item, size_t pos)
+    {
+        erase(item);
+        auto const iter = std::lower_bound(std::begin(items_), std::end(items_), pos, CompareByPos{});
+        items_.emplace(iter, pos, std::move(item));
+    }
+
+    [[nodiscard]] std::optional<size_t> get_position(Item const& item) const
+    {
+        if (auto iter = find(item); item != std::end(items_))
+        {
+            return iter - std::begin(items_);
+        }
+
+        return {};
+    }
+
+    [[nodiscard]] std::optional<Item> pop()
+    {
+        if (std::empty(items_))
+        {
+            return {};
+        }
+
+        auto item = std::move(items_.at(0).second);
+        items_.erase(std::begin(items_));
+        return item;
+    }
+
+    [[nodiscard]] std::vector<Item> queue() const
+    {
+        auto ret = std::vector<Item>{};
+        ret.reserve(size());
+        for (auto const& [pos, item] : items_)
+        {
+            ret.emplace_back(item);
+        }
+        return ret;
+    }
+
+private:
+    using PairType = std::pair<size_t /*pos*/, Item>;
+    std::vector<PairType> items_;
+
+    [[nodiscard]] auto find(Item const& item) const
+    {
+        auto const pred = [&item = item](PairType const& pair)
+        {
+            return item == pair.second;
+        };
+
+        return std::find_if(std::begin(items_), std::end(items_), pred);
+    }
+
+    struct CompareByPos
+    {
+
+        [[nodiscard]] constexpr bool operator()(PairType const& pair, size_t pos)
+        {
+            return pair.first < pos;
+        }
+
+        [[nodiscard]] constexpr bool operator()(PairType const& a, PairType const& b)
+        {
+            return a.first < b.first;
+        }
+    };
+};
+
+} // namespace libtransmission
---
+++ tests/libtransmission/item-queue-test.cc
@@ -0,0 +1,207 @@
+// This file Copyright (C) 2023 Mnemosyne LLC.
+// It may be used under GPLv2 (SPDX: GPL-2.0-only), GPLv3 (SPDX: GPL-3.0-only),
+// or any future license endorsed by Mnemosyne LLC.
+// License text can be found in the licenses/ folder.
+
+#include <array>
+#include <sstream>
+#include <string_view>
+#include <utility>
+
+#include <libtransmission/transmission.h>
+
+#include <libtransmission/item-queue.h>
+
+#include "test-fixtures.h"
+
+using ItemQueueTest = ::testing::Test;
+using libtransmission::ItemQueue;
+
+TEST_F(ItemQueueTest, construct)
+{
+    auto items = ItemQueue<size_t>{};
+    EXPECT_TRUE(std::empty(items));
+    EXPECT_EQ(0U, std::size(items));
+}
+
+TEST_F(ItemQueueTest, setInOrder)
+{
+    using Type = std::string_view;
+    static auto constexpr Key1 = "hello"sv;
+    static auto constexpr Key2 = "world"sv;
+
+    auto items = ItemQueue<Type>{};
+    items.set(Key1, 0U);
+    items.set(Key2, 1U);
+
+    EXPECT_EQ(2U, std::size(items));
+    EXPECT_FALSE(std::empty(items));
+
+    auto const expected_queue = std::vector<Type>{ Key1, Key2 };
+    auto const actual_queue = items.queue();
+    EXPECT_EQ(expected_queue, actual_queue);
+}
+
+TEST_F(ItemQueueTest, eraseFront)
+{
+    using Type = std::string_view;
+    static auto constexpr Key1 = "a"sv;
+    static auto constexpr Key2 = "b"sv;
+    static auto constexpr Key3 = "c"sv;
+
+    auto items = ItemQueue<Type>{};
+    items.set(Key1, 0U);
+    items.set(Key2, 1U);
+    items.set(Key3, 2U);
+    items.erase(Key1);
+    auto expected_queue = std::vector<Type>{ Key2, Key3 };
+    auto actual_queue = items.queue();
+    EXPECT_EQ(expected_queue, actual_queue);
+}
+
+TEST_F(ItemQueueTest, eraseMiddle)
+{
+    using Type = std::string_view;
+    static auto constexpr Key1 = "a"sv;
+    static auto constexpr Key2 = "b"sv;
+    static auto constexpr Key3 = "c"sv;
+
+    auto items = ItemQueue<Type>{};
+    items.set(Key1, 0U);
+    items.set(Key2, 1U);
+    items.set(Key3, 2U);
+    items.erase(Key2);
+    auto expected_queue = std::vector<Type>{ Key1, Key3 };
+    auto actual_queue = items.queue();
+    EXPECT_EQ(expected_queue, actual_queue);
+}
+
+TEST_F(ItemQueueTest, eraseBack)
+{
+    using Type = std::string_view;
+    static auto constexpr Key1 = "a"sv;
+    static auto constexpr Key2 = "b"sv;
+    static auto constexpr Key3 = "c"sv;
+
+    auto items = ItemQueue<Type>{};
+    items.set(Key1, 0U);
+    items.set(Key2, 1U);
+    items.set(Key3, 2U);
+    items.erase(Key3);
+    auto expected_queue = std::vector<Type>{ Key1, Key2 };
+    auto actual_queue = items.queue();
+    EXPECT_EQ(expected_queue, actual_queue);
+}
+
+TEST_F(ItemQueueTest, setInReverseOrder)
+{
+    using Type = std::string_view;
+    static auto constexpr Key1 = "hello"sv;
+    static auto constexpr Key2 = "world"sv;
+
+    auto items = ItemQueue<Type>{};
+    items.set(Key1, 1U);
+    items.set(Key2, 0U);
+    EXPECT_EQ(2U, std::size(items));
+    EXPECT_FALSE(std::empty(items));
+
+    auto const expected_queue = std::vector<Type>{ Key2, Key1 };
+    auto const actual_queue = items.queue();
+    EXPECT_EQ(expected_queue, actual_queue);
+}
+
+TEST_F(ItemQueueTest, setReplacesPreviousPosition)
+{
+    using Type = std::string_view;
+    static auto constexpr Key1 = "hello"sv;
+    static auto constexpr Key2 = "world"sv;
+
+    auto items = ItemQueue<Type>{};
+    items.set(Key1, 0U);
+    items.set(Key2, 1U);
+    items.set(Key1, 2U);
+    EXPECT_EQ(2U, std::size(items));
+    EXPECT_FALSE(std::empty(items));
+
+    auto const expected_queue = std::vector<Type>{ Key2, Key1 };
+    auto const actual_queue = items.queue();
+    EXPECT_EQ(expected_queue, actual_queue);
+}
+
+TEST_F(ItemQueueTest, pop)
+{
+    using Type = std::string_view;
+    static auto constexpr Key1 = "hello"sv;
+    static auto constexpr Key2 = "world"sv;
+
+    auto items = ItemQueue<Type>{};
+    items.set(Key1, 0U);
+    items.set(Key2, 1U);
+
+    auto popped = items.pop();
+    EXPECT_TRUE(popped.has_value());
+    EXPECT_EQ(Key1, *popped);
+    EXPECT_EQ(1U, std::size(items));
+
+    popped = items.pop();
+    EXPECT_TRUE(popped.has_value());
+    EXPECT_EQ(Key2, *popped);
+    EXPECT_TRUE(std::empty(items));
+
+    popped = items.pop();
+    EXPECT_FALSE(popped.has_value());
+}
+
+namespace
+{
+
+template<typename Type>
+[[nodiscard]] auto toString(std::vector<Type> const& items)
+{
+    auto out = std::ostringstream{};
+    for (auto const& item : items)
+    {
+        out << '[' << item << ']';
+    }
+    return out.str();
+}
+
+} // namespace
+
+
+TEST_F(ItemQueueTest, moveUp)
+{
+    using Type = std::string_view;
+
+    struct Test
+    {
+        std::vector<Type> initial;
+        std::vector<Type> moved;
+        std::vector<Type> expected;
+    };
+
+    auto const tests = std::array<Test, 7>{{
+        { { "a", "b", "c", "d", "e", "f" }, { "a" },      { "a", "b", "c", "d", "e", "f" } },
+        { { "a", "b", "c", "d", "e", "f" }, { "a", "b" }, { "b", "a", "c", "d", "e", "f" } },
+        { { "a", "b", "c", "d", "e", "f" }, { "b", "d" }, { "b", "a", "d", "c", "e", "f" } },
+        { { "a", "b", "c", "d", "e", "f" }, { "c", "d" }, { "a", "c", "d", "b", "e", "f" } },
+        { { "a", "b", "c", "d", "e", "f" }, { "f" },      { "a", "b", "c", "d", "f", "e" } },
+        { { "a", "b", "c", "d", "e", "f" }, { "f", "f" }, { "a", "b", "c", "d", "f", "e" } },
+        { { "a", "b", "c", "d", "e", "f" }, { "z", "q" }, { "a", "b", "c", "d", "e", "f" } },
+    }};
+
+    for (auto const& [initial, moved, expected] : tests)
+    {
+        // set up the test queue
+        auto items = ItemQueue<Type>{};
+        for (size_t i = 0; i < std::size(initial); ++i)
+        {
+            items.set(initial[i], i);
+        }
+
+        items.move_up(std::data(moved), std::size(moved));
+
+        auto const actual = items.queue();
+        EXPECT_EQ(expected, actual) << "initial: " << toString(initial) << " move " << toString(moved) << " expected " << toString(expected) << " actual " << toString(actual) << std::endl;
+    }
+}
