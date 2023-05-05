dab5a18070ba5c22f57d815411a2dd718dab9038
Arnold_Layne <kostet@gmail.com>
2023-05-05 12:42:38 +0300

    sync stats.json: remove saveIfDirty(), call save()

--- libtransmission/session.cc
+++ libtransmission/session.cc
@@ -1306,7 +1306,7 @@ void tr_session::closeImplPart2(std::promise<void>* closed_promise, std::chrono:
     this->announcer_.reset();
     this->announcer_udp_.reset();
 
-    stats().saveForceDirty();
+    stats().save();
     peer_mgr_.reset();
     openFiles().closeAll();
     tr_utpClose(this);
@@ -2136,7 +2136,7 @@ tr_session::tr_session(std::string_view config_dir, tr_variant* settings_dict)
                 tr_torrentSave(tor);
             }
 
-            stats().saveForceDirty();
+            stats().save();
         });
     save_timer_->startRepeating(SaveIntervalSecs);
 
