aa701a6cc6592fd07259f5d58f3e2185bde26c24
Charles Kerr <charles@charleskerr.com>
2023-06-19 16:01:29 -0500

    refactor: use std::map instead of QMap in DetailsDialog.cc

--- qt/DetailsDialog.cc
+++ qt/DetailsDialog.cc
@@ -21,7 +21,6 @@
 #include <QItemSelectionModel>
 #include <QLabel>
 #include <QList>
-#include <QMap>
 #include <QMessageBox>
 #include <QResizeEvent>
 #include <QRegularExpression>
@@ -1098,20 +1097,23 @@ void DetailsDialog::refreshUI()
     ///  Peers tab
     ///
 
-    QMap<QString, QTreeWidgetItem*> peers2;
+    auto peers2 = decltype(peers_){};
     QList<QTreeWidgetItem*> new_items;
 
     for (Torrent const* const t : torrents)
     {
         QString const id_str(QString::number(t->id()));
-        PeerList const peers = t->peers();
 
-        for (Peer const& peer : peers)
+        for (Peer const& peer : t->peers())
         {
             QString const key = id_str + QLatin1Char(':') + peer.address;
-            auto* item = dynamic_cast<PeerItem*>(peers_.value(key, nullptr));
 
-            if (item == nullptr) // new peer has connected
+            PeerItem* item = nullptr;
+            if (auto iter = peers_.find(key); iter != std::end(peers_))
+            {
+                item = dynamic_cast<PeerItem*>(iter->second);
+            }
+            else // new peer has connected
             {
                 item = new PeerItem(peer);
                 item->setTextAlignment(COL_UP, Qt::AlignRight | Qt::AlignVCenter);
@@ -1124,7 +1126,7 @@ void DetailsDialog::refreshUI()
                 new_items << item;
             }
 
-            QString const code = peer.flags;
+            auto const& code = peer.flags;
             item->setStatus(code);
             item->refresh(peer);
 
@@ -1207,23 +1209,22 @@ void DetailsDialog::refreshUI()
             item->setText(COL_STATUS, code);
             item->setToolTip(COL_STATUS, code_tip);
 
-            peers2.insert(key, item);
+            peers2.try_emplace(key, item);
         }
     }
 
     ui_.peersView->addTopLevelItems(new_items);
 
-    for (QString const& key : peers_.keys())
+    for (auto const& [key, item] : peers_)
     {
-        if (!peers2.contains(key)) // old peer has disconnected
+        if (peers2.count(key) == 0U) // old peer has disconnected
         {
-            QTreeWidgetItem* item = peers_.value(key, nullptr);
             ui_.peersView->takeTopLevelItem(ui_.peersView->indexOfTopLevelItem(item));
             delete item;
         }
     }
 
-    peers_ = peers2;
+    peers_ = std::move(peers2);
 
     if (single)
     {
