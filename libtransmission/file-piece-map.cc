transmission/transmission

commit 7c7771a12bfbcab9c42f25b9c7b5e27d9e8d41a4

perf: use small:set in tr_file_piece_map::reset() (#5720)

libtransmission/file-piece-map.cc
@@ -3,8 +3,9 @@
 // or any future license endorsed by Mnemosyne LLC.
 // License text can be found in the licenses/ folder.
 
-#include <set>
 #include <vector>
+
+#include <small/set.hpp>
 
 #include "libtransmission/transmission.h"
 
@@ -21,7 +22,8 @@ void tr_file_piece_map::reset(tr_block_info const& block_info, uint64_t const* f
     file_pieces_.resize(n_files);
     file_pieces_.shrink_to_fit();
 
-    auto edge_pieces = std::set<tr_piece_index_t>{};
+    auto edge_pieces = small::set<tr_piece_index_t, 1024U>{};
+    edge_pieces.reserve(n_files * 2U);
 
     uint64_t offset = 0;
     for (tr_file_index_t i = 0; i < n_files; ++i)

commit 43b74a706dd5510b7a400c54c5fd18daa6646aef

fix: add missing cstdint include
<vector> does not include <cstdint> at all in some STL versions.

Fixes issue introduced by https://github.com/transmission/transmission/pull/5720

Fixes: https://github.com/transmission/transmission/issues/5727

libtransmission/file-piece-map.cc
@@ -3,6 +3,7 @@
 // or any future license endorsed by Mnemosyne LLC.
 // License text can be found in the licenses/ folder.
 
+#include <cstdint>
 #include <vector>
 
 #include <small/set.hpp>