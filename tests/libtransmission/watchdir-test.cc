89b42c78efb68fb0e975598a796f4f86b737d6da
tearfur <lagoho7@gmail.com>
2023-07-08 11:26:45 +0800

    watchdir-test.cc

--- tests/libtransmission/watchdir-test.cc
+++ tests/libtransmission/watchdir-test.cc
@@ -5,22 +5,33 @@
 
 #include <chrono>
 #include <memory>
+#include <set>
 #include <string>
+#include <string_view>
+#include <utility>
 #include <vector>
+
+#ifdef _WIN32
+#include <ws2tcpip.h>
+#else
+#include <sys/time.h> // timeval
+#endif
+
+#include <event2/event.h>
 
 #define LIBTRANSMISSION_WATCHDIR_MODULE
 
-#include <libtransmission/transmission.h>
-
 #include <libtransmission/file.h>
-#include <libtransmission/net.h>
+#include <libtransmission/timer.h>
+#include <libtransmission/timer-ev.h>
+#include <libtransmission/tr-macros.h>
+#include <libtransmission/tr-strbuf.h>
+#include <libtransmission/utils.h>
 #include <libtransmission/watchdir.h>
 #include <libtransmission/watchdir-base.h>
-#include <libtransmission/timer-ev.h>
 
+#include "gtest/gtest.h"
 #include "test-fixtures.h"
-
-#include <event2/event.h>
 
 using namespace std::literals;
 
