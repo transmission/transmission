0f0b9847566bb00db458bc488a4412ffabbcc34d
tearfur <46261767+tearfur@users.noreply.github.com>
2023-07-19 11:03:30 +0800

    fix: cover all pex disabled cases in `get_peer_candidate()`

--- libtransmission/peer-mgr.cc
+++ libtransmission/peer-mgr.cc
@@ -2162,10 +2162,10 @@ struct peer_candidate
             continue;
         }
 
-        /* if everyone in the swarm is seeds and pex is disabled because
-         * the torrent is private, then don't initiate connections */
+        /* if everyone in the swarm is seeds and pex is disabled,
+         * then don't initiate connections */
         bool const seeding = tor->is_done();
-        if (seeding && swarm->isAllSeeds() && tor->is_private())
+        if (seeding && swarm->isAllSeeds() && !tor->allows_pex())
         {
             continue;
         }
