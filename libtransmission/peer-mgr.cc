f6d5405e0bd40e445b971e966053d24a3567ed6a
tearfur <lagoho7@gmail.com>
2023-07-19 12:07:29 +0800

    fixup! perf: improve sorting performance in `tr_peerMgrGetPeers()`

--- libtransmission/peer-mgr.cc
+++ libtransmission/peer-mgr.cc
@@ -1156,26 +1156,31 @@ std::vector<tr_pex> tr_peerMgrGetPeers(tr_torrent const* tor, uint8_t address_ty
 
     tr_swarm const* s = tor->swarm;
 
-    // build a list of atoms
+    // build a list of peer info objects
 
     auto infos = std::vector<tr_peer_info const*>{};
     if (list_mode == TR_PEERS_CONNECTED) /* connected peers only */
     {
         auto const& peers = s->peers;
         infos.reserve(std::size(peers));
-        std::transform(
-            std::begin(peers),
-            std::end(peers),
-            std::back_inserter(infos),
-            [](auto const* peer) { return peer->peer_info; });
+        for (auto const* peer : peers)
+        {
+            if (peer->socketAddress().address().type == address_type)
+            {
+                infos.emplace_back(peer->peer_info);
+            }
+        }
     }
     else /* TR_PEERS_INTERESTING */
     {
-        for (auto const& [socket_address, peer_info] : s->pool)
+        auto const& pool = s->pool;
+        infos.reserve(std::size(pool));
+        for (auto const& [socket_address, peer_info] : pool)
         {
-            if (is_peer_interesting(tor, peer_info))
+            TR_ASSERT(socket_address == peer_info.socket_address());
+            if (socket_address.address().type == address_type && is_peer_interesting(tor, peer_info))
             {
-                infos.push_back(&peer_info);
+                infos.emplace_back(&peer_info);
             }
         }
     }
@@ -1193,14 +1198,11 @@ std::vector<tr_pex> tr_peerMgrGetPeers(tr_torrent const* tor, uint8_t address_ty
     {
         auto const& [addr, port] = info->socket_address();
 
-        if (addr.type == address_type)
-        {
-            TR_ASSERT(addr.is_valid());
-            pex.emplace_back(addr, port, info->pex_flags());
-        }
+        TR_ASSERT(addr.is_valid());
+        TR_ASSERT(addr.type == address_type);
+        pex.emplace_back(addr, port, info->pex_flags());
     }
 
-    std::sort(std::begin(pex), std::end(pex));
     return pex;
 }
 
