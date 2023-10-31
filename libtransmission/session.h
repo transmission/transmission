c51fabf576240fcf60476e45b1bea1e20102eeb5
Charles Kerr <charles@charleskerr.com>
2023-10-31 14:02:30 -0400

    chore: update dependency comments for tr_session::peer_mgr_

--- libtransmission/session.h
+++ libtransmission/session.h
@@ -1158,7 +1158,7 @@ public:
     std::unique_ptr<Cache> cache = std::make_unique<Cache>(torrents_, 1024 * 1024 * 2);
 
 private:
-    // depends-on: timer_maker_, top_bandwidth_, utp_context, torrents_, web_, blocklist_changed_
+    // depends-on: timer_maker_, blocklists_, top_bandwidth_, utp_context, torrents_, web_
     std::unique_ptr<struct tr_peerMgr, void (*)(struct tr_peerMgr*)> peer_mgr_;
 
     // depends-on: peer_mgr_, advertised_peer_port_, torrents_
