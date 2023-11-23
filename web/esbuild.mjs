664de200a4631a17df80a363e7b8da6d477f98d9
Charles Kerr <charles@charleskerr.com>
2023-11-23 11:44:22 -0600

    build: generate LEGAL.txt file for web client

--- web/esbuild.mjs
+++ web/esbuild.mjs
@@ -1,14 +1,15 @@
-import * as esbuild from 'esbuild'
-import {sassPlugin} from 'esbuild-sass-plugin'
+import * as esbuild from 'esbuild';
+import {sassPlugin} from 'esbuild-sass-plugin';
 
 await esbuild.build({
   bundle: true,
   entryPoints: ['./src/main.js'],
+  legalComments: 'external',
   loader: {
     '.png': 'binary',
     '.svg': 'binary' },
   minify: true,
   outfile: './public_html/transmission-app.js',
-  sourcemap: true,
   plugins: [sassPlugin()],
-})
+  sourcemap: true,
+});
