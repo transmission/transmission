c673f16bb0d35d06456bc4d40658154b789cbbe6
Nick <nick.naumann@mailbox.tu-dresden.de>
2023-12-24 23:55:11 +0100

    remove labels key from DetailStatKeys

--- qt/Session.cc
+++ qt/Session.cc
@@ -580,7 +580,7 @@ std::vector<std::string_view> const& Session::getKeyNames(TorrentProperties prop
         };
 
         // changing fields needed by the details dialog
-        static auto constexpr DetailStatKeys = std::array<tr_quark, 19>{
+        static auto constexpr DetailStatKeys = std::array<tr_quark, 18>{
             TR_KEY_activityDate, //
             TR_KEY_bandwidthPriority, //
             TR_KEY_corruptEver, //
@@ -591,7 +591,6 @@ std::vector<std::string_view> const& Session::getKeyNames(TorrentProperties prop
             TR_KEY_fileStats, //
             TR_KEY_haveUnchecked, //
             TR_KEY_honorsSessionLimits, //
-            TR_KEY_labels, //
             TR_KEY_peer_limit, //
             TR_KEY_peers, //
             TR_KEY_seedIdleLimit, //
