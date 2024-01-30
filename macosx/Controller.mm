b1a7eaf512468f571bc092f038e8b862e7e144da
Cœur <coeur@gmx.fr>
2024-01-30 19:19:42 +0100

    re-adding support for ignoring beta

--- macosx/Controller.mm
+++ macosx/Controller.mm
@@ -54,6 +54,7 @@
 #import "NSStringAdditions.h"
 #import "ExpandedPathToPathTransformer.h"
 #import "ExpandedPathToIconTransformer.h"
+#import "VersionComparator.h"
 
 typedef NSString* ToolbarItemIdentifier NS_TYPED_EXTENSIBLE_ENUM;
 
@@ -5646,6 +5647,11 @@ void onTorrentCompletenessChanged(tr_torrent* tor, tr_completeness status, bool 
 - (void)updaterWillRelaunchApplication:(SUUpdater*)updater
 {
     self.fQuitRequested = YES;
+}
+
+- (nullable id<SUVersionComparison>)versionComparatorForUpdater:(SUUpdater*)updater
+{
+    return [VersionComparator new];
 }
 
 @end
