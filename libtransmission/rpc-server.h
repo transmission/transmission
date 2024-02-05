802c778ad5e882fced08e8714f3c73fe517fda44
Charles Kerr <charles@charleskerr.com>
2024-02-05 10:41:16 -0600

    Revert "chore: remove unused #include"
    This reverts commit a161298b5ff2262e07c60ebeabf6800c5a3af5de.

--- libtransmission/rpc-server.h
+++ libtransmission/rpc-server.h
@@ -20,6 +20,7 @@
 #include "libtransmission/net.h"
 #include "libtransmission/quark.h"
 #include "libtransmission/settings.h"
+#include "libtransmission/utils-ev.h"
 
 class tr_rpc_address;
 struct tr_session;
