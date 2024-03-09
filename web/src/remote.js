transmission/transmission

commit 250f43ae68f317ac0a48fe8037a67ceeb9a23d85

fix(web): close connection failed alert on successful request (#6657)

web/src/remote.js
@@ -23,8 +23,8 @@ export const RPC = {
 export class Remote {
   // TODO: decouple from controller
   constructor(controller) {
+    this._connection_alert = null;
     this._controller = controller;
-    this._error = '';
     this._session_id = '';
   }
 
@@ -56,6 +56,8 @@ export class Remote {
         if (callback) {
           callback.call(context, payload, response_argument);
         }
+
+        this._connection_alert = null;
       })
       .catch((error) => {
         if (error.message === Remote._SessionHeader) {
@@ -66,13 +68,15 @@ export class Remote {
         }
         console.trace(error);
         this._controller.togglePeriodicSessionRefresh(false);
-        this._controller.setCurrentPopup(
-          new AlertDialog({
-            heading: 'Connection failed',
-            message:
-              'Could not connect to the server. You may need to reload the page to reconnect.',
-          }),
-        );
+
+        this._connection_alert = new AlertDialog({
+          heading: 'Connection failed',
+          message:
+            'Could not connect to the server. You may need to reload the page to reconnect.',
+        });
+      })
+      .finally(() => {
+        this._controller.setCurrentPopup(this._connection_alert);
       });
   }
 

commit a2b917bce11032fa3b404310a042d1a1e47bc6d3

fix(web): do not close all popups on successful rpc requests
Fixes regression introduced in 250f43ae

web/src/remote.js
@@ -57,7 +57,10 @@ export class Remote {
           callback.call(context, payload, response_argument);
         }
 
-        this._connection_alert = null;
+        if (this._connection_alert) {
+          this._connection_alert.close();
+          this._connection_alert = null;
+        }
       })
       .catch((error) => {
         if (error.message === Remote._SessionHeader) {
@@ -74,8 +77,6 @@ export class Remote {
           message:
             'Could not connect to the server. You may need to reload the page to reconnect.',
         });
-      })
-      .finally(() => {
         this._controller.setCurrentPopup(this._connection_alert);
       });
   }