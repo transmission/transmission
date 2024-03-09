6909ec0badd5ea30e700beaad3a60642e6198a7d
Cœur <coeur@gmx.fr>
2024-03-10 00:27:20 +0800

    build: fix building on macOS 10.14.6, 10.15.7 and 11.7 (#6590)

--- libtransmission/variant.h
+++ libtransmission/variant.h
@@ -180,9 +180,9 @@ public:
     tr_variant& operator=(tr_variant&& that) noexcept = default;
 
     template<typename Val>
-    tr_variant(Val value)
+    tr_variant(Val&& value)
     {
-        *this = std::move(value);
+        *this = std::forward<Val>(value);
     }
 
     [[nodiscard]] static auto make_map(size_t const n_reserve = 0U) noexcept
