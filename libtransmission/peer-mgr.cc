224f34398c5de6f88f48b75a0ffab9bae9f7f10c
Cœur <coeur@gmx.fr>
2024-03-28 10:57:59 +0800

    Update peer-mgr.cc
    code review

--- libtransmission/peer-mgr.cc
+++ libtransmission/peer-mgr.cc
@@ -1828,8 +1828,6 @@ namespace peer_stat_helpers
 
 tr_peer_stat* tr_peerMgrPeerStats(tr_torrent const* tor, size_t* setme_count)
 {
-    using namespace peer_stat_helpers;
-
     TR_ASSERT(tr_isTorrent(tor));
     TR_ASSERT(tor->swarm->manager != nullptr);
 
