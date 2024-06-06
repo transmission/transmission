666e9265e062057e595b33042acf29163b6ddaf3
Alexandre Rossi <alexandre.rossi@gmail.com>
2024-06-06 22:28:29 +0200

    allow to use system includes for fast_float, fmt, utfcpp and googletest

--- cmake/FindFmt.cmake
+++ cmake/FindFmt.cmake
@@ -1,8 +1,14 @@
 add_library(fmt::fmt-header-only INTERFACE IMPORTED)
 
+find_path(FMT_INCLUDE_PATH
+    NAMES
+        fmt/core.h
+    HINTS
+        ${TR_THIRD_PARTY_SOURCE_DIR}/fmt/include)
+
 target_include_directories(fmt::fmt-header-only
     INTERFACE
-        ${TR_THIRD_PARTY_SOURCE_DIR}/fmt/include)
+        ${FMT_INCLUDE_PATH})
 
 target_compile_definitions(fmt::fmt-header-only
     INTERFACE
