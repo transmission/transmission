21aa04fcbef5b9bdfdfcb2bb814ac9d053348400
Rukario <kakashiru@yahoo.com>
2024-07-18 03:00:28 -0700

    Update transmission.js

--- web/src/transmission.js
+++ web/src/transmission.js
@@ -376,7 +376,7 @@ export class Transmission extends EventTarget {
         clearInterval(this.refreshTorrentsInterval);
         const callback = this.refreshTorrents.bind(this);
         const msec =
-          parseFloat(this.prefs.refresh_rate_sec) > 0
+          Number.parseFloat(this.prefs.refresh_rate_sec) > 0
             ? this.prefs.refresh_rate_sec * 1000
             : 1000;
         this.refreshTorrentsInterval = setInterval(callback, msec);
