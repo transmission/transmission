87bc960729e5bbaa7d98da5242c423f2d19a9055
Yat Ho <lagoho7@gmail.com>
2024-07-19 15:13:15 +0800

    chore: enable prettier for whole `transmission-web` package

--- web/stylelint.config.js
+++ web/stylelint.config.js
@@ -1,48 +1,44 @@
 /** @type {import('stylelint').Config} */
 export default {
-  "extends": [
-    "stylelint-config-sass-guidelines"
+  extends: ['stylelint-config-sass-guidelines'],
+  plugins: [
+    '@primer/stylelint-config/plugins/no-undefined-vars',
+    '@primer/stylelint-config/plugins/no-unused-vars',
   ],
-  "plugins":  [
-    "@primer/stylelint-config/plugins/no-undefined-vars",
-    "@primer/stylelint-config/plugins/no-unused-vars"
-  ],
-  "rules": {
-    "@stylistic/function-parentheses-space-inside": "never-single-line",
-    "block-no-empty": true,
-    "color-no-invalid-hex": true,
-    "comment-no-empty": true,
-    "declaration-block-no-duplicate-properties": true,
-    "declaration-block-no-shorthand-property-overrides": true,
-    "font-family-no-duplicate-names": true,
-    "function-calc-no-unspaced-operator": true,
-    "function-linear-gradient-no-nonstandard-direction": true,
-    "max-nesting-depth": null,
-    "media-feature-name-no-unknown": true,
-    "no-duplicate-at-import-rules": true,
-    "no-duplicate-selectors": null,
-    "no-empty-source": true,
-    "no-invalid-double-slash-comments": true,
-    "primer/no-undefined-vars": true,
-    "primer/no-unused-vars": true,
-    "property-no-unknown": true,
-    "property-no-vendor-prefix": null,
-    "scss/at-rule-no-unknown": true,
-    "selector-attribute-quotes": null,
-    "selector-max-compound-selectors": null,
-    "selector-max-id": null,
-    "selector-no-qualifying-type": [
+  rules: {
+    '@stylistic/function-parentheses-space-inside': 'never-single-line',
+    'block-no-empty': true,
+    'color-no-invalid-hex': true,
+    'comment-no-empty': true,
+    'declaration-block-no-duplicate-properties': true,
+    'declaration-block-no-shorthand-property-overrides': true,
+    'font-family-no-duplicate-names': true,
+    'function-calc-no-unspaced-operator': true,
+    'function-linear-gradient-no-nonstandard-direction': true,
+    'max-nesting-depth': null,
+    'media-feature-name-no-unknown': true,
+    'no-duplicate-at-import-rules': true,
+    'no-duplicate-selectors': null,
+    'no-empty-source': true,
+    'no-invalid-double-slash-comments': true,
+    'primer/no-undefined-vars': true,
+    'primer/no-unused-vars': true,
+    'property-no-unknown': true,
+    'property-no-vendor-prefix': null,
+    'scss/at-rule-no-unknown': true,
+    'selector-attribute-quotes': null,
+    'selector-max-compound-selectors': null,
+    'selector-max-id': null,
+    'selector-no-qualifying-type': [
       true,
       {
-        "ignore": [
-          "attribute"
-        ]
-      }
+        ignore: ['attribute'],
+      },
     ],
-    "selector-pseudo-class-no-unknown": true,
-    "selector-pseudo-element-no-unknown": true,
-    "selector-type-no-unknown": true,
-    "string-no-newline": true,
-    "unit-no-unknown": true,
-  }
+    'selector-pseudo-class-no-unknown': true,
+    'selector-pseudo-element-no-unknown': true,
+    'selector-type-no-unknown': true,
+    'string-no-newline': true,
+    'unit-no-unknown': true,
+  },
 };
