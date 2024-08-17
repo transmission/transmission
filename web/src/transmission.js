711df4a64dba009718f54a7908ef8aec2cba0e34
Yat Ho <lagoho7@gmail.com>
2024-08-17 08:53:03 +0800

    Revert "code review: use `URL.canParse()`"
    This reverts commit b1a436031ab154d8075762c91dd67d8c9eb954af.

--- web/src/transmission.js
+++ web/src/transmission.js
@@ -619,6 +619,15 @@ export class Transmission extends EventTarget {
     return true;
   }
 
+  static _isValidURL(string) {
+    try {
+      const url = new URL(string);
+      return Boolean(url);
+    } catch {
+      return false;
+    }
+  }
+
   shouldAddedTorrentsStart() {
     return this.session_properties['start-added-torrents'];
   }
@@ -637,7 +646,7 @@ export class Transmission extends EventTarget {
       .getData(type)
       .split('\n')
       .map((string) => string.trim())
-      .filter((string) => URL.canParse(string))) {
+      .filter((string) => Transmission._isValidURL(string))) {
       this.remote.addTorrentByUrl(uri, paused);
     }
 
