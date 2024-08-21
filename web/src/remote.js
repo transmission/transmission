769d6a5e8c3b71d3a57a35ca5c73cb2c39fcf8f7
Yat Ho <lagoho7@gmail.com>
2024-08-21 10:56:50 +0800

    refactor: change `torrent-stop` to `torrent_stop`

--- web/src/remote.js
+++ web/src/remote.js
@@ -220,7 +220,7 @@ export class Remote {
   }
   stopTorrents(torrent_ids, callback, context) {
     this.sendTorrentActionRequests(
-      'torrent-stop',
+      'torrent_stop',
       torrent_ids,
       callback,
       context,
