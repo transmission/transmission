ca4ad9f6f1bd0210588c677ca68bf6b356964046
Yat Ho <lagoho7@gmail.com>
2024-08-22 15:26:37 +0800

    refactor: change `priority-high` to `priority_high`

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -1180,7 +1180,7 @@ namespace make_torrent_field_helpers
             tr_torrentSetPeerLimit(tor, *val);
         }
 
-        if (auto const* val = args_in.find_if<tr_variant::Vector>(TR_KEY_priority_high);
+        if (auto const* val = args_in.find_if<tr_variant::Vector>({ TR_KEY_priority_high, TR_KEY_priority_high_kebab });
             val != nullptr && err == Error::SUCCESS)
         {
             std::tie(err, errmsg) = set_file_priorities(tor, TR_PRI_HIGH, *val);
@@ -1688,7 +1688,8 @@ void torrentAdd(tr_session* session, tr_variant::Map const& args_in, DoneCb&& do
         ctor.set_file_priorities(std::data(files), std::size(files), TR_PRI_NORMAL);
     }
 
-    if (auto const* val = args_in.find_if<tr_variant::Vector>(TR_KEY_priority_high))
+    if (auto const* val = args_in.find_if<tr_variant::Vector>({ TR_KEY_priority_high, TR_KEY_priority_high_kebab });
+        val != nullptr)
     {
         auto const files = file_list_from_list(*val);
         ctor.set_file_priorities(std::data(files), std::size(files), TR_PRI_HIGH);
