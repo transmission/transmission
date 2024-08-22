ed9ec27a145206339d5d4506ffd34ec34aec9969
Yat Ho <lagoho7@gmail.com>
2024-08-22 15:31:57 +0800

    refactor: change `priority-low` to `priority_low`

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -1224,7 +1224,8 @@ namespace make_torrent_field_helpers
             std::tie(err, errmsg) = set_file_priorities(tor, TR_PRI_HIGH, *val);
         }
 
-        if (auto const* val = args_in.find_if<tr_variant::Vector>(TR_KEY_priority_low); val != nullptr && err == Error::SUCCESS)
+        if (auto const* val = args_in.find_if<tr_variant::Vector>({ TR_KEY_priority_low, TR_KEY_priority_low_kebab });
+            val != nullptr && err == Error::SUCCESS)
         {
             std::tie(err, errmsg) = set_file_priorities(tor, TR_PRI_LOW, *val);
         }
@@ -1719,7 +1720,8 @@ void torrentAdd(tr_session* session, tr_variant::Map const& args_in, DoneCb&& do
         ctor.set_files_wanted(std::data(files), std::size(files), true);
     }
 
-    if (auto const val = args_in.find_if<tr_variant::Vector>(TR_KEY_priority_low))
+    if (auto const val = args_in.find_if<tr_variant::Vector>({ TR_KEY_priority_low, TR_KEY_priority_low_kebab });
+        val != nullptr)
     {
         auto const files = file_list_from_list(*val);
         ctor.set_file_priorities(std::data(files), std::size(files), TR_PRI_LOW);
