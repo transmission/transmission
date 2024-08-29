c5795b1517fc6c70ccc9bb0b3332079ecb0073c5
Yat Ho <lagoho7@gmail.com>
2024-08-29 11:34:23 +0800

    refactor: change `webseedsSendingToUs` to `webseeds_sending_to_us`

--- web/src/torrent.js
+++ web/src/torrent.js
@@ -247,7 +247,7 @@ export class Torrent extends EventTarget {
     return this.fields.uploaded_ever;
   }
   getWebseedsSendingToUs() {
-    return this.fields.webseedsSendingToUs;
+    return this.fields.webseeds_sending_to_us;
   }
   isFinished() {
     return this.fields.is_finished;
@@ -625,7 +625,7 @@ Torrent.Fields.Stats = [
   'download_dir',
   'uploaded_ever',
   'upload_ratio',
-  'webseedsSendingToUs',
+  'webseeds_sending_to_us',
 ];
 
 // fields used by the inspector which only need to be loaded once
