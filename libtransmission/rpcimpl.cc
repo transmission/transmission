a627f01c8982bd6ce17ddb829ac432e2888113a2
Yat Ho <lagoho7@gmail.com>
2024-08-29 11:29:34 +0800

    refactor: change `uploadRatio` to `upload_ratio`

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -804,7 +804,8 @@ namespace make_torrent_field_helpers
     case TR_KEY_upload_limit_camel:
     case TR_KEY_upload_limited:
     case TR_KEY_upload_limited_camel:
-    case TR_KEY_uploadRatio:
+    case TR_KEY_upload_ratio:
+    case TR_KEY_upload_ratio_camel:
     case TR_KEY_uploaded_ever:
     case TR_KEY_uploaded_ever_camel:
     case TR_KEY_wanted:
@@ -1013,7 +1014,9 @@ namespace make_torrent_field_helpers
     case TR_KEY_upload_limited:
     case TR_KEY_upload_limited_camel:
         return tor.uses_speed_limit(TR_UP);
-    case TR_KEY_uploadRatio: return st.ratio;
+    case TR_KEY_upload_ratio:
+    case TR_KEY_upload_ratio_camel:
+        return st.ratio;
     case TR_KEY_uploaded_ever:
     case TR_KEY_uploaded_ever_camel:
         return st.uploadedEver;
