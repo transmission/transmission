91d3e63c428e42a41dc3e1b28149ddeb77dfa6fe
Yat Ho <lagoho7@gmail.com>
2024-08-29 15:08:54 +0800

    refactor: change `clientIsInterested` to `client_is_interested`

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -611,7 +611,8 @@ namespace make_torrent_field_helpers
         peer_map.try_emplace(TR_KEY_address, peer.addr);
         peer_map.try_emplace(TR_KEY_client_is_choked, peer.clientIsChoked);
         peer_map.try_emplace(TR_KEY_client_is_choked_camel, peer.clientIsChoked);
-        peer_map.try_emplace(TR_KEY_clientIsInterested, peer.clientIsInterested);
+        peer_map.try_emplace(TR_KEY_client_is_interested, peer.clientIsInterested);
+        peer_map.try_emplace(TR_KEY_client_is_interested_camel, peer.clientIsInterested);
         peer_map.try_emplace(TR_KEY_client_name, peer.client);
         peer_map.try_emplace(TR_KEY_client_name_camel, peer.client);
         peer_map.try_emplace(TR_KEY_peer_id, tr_base64_encode(std::string_view{ peer.peer_id.data(), peer.peer_id.size() }));
