9ea710b3cf272525d58f662c68e209f312da8e77
Yat Ho <lagoho7@gmail.com>
2024-08-30 10:50:37 +0800

    refactor: change `lastScrapeSucceeded` to `last_scrape_succeeded`

--- libtransmission/quark.cc
+++ libtransmission/quark.cc
@@ -238,6 +238,7 @@ auto constexpr MyStatic = std::array<std::string_view, TR_N_KEYS>{
     "last_announce_timed_out"sv,
     "last_scrape_result"sv,
     "last_scrape_start_time"sv,
+    "last_scrape_succeeded"sv,
     "leecherCount"sv,
     "leftUntilDone"sv,
     "left_until_done"sv,
@@ -669,6 +670,7 @@ tr_quark tr_quark_convert(tr_quark q)
     case TR_KEY_last_announce_timed_out_camel: return TR_KEY_last_announce_timed_out;
     case TR_KEY_last_scrape_result_camel: return TR_KEY_last_scrape_result;
     case TR_KEY_last_scrape_start_time_camel: return TR_KEY_last_scrape_start_time;
+    case TR_KEY_last_scrape_succeeded_camel: return TR_KEY_last_scrape_succeeded;
     case TR_KEY_left_until_done_camel: return TR_KEY_left_until_done;
     case TR_KEY_magnet_link_camel: return TR_KEY_magnet_link;
     case TR_KEY_manual_announce_time_camel: return TR_KEY_manual_announce_time;
