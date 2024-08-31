7f727240bf06a9e0392bc2e2213fd7086b276939
Yat Ho <lagoho7@gmail.com>
2024-08-31 10:04:37 +0800

    refactor: change `compact-view` to `compact_view`

--- libtransmission/quark.h
+++ libtransmission/quark.h
@@ -77,6 +77,7 @@ enum
     TR_KEY_client_name,
     TR_KEY_comment,
     TR_KEY_comment_utf_8,
+    TR_KEY_compact_view_kebab,
     TR_KEY_compact_view,
     TR_KEY_complete,
     TR_KEY_config_dir,
