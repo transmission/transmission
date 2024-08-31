309f430dab513097a276c7ced3165f4e28746bc2
Yat Ho <lagoho7@gmail.com>
2024-08-31 10:36:48 +0800

    refactor: change `alt-speed-time-begin` to `alt_speed_time_begin`

--- libtransmission/quark.h
+++ libtransmission/quark.h
@@ -36,13 +36,14 @@ enum
     TR_KEY_address, /* rpc */
     TR_KEY_alt_speed_down_kebab, /* rpc, settings (deprecated) */
     TR_KEY_alt_speed_enabled_kebab, /* rpc, settings (deprecated) */
-    TR_KEY_alt_speed_time_begin, /* rpc, settings */
+    TR_KEY_alt_speed_time_begin_kebab, /* rpc, settings (deprecated) */
     TR_KEY_alt_speed_time_day, /* rpc, settings */
     TR_KEY_alt_speed_time_enabled, /* rpc, settings */
     TR_KEY_alt_speed_time_end, /* rpc, settings */
     TR_KEY_alt_speed_up, /* rpc, settings */
     TR_KEY_alt_speed_down, /* rpc, settings */
     TR_KEY_alt_speed_enabled, /* rpc, settings */
+    TR_KEY_alt_speed_time_begin, /* rpc, settings */
     TR_KEY_announce, /* metainfo */
     TR_KEY_announce_ip, /* metainfo, settings */
     TR_KEY_announce_ip_enabled, /* metainfo, settings */
