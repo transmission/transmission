e386fdc30f98e9af091e64e4a64b31d51e563448
Yat Ho <lagoho7@gmail.com>
2024-09-02 11:30:01 +0800

    refactor: change `download-dir-free-space` to `download_dir_free_space`

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -2219,7 +2219,9 @@ char const* sessionStats(tr_session* session, tr_variant::Map const& /*args_in*/
     case TR_KEY_download_dir:
     case TR_KEY_download_dir_kebab:
         return session.downloadDir();
-    case TR_KEY_download_dir_free_space: return tr_sys_path_get_capacity(session.downloadDir()).value_or(tr_sys_path_capacity{}).free;
+    case TR_KEY_download_dir_free_space:
+    case TR_KEY_download_dir_free_space_kebab:
+        return tr_sys_path_get_capacity(session.downloadDir()).value_or(tr_sys_path_capacity{}).free;
     case TR_KEY_download_queue_enabled: return session.queueEnabled(TR_DOWN);
     case TR_KEY_download_queue_size: return session.queueSize(TR_DOWN);
     case TR_KEY_encryption: return getEncryptionModeString(tr_sessionGetEncryption(&session));
