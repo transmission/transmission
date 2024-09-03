bd97940b80c07ffe3869452ed63d788db6259275
Yat Ho <lagoho7@gmail.com>
2024-09-03 17:11:12 +0800

    refactor: change `cumulative-stats` to `cumulative_stats`

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -2125,6 +2125,7 @@ char const* sessionStats(tr_session* session, tr_variant::Map const& /*args_in*/
     args_out.try_emplace(TR_KEY_active_torrent_count, n_running);
     args_out.try_emplace(TR_KEY_active_torrent_count_camel, n_running);
     args_out.try_emplace(TR_KEY_cumulative_stats, make_stats_map(session->stats().cumulative()));
+    args_out.try_emplace(TR_KEY_cumulative_stats_kebab, make_stats_map(session->stats().cumulative()));
     args_out.try_emplace(TR_KEY_current_stats, make_stats_map(session->stats().current()));
     args_out.try_emplace(TR_KEY_download_speed, session->piece_speed(TR_DOWN).base_quantity());
     args_out.try_emplace(TR_KEY_download_speed_camel, session->piece_speed(TR_DOWN).base_quantity());
