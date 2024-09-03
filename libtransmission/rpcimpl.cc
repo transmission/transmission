b40bf2a190b0417a81759c0a8b854d5ab5113cac
Yat Ho <lagoho7@gmail.com>
2024-09-03 16:58:50 +0800

    refactor: change `torrentCount` to `torrent_count`

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -2451,7 +2451,8 @@ void add_strings_from_var(std::set<std::string_view>& strings, tr_variant const&
     args_out.try_emplace(TR_KEY_download_speed_camel, session->piece_speed(TR_DOWN).base_quantity());
     args_out.try_emplace(TR_KEY_paused_torrent_count, total - n_running);
     args_out.try_emplace(TR_KEY_paused_torrent_count_camel, total - n_running);
-    args_out.try_emplace(TR_KEY_torrentCount, total);
+    args_out.try_emplace(TR_KEY_torrent_count, total);
+    args_out.try_emplace(TR_KEY_torrent_count_camel, total);
     args_out.try_emplace(TR_KEY_uploadSpeed, session->piece_speed(TR_UP).base_quantity());
 
     return { JsonRpc::Error::SUCCESS, {} };
