9b0d314db80882bb571f303bff8f17da820b9af0
Yat Ho <lagoho7@gmail.com>
2024-09-03 16:36:39 +0800

    refactor: change `session-set` to `session_set`

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -2796,7 +2796,7 @@ auto constexpr SyncHandlers = std::array<std::tuple<std::string_view, SyncHandle
     { "queue-move-up"sv, queueMoveUp, true },
     { "session-close"sv, sessionClose, true },
     { "session-get"sv, sessionGet, false },
-    { "session-set"sv, sessionSet, true },
+    { "session_set"sv, sessionSet, true },
     { "session-stats"sv, sessionStats, false },
     { "torrent_get"sv, torrentGet, false },
     { "torrent_reannounce"sv, torrentReannounce, true },
