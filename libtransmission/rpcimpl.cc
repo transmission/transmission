39241678564117628a97234a09b698d0e8a5005a
Yat Ho <lagoho7@gmail.com>
2024-09-04 10:31:26 +0800

    refactor: change `uploadedBytes` and `uploaded-bytes` to `uploaded_bytes`

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -2110,7 +2110,8 @@ char const* sessionStats(tr_session* session, tr_variant::Map const& /*args_in*/
         stats_map.try_emplace(TR_KEY_filesAdded, stats.filesAdded);
         stats_map.try_emplace(TR_KEY_secondsActive, stats.secondsActive);
         stats_map.try_emplace(TR_KEY_sessionCount, stats.sessionCount);
-        stats_map.try_emplace(TR_KEY_uploadedBytes, stats.uploadedBytes);
+        stats_map.try_emplace(TR_KEY_uploaded_bytes, stats.uploadedBytes);
+        stats_map.try_emplace(TR_KEY_uploaded_bytes_camel, stats.uploadedBytes);
         return stats_map;
     };
 
