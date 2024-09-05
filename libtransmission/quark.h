ef25de91803de766202ce4a2ae3f02677f7bdb07
Yat Ho <lagoho7@gmail.com>
2024-09-05 21:54:36 +0800

    refactor: change `announce-ip` to `announce_ip`

--- libtransmission/quark.h
+++ libtransmission/quark.h
@@ -50,10 +50,11 @@ enum
     TR_KEY_alt_speed_time_end, /* rpc, settings */
     TR_KEY_alt_speed_up, /* rpc, settings */
     TR_KEY_announce, /* metainfo */
-    TR_KEY_announce_ip, /* metainfo, settings */
+    TR_KEY_announce_ip_kebab, /* settings (legacy) */
     TR_KEY_announce_ip_enabled, /* metainfo, settings */
     TR_KEY_announce_list, /* metainfo */
     TR_KEY_announce_state_camel, /* rpc (deprecated) */
+    TR_KEY_announce_ip, /* settings */
     TR_KEY_announce_state, /* rpc */
     TR_KEY_anti_brute_force_enabled, /* rpc */
     TR_KEY_anti_brute_force_threshold, /* rpc */
