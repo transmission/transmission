66d20b2cd5b89ec70be7a8d50ac5a374ce4c5c61
Yat Ho <lagoho7@gmail.com>
2024-09-09 11:10:59 +0800

    refactor: change `rpc-host-whitelist` to `rpc_host_whitelist`

--- libtransmission/quark.cc
+++ libtransmission/quark.cc
@@ -452,6 +452,7 @@ auto constexpr MyStatic = std::array<std::string_view, TR_N_KEYS>{
     "rpc_authentication_required"sv,
     "rpc_bind_address"sv,
     "rpc_enabled"sv,
+    "rpc_host_whitelist"sv,
     "rpc_version"sv,
     "rpc_version_minimum"sv,
     "rpc_version_semver"sv,
@@ -862,6 +863,7 @@ tr_quark tr_quark_convert(tr_quark q)
     case TR_KEY_rpc_authentication_required_kebab: return TR_KEY_rpc_authentication_required;
     case TR_KEY_rpc_bind_address_kebab: return TR_KEY_rpc_bind_address;
     case TR_KEY_rpc_enabled_kebab: return TR_KEY_rpc_enabled;
+    case TR_KEY_rpc_host_whitelist_kebab: return TR_KEY_rpc_host_whitelist;
     case TR_KEY_rpc_version_kebab: return TR_KEY_rpc_version;
     case TR_KEY_rpc_version_minimum_kebab: return TR_KEY_rpc_version_minimum;
     case TR_KEY_rpc_version_semver_kebab: return TR_KEY_rpc_version_semver;
