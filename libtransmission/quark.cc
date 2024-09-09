795aeb9168aec578beac05a5208755f15b6e70ed
Yat Ho <lagoho7@gmail.com>
2024-09-09 11:07:50 +0800

    refactor: change `rpc-enabled` to `rpc_enabled`

--- libtransmission/quark.cc
+++ libtransmission/quark.cc
@@ -451,6 +451,7 @@ auto constexpr MyStatic = std::array<std::string_view, TR_N_KEYS>{
     "rpc-whitelist-enabled"sv,
     "rpc_authentication_required"sv,
     "rpc_bind_address"sv,
+    "rpc_enabled"sv,
     "rpc_version"sv,
     "rpc_version_minimum"sv,
     "rpc_version_semver"sv,
@@ -860,6 +861,7 @@ tr_quark tr_quark_convert(tr_quark q)
     case TR_KEY_rename_partial_files_kebab: return TR_KEY_rename_partial_files;
     case TR_KEY_rpc_authentication_required_kebab: return TR_KEY_rpc_authentication_required;
     case TR_KEY_rpc_bind_address_kebab: return TR_KEY_rpc_bind_address;
+    case TR_KEY_rpc_enabled_kebab: return TR_KEY_rpc_enabled;
     case TR_KEY_rpc_version_kebab: return TR_KEY_rpc_version;
     case TR_KEY_rpc_version_minimum_kebab: return TR_KEY_rpc_version_minimum;
     case TR_KEY_rpc_version_semver_kebab: return TR_KEY_rpc_version_semver;
