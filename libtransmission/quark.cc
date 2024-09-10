0f7a3a98ca07d96daa1791c3c6521d94f847e089
Yat Ho <lagoho7@gmail.com>
2024-09-10 11:15:56 +0800

    refactor: change `torrent-complete-notification-enabled` to `torrent_complete_notification_enabled`

--- libtransmission/quark.cc
+++ libtransmission/quark.cc
@@ -587,6 +587,7 @@ auto constexpr MyStatic = std::array<std::string_view, TR_N_KEYS>{
     "torrent_added"sv,
     "torrent_added_notification_enabled"sv,
     "torrent_added_verify_mode"sv,
+    "torrent_complete_notification_enabled"sv,
     "torrent_count"sv,
     "torrent_duplicate"sv,
     "torrent_file"sv,
@@ -944,6 +945,7 @@ tr_quark tr_quark_convert(tr_quark q)
     case TR_KEY_torrent_added_kebab: return TR_KEY_torrent_added;
     case TR_KEY_torrent_added_notification_enabled_kebab: return TR_KEY_torrent_added_notification_enabled;
     case TR_KEY_torrent_added_verify_mode_kebab: return TR_KEY_torrent_added_verify_mode;
+    case TR_KEY_torrent_complete_notification_enabled_kebab: return TR_KEY_torrent_complete_notification_enabled;
     case TR_KEY_torrent_count_camel: return TR_KEY_torrent_count;
     case TR_KEY_torrent_duplicate_kebab: return TR_KEY_torrent_duplicate;
     case TR_KEY_torrent_file_camel: return TR_KEY_torrent_file;
