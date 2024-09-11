1dac5508a83be74dce4b035f212a751cacc2bf8f
Yat Ho <lagoho7@gmail.com>
2024-09-11 20:49:52 +0800

    refactor: change `seeding-time-seconds` to `seeding_time_seconds`

--- libtransmission/resume.cc
+++ libtransmission/resume.cc
@@ -740,7 +740,7 @@ tr_resume::fields_t load_from_file(tr_torrent* tor, tr_torrent::ResumeHelper& he
 
     if ((fields_to_load & tr_resume::TimeSeeding) != 0)
     {
-        if (auto i = map.value_if<int64_t>(TR_KEY_seeding_time_seconds); i)
+        if (auto i = map.value_if<int64_t>({ TR_KEY_seeding_time_seconds, TR_KEY_seeding_time_seconds_kebab }); i)
         {
             helper.load_seconds_seeding_before_current_start(*i);
             fields_loaded |= tr_resume::TimeSeeding;
