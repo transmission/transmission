7a8a2198762498758bfd31150469150353d14ec0
Yat Ho <lagoho7@gmail.com>
2024-09-11 16:56:31 +0800

    refactor: change `details-window-height` to `details_window_height`

--- libtransmission/quark.cc
+++ libtransmission/quark.cc
@@ -122,6 +122,7 @@ auto constexpr MyStatic = std::array<std::string_view, TR_N_KEYS>{
     "destination"sv,
     "details-window-height"sv,
     "details-window-width"sv,
+    "details_window_height"sv,
     "dht-enabled"sv,
     "dht_enabled"sv,
     "dnd"sv,
@@ -794,6 +795,7 @@ tr_quark tr_quark_convert(tr_quark q)
     case TR_KEY_default_trackers_kebab: return TR_KEY_default_trackers;
     case TR_KEY_delete_local_data_kebab: return TR_KEY_delete_local_data;
     case TR_KEY_desired_available_camel: return TR_KEY_desired_available;
+    case TR_KEY_details_window_height_kebab: return TR_KEY_details_window_height;
     case TR_KEY_dht_enabled_kebab: return TR_KEY_dht_enabled;
     case TR_KEY_done_date_camel:
     case TR_KEY_done_date_kebab:
