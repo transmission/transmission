74320d2579ab7e8e25f5c40166da0b19ea025ebc
cdowen <hhylx1950@gmail.com>
2024-10-18 15:21:03 +0800

    Change log api
    Co-authored-by: Yat Ho <lagoho7@gmail.com>

--- libtransmission/peer-mgr.cc
+++ libtransmission/peer-mgr.cc
@@ -1136,7 +1136,7 @@ private:
                 if (peer->peer_info->is_blocklisted(blocklists_))
                 {
                     peer->disconnect_soon();
-                    tr_logAddDebug(fmt::format("Peer {} blocked in blocklists update", peer->display_name()));
+                    tr_logAddDebugTor(tor, fmt::format("Peer {} blocked in blocklists update", peer->display_name()));
                 }
             }
         }
