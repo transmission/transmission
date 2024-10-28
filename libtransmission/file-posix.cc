d6087b017fd1e660f7e1243e43dbfd0dbf30defd
Charles Kerr <charles@charleskerr.com>
2024-10-27 20:27:20 -0500

    chore: remove unused TR_SYS_FILE_APPEND

--- libtransmission/file-posix.cc
+++ libtransmission/file-posix.cc
@@ -565,7 +565,6 @@ tr_sys_file_t tr_sys_file_open(char const* path, int flags, int permissions, tr_
           { TR_SYS_FILE_READ | TR_SYS_FILE_WRITE, TR_SYS_FILE_READ, O_RDONLY },
           { TR_SYS_FILE_READ | TR_SYS_FILE_WRITE, TR_SYS_FILE_WRITE, O_WRONLY },
           { TR_SYS_FILE_CREATE, TR_SYS_FILE_CREATE, O_CREAT },
-          { TR_SYS_FILE_APPEND, TR_SYS_FILE_APPEND, O_APPEND },
           { TR_SYS_FILE_TRUNCATE, TR_SYS_FILE_TRUNCATE, O_TRUNC },
           { TR_SYS_FILE_SEQUENTIAL, TR_SYS_FILE_SEQUENTIAL, O_SEQUENTIAL } }
     };
