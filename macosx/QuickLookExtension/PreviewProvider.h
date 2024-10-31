307a3915c0ffbc5229630306fe30328b3f7aef24
Dzmitry Neviadomski <nevack.d@gmail.com>
2024-10-31 04:21:06 +0300

    feat: Add initial implementation of QuickLookExtension
    Signed-off-by: Dzmitry Neviadomski <nevack.d@gmail.com>

---
+++ macosx/QuickLookExtension/PreviewProvider.h
@@ -0,0 +1,14 @@
+//
+//  PreviewProvider.h
+//  QuickLookExtension
+//
+//  Created by Dzmitry Neviadomski on 18.10.24.
+//  Copyright © 2024 The Transmission Project. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Quartz/Quartz.h>
+
+@interface PreviewProvider : QLPreviewProvider <QLPreviewingController>
+
+@end
