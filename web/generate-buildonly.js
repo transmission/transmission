3a166592f476ffe9868eb2985d84d4677ad074c6
Yat Ho <lagoho7@gmail.com>
2024-10-31 10:18:55 +0800

    fix: support nodejs below v20.11 for `generate_buildonly.js`

--- web/generate-buildonly.js
+++ web/generate-buildonly.js
@@ -1,7 +1,9 @@
 import fs from 'node:fs';
 import path from 'node:path';
+import url from 'node:url';
 
-const __dirname = import.meta.dirname;
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const obj = JSON.parse(
   fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8'),
 );
