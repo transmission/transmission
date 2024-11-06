transmission/transmission

commit f1a53840f924c7bb4194ab6971c1a2d57f525ade

refactor: tr_web (#2633)
* refactor: fix tr_web rate limiting and dns caching

libtransmission/session.cc
@@ -118,6 +118,74 @@ tr_peer_id_t tr_peerIdInit()
 ****
 ***/
 
+std::optional<std::string> tr_session::WebController::cookieFile() const
+{
+    auto const str = tr_strvPath(session_->config_dir, "cookies.txt");
+    return tr_sys_path_exists(str.c_str(), nullptr) ? std::optional<std::string>{ str } : std::nullopt;
+}
+
+std::optional<std::string> tr_session::WebController::userAgent() const
+{
+    return tr_strvJoin(TR_NAME, "/"sv, SHORT_VERSION_STRING);
+}
+
+std::optional<std::string> tr_session::WebController::publicAddress() const
+{
+    for (auto const type : { TR_AF_INET, TR_AF_INET6 })
+    {
+        auto is_default_value = bool{};
+        tr_address const* addr = tr_sessionGetPublicAddress(session_, type, &is_default_value);
+        if (addr != nullptr && !is_default_value)
+        {
+            return tr_address_to_string(addr);
+        }
+    }
+
+    return std::nullopt;
+}
+
+unsigned int tr_session::WebController::clamp(int torrent_id, unsigned int byte_count) const
+{
+    auto const lock = session_->unique_lock();
+    auto const it = session_->torrentsById.find(torrent_id);
+    return it == std::end(session_->torrentsById) ? 0U : it->second->bandwidth->clamp(TR_DOWN, byte_count);
+}
+
+void tr_session::WebController::notifyBandwidthConsumed(int torrent_id, size_t byte_count)
+{
+    auto const lock = session_->unique_lock();
+    auto const it = session_->torrentsById.find(torrent_id);
+    if (it != std::end(session_->torrentsById))
+    {
+        it->second->bandwidth->notifyBandwidthConsumed(TR_DOWN, byte_count, true, tr_time_msec());
+    }
+}
+
+void tr_session::WebController::run(tr_web::FetchDoneFunc func, tr_web::FetchResponse&& response) const
+{
+    // marshall the `func` call into the libtransmission thread
+
+    using wrapper_t = std::pair<tr_web::FetchDoneFunc, tr_web::FetchResponse>;
+
+    auto constexpr callback = [](void* vwrapped)
+    {
+        auto* const wrapped = static_cast<wrapper_t*>(vwrapped);
+        wrapped->first(std::move(wrapped->second));
+        delete wrapped;
+    };
+
+    tr_runInEventThread(session_, callback, new wrapper_t{ func, std::move(response) });
+}
+
+void tr_sessionFetch(tr_session* session, tr_web::FetchOptions&& options)
+{
+    session->web->fetch(std::move(options));
+}
+
+/***
+****
+***/
+
 tr_encryption_mode tr_sessionGetEncryption(tr_session* session)
 {
     TR_ASSERT(session != nullptr);
@@ -692,6 +760,8 @@ static void tr_sessionInitImpl(void* vdata)
     tr_sessionSet(session, &settings);
 
     tr_udpInit(session);
+
+    session->web = tr_web::create(session->web_controller);
 
     if (session->isLPDEnabled)
     {
@@ -1799,7 +1869,7 @@ static void sessionCloseImplStart(tr_session* session)
 
     /* and this goes *after* announcer close so that
        it won't be idle until the announce events are sent... */
-    tr_webClose(session, TR_WEB_CLOSE_WHEN_IDLE);
+    session->web->closeSoon();
 
     tr_cacheFree(session->cache);
     session->cache = nullptr;
@@ -1890,7 +1960,7 @@ void tr_sessionClose(tr_session* session)
      * so we need to keep the transmission thread alive
      * for a bit while they tell the router & tracker
      * that we're closing now */
-    while ((session->shared != nullptr || session->web != nullptr || session->announcer != nullptr ||
+    while ((session->shared != nullptr || !session->web->isClosed() || session->announcer != nullptr ||
             session->announcer_udp != nullptr) &&
            !deadlineReached(deadline))
     {
@@ -1903,7 +1973,7 @@ void tr_sessionClose(tr_session* session)
         tr_wait_msec(50);
     }
 
-    tr_webClose(session, TR_WEB_CLOSE_NOW);
+    session->web.reset();
 
     /* close the libtransmission thread */
     tr_eventClose(session);

libtransmission/session.h
@@ -29,6 +29,7 @@
 
 #include "net.h" // tr_socket_t
 #include "quark.h"
+#include "web.h"
 
 enum tr_auto_switch_state_t
 {
@@ -44,6 +45,7 @@ struct evdns_base;
 
 class tr_bitfield;
 class tr_rpc_server;
+class tr_web;
 struct Bandwidth;
 struct tr_address;
 struct tr_announcer;
@@ -334,7 +336,29 @@ public:
 
     struct tr_cache* cache;
 
-    struct tr_web* web;
+    class WebController final : public tr_web::Controller
+    {
+    public:
+        explicit WebController(tr_session* session)
+            : session_{ session }
+        {
+        }
+        ~WebController() override = default;
+
+        [[nodiscard]] std::optional<std::string> cookieFile() const override;
+        [[nodiscard]] std::optional<std::string> publicAddress() const override;
+        [[nodiscard]] std::optional<std::string> userAgent() const override;
+        [[nodiscard]] unsigned int clamp(int bandwidth_tag, unsigned int byte_count) const override;
+        void notifyBandwidthConsumed(int torrent_id, size_t byte_count) override;
+        // runs the tr_web::fetch response callback in the libtransmission thread
+        void run(tr_web::FetchDoneFunc func, tr_web::FetchResponse&& response) const override;
+
+    private:
+        tr_session* const session_;
+    };
+
+    WebController web_controller{ this };
+    std::unique_ptr<tr_web> web;
 
     struct tr_session_id* session_id;
 

libtransmission/web.h
@@ -5,48 +5,142 @@
 
 #pragma once
 
-#include <cstdint>
 #include <optional>
+#include <memory>
 #include <string>
 #include <string_view>
 
-#include "transmission.h"
-
 struct evbuffer;
 
-enum tr_web_close_mode
-{
-    TR_WEB_CLOSE_WHEN_IDLE,
-    TR_WEB_CLOSE_NOW
-};
-
-void tr_webClose(tr_session* session, tr_web_close_mode close_mode);
-
-using tr_web_done_func = void (*)(
-    tr_session* session,
-    bool did_connect_flag,
-    bool timeout_flag,
-    long response_code,
-    std::string_view response,
-    void* user_data);
-
-class tr_web_options
+class tr_web
 {
 public:
-    tr_web_options(std::string_view url_in, tr_web_done_func done_func_in, void* done_func_user_data_in)
-        : url{ url_in }
-        , done_func{ done_func_in }
-        , done_func_user_data{ done_func_user_data_in }
+    // The response struct passed to the user's FetchDoneFunc callback
+    // when a fetch() finishes.
+    struct FetchResponse
     {
-    }
+        long status; // http server response, e.g. 200
+        std::string body;
+        bool did_connect;
+        bool did_timeout;
+        void* user_data;
+    };
 
-    std::string url;
-    std::optional<int> torrent_id;
-    tr_web_done_func done_func = nullptr;
-    void* done_func_user_data = nullptr;
-    std::string range;
-    std::string cookies;
-    evbuffer* buffer = nullptr;
+    using FetchDoneFunc = void (*)(FetchResponse&& response);
+
+    class FetchOptions
+    {
+    public:
+        FetchOptions(std::string_view url_in, FetchDoneFunc done_func_in, void* done_func_user_data_in)
+            : url{ url_in }
+            , done_func{ done_func_in }
+            , done_func_user_data{ done_func_user_data_in }
+        {
+        }
+
+        // the URL to fetch
+        std::string url;
+
+        // Callback to invoke with a FetchResponse when done
+        FetchDoneFunc done_func = nullptr;
+        void* done_func_user_data = nullptr;
+
+        // If you need to set multiple cookies, set them all using a single
+        // option concatenated like this: "name1=content1; name2=content2;"
+        std::optional<std::string> cookies;
+
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Range_requests
+        std::optional<std::string> range;
+
+        // Tag used by tr_web::Controller to limit some transfers' bandwidth
+        std::optional<int> speed_limit_tag;
+
+        // Optionaly set the underlying sockets' send/receive buffers' size.
+        // Can be useful for scrapes / announces where the payload is known
+        // to be small.
+        std::optional<int> sndbuf;
+        std::optional<int> rcvbuf;
+
+        // Maximum time to wait before timeout
+        int timeout_secs = DefaultTimeoutSecs;
+
+        // If provided, this buffer will be used to hold the response body.
+        // Provided for webseeds, which need to set low-level callbacks on
+        // the buffer itself.
+        evbuffer* buffer = nullptr;
+
+        static constexpr int DefaultTimeoutSecs = 120;
+    };
+
+    void fetch(FetchOptions&& options);
+
+    // Notify tr_web that it's going to be destroyed sooon.
+    // New fetch() tasks will be rejected, but already-running tasks
+    // are left alone so that they can finish.
+    void closeSoon();
+
+    // True when tr_web is ready to be destroyed.
+    // Will never be true until after closeSoon() is called.
+    [[nodiscard]] bool isClosed() const;
+
+    // closeSoon() *should* be called first, but OK to destroy tr_web before
+    // isClosed() is true, e.g. there could be a hung fetch task that hasn't
+    // timmed out yet. Deleting the tr_web object will force-terminate any
+    // pending tasks.
+    ~tr_web();
+
+    /**
+     * Mediates between tr_web and its clients.
+     *
+     * NB: Note that tr_web calls all these methods in the web thread.
+     */
+    class Controller
+    {
+    public:
+        virtual ~Controller() = default;
+
+        // Return the location of the cookie file, or nullopt to not use one
+        [[nodiscard]] virtual std::optional<std::string> cookieFile() const
+        {
+            return std::nullopt;
+        }
+
+        // Return the preferred user public address string, or nullopt to not use one
+        [[nodiscard]] virtual std::optional<std::string> publicAddress() const
+        {
+            return std::nullopt;
+        }
+
+        // Return the preferred user aagent, or nullopt to not use one
+        [[nodiscard]] virtual std::optional<std::string> userAgent() const
+        {
+            return std::nullopt;
+        }
+
+        // Notify the system that `byte_count` of download bandwidth was used
+        virtual void notifyBandwidthConsumed([[maybe_unused]] int bandwidth_tag, [[maybe_unused]] size_t byte_count)
+        {
+        }
+
+        // Return the number of bytes that should be allowed. See Bandwidth::clamp()
+        [[nodiscard]] virtual unsigned int clamp([[maybe_unused]] int bandwidth_tag, unsigned int byte_count) const
+        {
+            return byte_count;
+        }
+
+        // Invoke the user-provided fetch callback
+        virtual void run(FetchDoneFunc func, FetchResponse&& response) const
+        {
+            func(std::move(response));
+        }
+    };
+
+    static std::unique_ptr<tr_web> create(Controller& controller);
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> const impl_;
+    explicit tr_web(Controller& controller);
 };
 
-void tr_webRun(tr_session* session, tr_web_options&& options);
+void tr_sessionFetch(struct tr_session* session, tr_web::FetchOptions&& options);

cli/cli.cc
@@ -19,7 +19,7 @@
 #include <libtransmission/variant.h>
 #include <libtransmission/version.h>
 #include <libtransmission/web-utils.h>
-#include <libtransmission/web.h> /* tr_webRun */
+#include <libtransmission/web.h> // tr_sessionFetch()
 
 /***
 ****
@@ -124,16 +124,10 @@ static char* tr_strlratio(char* buf, double ratio, size_t buflen)
 
 static bool waitingOnWeb;
 
-static void onTorrentFileDownloaded(
-    tr_session* /*session*/,
-    bool /*did_connect*/,
-    bool /*did_timeout*/,
-    long /*response_code*/,
-    std::string_view response,
-    void* vctor)
+static void onTorrentFileDownloaded(tr_web::FetchResponse&& response)
 {
-    auto* ctor = static_cast<tr_ctor*>(vctor);
-    tr_ctorSetMetainfo(ctor, std::data(response), std::size(response), nullptr);
+    auto* ctor = static_cast<tr_ctor*>(response.user_data);
+    tr_ctorSetMetainfo(ctor, std::data(response.body), std::size(response.body), nullptr);
     waitingOnWeb = false;
 }
 
@@ -286,7 +280,7 @@ int tr_main(int argc, char* argv[])
     else if (tr_urlIsValid(torrentPath))
     {
         // fetch it
-        tr_webRun(h, { torrentPath, onTorrentFileDownloaded, ctor });
+        tr_sessionFetch(h, { torrentPath, onTorrentFileDownloaded, ctor });
         waitingOnWeb = true;
         while (waitingOnWeb)
         {

gtk/FaviconCache.cc
@@ -10,7 +10,7 @@
 #include <glib/gstdio.h> /* g_remove() */
 
 #include <libtransmission/transmission.h>
-#include <libtransmission/web.h> /* tr_webRun() */
+#include <libtransmission/web.h> // tr_sessionFetch()
 #include <libtransmission/web-utils.h>
 
 #include "FaviconCache.h"
@@ -73,7 +73,7 @@ Glib::RefPtr<Gdk::Pixbuf> favicon_load_from_cache(std::string const& host)
     }
 }
 
-void favicon_web_done_cb(tr_session*, bool, bool, long, std::string_view, gpointer);
+void favicon_web_done_cb(tr_web::FetchResponse&& response);
 
 bool favicon_web_done_idle_cb(std::unique_ptr<favicon_data> fav)
 {
@@ -90,7 +90,7 @@ bool favicon_web_done_idle_cb(std::unique_ptr<favicon_data> fav)
         fav->contents.clear();
         auto* const session = fav->session;
         auto const next_url = get_url(fav->host, fav->type);
-        tr_webRun(session, { next_url.raw(), favicon_web_done_cb, fav.release() });
+        tr_sessionFetch(session, { next_url.raw(), favicon_web_done_cb, fav.release() });
     }
 
     // Not released into the next web request, means we're done trying (even if `pixbuf` is still invalid)
@@ -102,17 +102,10 @@ bool favicon_web_done_idle_cb(std::unique_ptr<favicon_data> fav)
     return false;
 }
 
-void favicon_web_done_cb(
-    tr_session* /*session*/,
-    bool /*did_connect*/,
-    bool /*did_timeout*/,
-    long /*code*/,
-    std::string_view data,
-    gpointer vfav)
+void favicon_web_done_cb(tr_web::FetchResponse&& response)
 {
-    auto* fav = static_cast<favicon_data*>(vfav);
-    fav->contents.assign(std::data(data), std::size(data));
-
+    auto* const fav = static_cast<favicon_data*>(response.user_data);
+    fav->contents = response.body;
     Glib::signal_idle().connect([fav]() { return favicon_web_done_idle_cb(std::unique_ptr<favicon_data>(fav)); });
 }
 
@@ -136,7 +129,7 @@ void gtr_get_favicon(
         data->func = pixbuf_ready_func;
         data->host = host;
 
-        tr_webRun(session, { get_url(host, 0).raw(), favicon_web_done_cb, data.release() });
+        tr_sessionFetch(session, { get_url(host, 0).raw(), favicon_web_done_cb, data.release() });
     }
 }
 

libtransmission/announcer-http.cc
@@ -121,26 +121,6 @@ static std::string announce_url_new(tr_session const* session, tr_announce_reque
     return evbuffer_free_to_str(buf);
 }
 
-struct announce_data
-{
-    tr_announce_response response;
-    tr_announce_response_func response_func;
-    void* response_func_user_data;
-    char log_name[128];
-};
-
-static void on_announce_done_eventthread(void* vdata)
-{
-    auto* data = static_cast<struct announce_data*>(vdata);
-
-    if (data->response_func != nullptr)
-    {
-        data->response_func(&data->response, data->response_func_user_data);
-    }
-
-    delete data;
-}
-
 static void verboseLog(std::string_view description, tr_direction direction, std::string_view message)
 {
     auto& out = std::cerr;
@@ -293,14 +273,17 @@ void tr_announcerParseHttpAnnounceResponse(tr_announce_response& response, std::
     }
 }
 
-static void on_announce_done(
-    tr_session* session,
-    bool did_connect,
-    bool did_timeout,
-    long response_code,
-    std::string_view msg,
-    void* vdata)
+struct announce_data
 {
+    tr_announce_response response;
+    tr_announce_response_func response_func;
+    void* response_func_user_data;
+    char log_name[128];
+};
+
+static void onAnnounceDone(tr_web::FetchResponse&& web_response)
+{
+    auto const& [status, body, did_connect, did_timeout, vdata] = web_response;
     auto* data = static_cast<struct announce_data*>(vdata);
 
     tr_announce_response* const response = &data->response;
@@ -308,14 +291,14 @@ static void on_announce_done(
     response->did_timeout = did_timeout;
     dbgmsg(data->log_name, "Got announce response");
 
-    if (response_code != HTTP_OK)
+    if (status != HTTP_OK)
     {
-        auto const* const response_str = tr_webGetResponseStr(response_code);
-        response->errmsg = tr_strvJoin("Tracker HTTP response "sv, std::to_string(response_code), " ("sv, response_str, ")"sv);
+        auto const* const response_str = tr_webGetResponseStr(status);
+        response->errmsg = tr_strvJoin("Tracker HTTP response "sv, std::to_string(status), " ("sv, response_str, ")"sv);
     }
     else
     {
-        tr_announcerParseHttpAnnounceResponse(*response, msg);
+        tr_announcerParseHttpAnnounceResponse(*response, body);
     }
 
     if (!std::empty(response->pex6))
@@ -328,7 +311,12 @@ static void on_announce_done(
         dbgmsg(data->log_name, "got a peers length of %zu", std::size(response->pex));
     }
 
-    tr_runInEventThread(session, on_announce_done_eventthread, data);
+    if (data->response_func != nullptr)
+    {
+        data->response_func(&data->response, data->response_func_user_data);
+    }
+
+    delete data;
 }
 
 void tr_tracker_http_announce(
@@ -345,7 +333,12 @@ void tr_tracker_http_announce(
 
     auto const url = announce_url_new(session, request);
     dbgmsg(request->log_name, "Sending announce to libcurl: \"%" TR_PRIsv "\"", TR_PRIsv_ARG(url));
-    tr_webRun(session, { url, on_announce_done, d });
+
+    auto options = tr_web::FetchOptions{ url, onAnnounceDone, d };
+    options.timeout_secs = 90L;
+    options.sndbuf = 1024;
+    options.rcvbuf = 3072;
+    session->web->fetch(std::move(options));
 }
 
 /****
@@ -353,26 +346,6 @@ void tr_tracker_http_announce(
 *****  SCRAPE
 *****
 ****/
-
-struct scrape_data
-{
-    tr_scrape_response response;
-    tr_scrape_response_func response_func;
-    void* response_func_user_data;
-    char log_name[128];
-};
-
-static void on_scrape_done_eventthread(void* vdata)
-{
-    auto* data = static_cast<struct scrape_data*>(vdata);
-
-    if (data->response_func != nullptr)
-    {
-        data->response_func(&data->response, data->response_func_user_data);
-    }
-
-    delete data;
-}
 
 void tr_announcerParseHttpScrapeResponse(tr_scrape_response& response, std::string_view benc)
 {
@@ -464,15 +437,18 @@ void tr_announcerParseHttpScrapeResponse(tr_scrape_response& response, std::stri
     }
 }
 
-static void on_scrape_done(
-    tr_session* session,
-    bool did_connect,
-    bool did_timeout,
-    long response_code,
-    std::string_view msg,
-    void* vdata)
+struct scrape_data
 {
-    auto* data = static_cast<struct scrape_data*>(vdata);
+    tr_scrape_response response;
+    tr_scrape_response_func response_func;
+    void* response_func_user_data;
+    char log_name[128];
+};
+
+static void onScrapeDone(tr_web::FetchResponse&& web_response)
+{
+    auto const& [status, body, did_connect, did_timeout, vdata] = web_response;
+    auto* const data = static_cast<struct scrape_data*>(vdata);
 
     tr_scrape_response& response = data->response;
     response.did_connect = did_connect;
@@ -481,20 +457,22 @@ static void on_scrape_done(
     auto const scrape_url_sv = response.scrape_url.sv();
     dbgmsg(data->log_name, "Got scrape response for \"%" TR_PRIsv "\"", TR_PRIsv_ARG(scrape_url_sv));
 
-    if (response_code != HTTP_OK)
+    if (status != HTTP_OK)
     {
-        char const* fmt = _("Tracker gave HTTP response code %1$ld (%2$s)");
-        char const* response_str = tr_webGetResponseStr(response_code);
-        char buf[512];
-        tr_snprintf(buf, sizeof(buf), fmt, response_code, response_str);
-        response.errmsg = buf;
+        auto const* const response_str = tr_webGetResponseStr(status);
+        response.errmsg = tr_strvJoin("Tracker HTTP response "sv, std::to_string(status), " ("sv, response_str, ")"sv);
     }
     else
     {
-        tr_announcerParseHttpScrapeResponse(response, msg);
+        tr_announcerParseHttpScrapeResponse(response, body);
     }
 
-    tr_runInEventThread(session, on_scrape_done_eventthread, data);
+    if (data->response_func != nullptr)
+    {
+        data->response_func(&data->response, data->response_func_user_data);
+    }
+
+    delete data;
 }
 
 static std::string scrape_url_new(tr_scrape_request const* req)
@@ -540,5 +518,10 @@ void tr_tracker_http_scrape(
 
     auto const url = scrape_url_new(request);
     dbgmsg(request->log_name, "Sending scrape to libcurl: \"%" TR_PRIsv "\"", TR_PRIsv_ARG(url));
-    tr_webRun(session, { url, on_scrape_done, d });
+
+    auto options = tr_web::FetchOptions{ url, onScrapeDone, d };
+    options.timeout_secs = 30L;
+    options.sndbuf = 4096;
+    options.rcvbuf = 4096;
+    session->web->fetch(std::move(options));
 }

libtransmission/rpcimpl.cc
@@ -1340,29 +1340,19 @@ static char const* torrentRenamePath(
 ****
 ***/
 
-static void portTested(
-    tr_session* /*session*/,
-    bool /*did_connect*/,
-    bool /*did_timeout*/,
-    long response_code,
-    std::string_view response,
-    void* user_data)
+static void portTested(tr_web::FetchResponse&& web_response)
 {
+    auto const& [status, body, did_connect, did_tmieout, user_data] = web_response;
     char result[1024];
     auto* data = static_cast<struct tr_rpc_idle_data*>(user_data);
 
-    if (response_code != 200)
+    if (status != 200)
     {
-        tr_snprintf(
-            result,
-            sizeof(result),
-            "portTested: http error %ld: %s",
-            response_code,
-            tr_webGetResponseStr(response_code));
+        tr_snprintf(result, sizeof(result), "portTested: http error %ld: %s", status, tr_webGetResponseStr(status));
     }
     else /* success */
     {
-        bool const isOpen = tr_strvStartsWith(response, '1');
+        bool const isOpen = tr_strvStartsWith(body, '1');
         tr_variantDictAddBool(data->args_out, TR_KEY_port_is_open, isOpen);
         tr_snprintf(result, sizeof(result), "success");
     }
@@ -1378,7 +1368,7 @@ static char const* portTest(
 {
     auto const port = tr_sessionGetPeerPort(session);
     auto const url = tr_strvJoin("https://portcheck.transmissionbt.com/"sv, std::to_string(port));
-    tr_webRun(session, { url, portTested, idle_data });
+    session->web->fetch({ url, portTested, idle_data });
     return nullptr;
 }
 
@@ -1386,28 +1376,18 @@ static char const* portTest(
 ****
 ***/
 
-static void gotNewBlocklist(
-    tr_session* session,
-    bool /*did_connect*/,
-    bool /*did_timeout*/,
-    long response_code,
-    std::string_view response,
-    void* user_data)
+static void gotNewBlocklist(tr_web::FetchResponse&& web_response)
 {
-    char result[1024];
+    auto const& [status, body, did_connect, did_timeout, user_data] = web_response;
     auto* data = static_cast<struct tr_rpc_idle_data*>(user_data);
+    auto* const session = data->session;
 
-    *result = '\0';
+    char result[1024] = {};
 
-    if (response_code != 200)
+    if (status != 200)
     {
         // we failed to download the blocklist...
-        tr_snprintf(
-            result,
-            sizeof(result),
-            "gotNewBlocklist: http error %ld: %s",
-            response_code,
-            tr_webGetResponseStr(response_code));
+        tr_snprintf(result, sizeof(result), "gotNewBlocklist: http error %ld: %s", status, tr_webGetResponseStr(status));
         tr_idle_function_done(data, result);
         return;
     }
@@ -1422,8 +1402,8 @@ static void gotNewBlocklist(
         auto actual_size = size_t{};
         auto const decompress_result = libdeflate_gzip_decompress(
             decompressor.get(),
-            std::data(response),
-            std::size(response),
+            std::data(body),
+            std::size(body),
             std::data(content),
             std::size(content),
             &actual_size);
@@ -1436,7 +1416,7 @@ static void gotNewBlocklist(
         if (decompress_result == LIBDEFLATE_BAD_DATA)
         {
             // couldn't decompress it; maybe we downloaded an uncompressed file
-            content.assign(std::begin(response), std::end(response));
+            content.assign(std::begin(body), std::end(body));
         }
         break;
     }
@@ -1466,7 +1446,7 @@ static char const* blocklistUpdate(
     tr_variant* /*args_out*/,
     struct tr_rpc_idle_data* idle_data)
 {
-    tr_webRun(session, { session->blocklistUrl(), gotNewBlocklist, idle_data });
+    session->web->fetch({ session->blocklistUrl(), gotNewBlocklist, idle_data });
     return nullptr;
 }
 
@@ -1521,36 +1501,26 @@ struct add_torrent_idle_data
     tr_ctor* ctor;
 };
 
-static void gotMetadataFromURL(
-    tr_session* /*session*/,
-    bool /*did_connect*/,
-    bool /*did_timeout*/,
-    long response_code,
-    std::string_view response,
-    void* user_data)
+static void gotMetadataFromURL(tr_web::FetchResponse&& web_response)
 {
+    auto const& [status, body, did_connect, did_timeout, user_data] = web_response;
     auto* data = static_cast<struct add_torrent_idle_data*>(user_data);
 
     dbgmsg(
         "torrentAdd: HTTP response code was %ld (%s); response length was %zu bytes",
-        response_code,
-        tr_webGetResponseStr(response_code),
-        std::size(response));
+        status,
+        tr_webGetResponseStr(status),
+        std::size(body));
 
-    if (response_code == 200 || response_code == 221) /* http or ftp success.. */
+    if (status == 200 || status == 221) /* http or ftp success.. */
     {
-        tr_ctorSetMetainfo(data->ctor, std::data(response), std::size(response), nullptr);
+        tr_ctorSetMetainfo(data->ctor, std::data(body), std::size(body), nullptr);
         addTorrentImpl(data->data, data->ctor);
     }
     else
     {
         char result[1024];
-        tr_snprintf(
-            result,
-            sizeof(result),
-            "gotMetadataFromURL: http error %ld: %s",
-            response_code,
-            tr_webGetResponseStr(response_code));
+        tr_snprintf(result, sizeof(result), "gotMetadataFromURL: http error %ld: %s", status, tr_webGetResponseStr(status));
         tr_idle_function_done(data->data, result);
     }
 
@@ -1686,9 +1656,9 @@ static char const* torrentAdd(tr_session* session, tr_variant* args_in, tr_varia
         d->data = idle_data;
         d->ctor = ctor;
 
-        auto options = tr_web_options{ filename, gotMetadataFromURL, d };
+        auto options = tr_web::FetchOptions{ filename, gotMetadataFromURL, d };
         options.cookies = cookies;
-        tr_webRun(session, std::move(options));
+        session->web->fetch(std::move(options));
     }
     else
     {

commit fef9110a788adf683423b86f16420087a4a3c524

fix: don't double count webseed download data rate
Regression from f1a53840f924c7bb4194ab6971c1a2d57f525ade

libtransmission/session.cc
@@ -341,16 +341,6 @@ size_t tr_session::WebMediator::clamp(int torrent_id, size_t byte_count) const
     return tor == nullptr ? 0U : tor->bandwidth().clamp(TR_DOWN, byte_count);
 }
 
-void tr_session::WebMediator::notifyBandwidthConsumed(int torrent_id, size_t byte_count)
-{
-    auto const lock = session_->unique_lock();
-
-    if (auto* const tor = session_->torrents().get(torrent_id); tor != nullptr)
-    {
-        tor->bandwidth().notify_bandwidth_consumed(TR_DOWN, byte_count, true, tr_time_msec());
-    }
-}
-
 void tr_session::WebMediator::run(tr_web::FetchDoneFunc&& func, tr_web::FetchResponse&& response) const
 {
     session_->run_in_session_thread(std::move(func), std::move(response));

libtransmission/session.h
@@ -244,7 +244,6 @@ private:
         [[nodiscard]] std::optional<std::string_view> userAgent() const override;
         [[nodiscard]] size_t clamp(int torrent_id, size_t byte_count) const override;
         [[nodiscard]] time_t now() const override;
-        void notifyBandwidthConsumed(int torrent_id, size_t byte_count) override;
         // runs the tr_web::fetch response callback in the libtransmission thread
         void run(tr_web::FetchDoneFunc&& func, tr_web::FetchResponse&& response) const override;
 

libtransmission/web.cc
@@ -482,8 +482,6 @@ public:
                 task->impl.paused_easy_handles.emplace(task->easy(), tr_time_msec());
                 return CURL_WRITEFUNC_PAUSE;
             }
-
-            task->impl.mediator.notifyBandwidthConsumed(*tag, bytes_used);
         }
 
         evbuffer_add(task->body(), data, bytes_used);

libtransmission/web.h
@@ -143,11 +143,6 @@ public:
             return std::nullopt;
         }
 
-        // Notify the system that `byte_count` of download bandwidth was used
-        virtual void notifyBandwidthConsumed([[maybe_unused]] int bandwidth_tag, [[maybe_unused]] size_t byte_count)
-        {
-        }
-
         // Return the number of bytes that should be allowed. See tr_bandwidth::clamp()
         [[nodiscard]] virtual size_t clamp([[maybe_unused]] int bandwidth_tag, size_t byte_count) const
         {