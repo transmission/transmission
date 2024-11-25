b086c3e806ab5acce64065da185a68c778ad9618
Yat Ho <lagoho7@gmail.com>
2024-11-25 16:52:46 +0800

    Revert "fix: possible heap-use-after-free with magnet links (#6815)"
    This reverts commit 09b67c84b1f17ed22b7cce0731867e0029c62fbe.

--- libtransmission/torrent-magnet.cc
+++ libtransmission/torrent-magnet.cc
@@ -68,15 +68,6 @@ tr_metadata_download::tr_metadata_download(std::string_view log_name, int64_t co
     create_all_needed(n);
 }
 
-void tr_torrent::do_magnet_idle_work()
-{
-    if (auto& m = metadata_download_; m && m->is_complete())
-    {
-        tr_logAddDebugTor(this, fmt::format("we now have all the metainfo!"));
-        on_have_all_metainfo();
-    }
-}
-
 void tr_torrent::maybe_start_metadata_transfer(int64_t const size) noexcept
 {
     if (has_metainfo() || metadata_download_)
@@ -271,20 +262,20 @@ void tr_torrent::on_have_all_metainfo()
     m.reset();
 }
 
-void tr_metadata_download::set_metadata_piece(int64_t const piece, void const* const data, size_t const len)
+bool tr_metadata_download::set_metadata_piece(int64_t const piece, void const* const data, size_t const len)
 {
     TR_ASSERT(data != nullptr);
 
     // sanity test: is `piece` in range?
     if (piece < 0 || piece >= piece_count_)
     {
-        return;
+        return false;
     }
 
     // sanity test: is `len` the right size?
     if (get_piece_length(piece) != len)
     {
-        return;
+        return false;
     }
 
     // do we need this piece?
@@ -295,7 +286,7 @@ void tr_metadata_download::set_metadata_piece(int64_t const piece, void const* c
         [piece](auto const& item) { return item.piece == piece; });
     if (iter == std::end(needed))
     {
-        return;
+        return false;
     }
 
     auto const offset = piece * MetadataPieceSize;
@@ -303,6 +294,8 @@ void tr_metadata_download::set_metadata_piece(int64_t const piece, void const* c
 
     needed.erase(iter);
     tr_logAddDebugMagnet(this, fmt::format("saving metainfo piece {}... {} remain", piece, std::size(needed)));
+
+    return std::empty(needed);
 }
 
 void tr_torrent::set_metadata_piece(int64_t const piece, void const* const data, size_t const len)
@@ -311,9 +304,10 @@ void tr_torrent::set_metadata_piece(int64_t const piece, void const* const data,
 
     tr_logAddDebugTor(this, fmt::format("got metadata piece {} of {} bytes", piece, len));
 
-    if (auto& m = metadata_download_)
+    if (auto& m = metadata_download_; m && m->set_metadata_piece(piece, data, len))
     {
-        m->set_metadata_piece(piece, data, len);
+        tr_logAddDebugTor(this, fmt::format("we now have all the metainfo!"));
+        on_have_all_metainfo();
     }
 }
 
