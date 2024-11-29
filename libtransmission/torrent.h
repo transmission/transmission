a18b0aefbcecf56b2740127a762b82884d015276
Yat Ho <lagoho7@gmail.com>
2024-11-30 00:21:11 +0800

    fix: clang-tidy warning

--- libtransmission/torrent.h
+++ libtransmission/torrent.h
@@ -184,7 +184,7 @@ struct tr_torrent
     void rename_path(
         std::string_view oldpath,
         std::string_view newname,
-        tr_torrent_rename_done_func callback,
+        tr_torrent_rename_done_func&& callback,
         void* callback_user_data);
 
     // these functions should become private when possible,
