7ec57cfe88a28fba493bf6048dd8c6cf5a326abb
Cœur <coeur@gmx.fr>
2024-12-13 00:05:10 +0100

    Adjust slider limits in GTK (#7251)

--- gtk/MakeDialog.cc
+++ gtk/MakeDialog.cc
@@ -403,8 +403,8 @@ void MakeDialog::Impl::updatePiecesLabel()
 
 void MakeDialog::Impl::configurePieceSizeScale(uint32_t piece_size)
 {
-    // the below lower & upper bounds would allow piece size selection between approx 1KiB - 64MiB
-    auto adjustment = Gtk::Adjustment::create(log2(piece_size), 10, 26, 1.0, 1.0);
+    // the below lower & upper bounds would allow piece size selection between approx 16KiB - 256MiB
+    auto adjustment = Gtk::Adjustment::create(log2(piece_size), 14, 28, 1.0, 1.0);
     piece_size_scale_->set_adjustment(adjustment);
     piece_size_scale_->set_visible(true);
 }
