4e5d1c7c30aaf100b727efcae72966ea70de8ffb
Yat Ho <lagoho7@gmail.com>
2024-12-16 09:27:40 +0800

    refactor: extract `fill_output_buffer()` impl method

--- libtransmission/peer-msgs.cc
+++ libtransmission/peer-msgs.cc
@@ -560,11 +560,7 @@ private:
         {
             peer_requested_.emplace_back(req);
 
-            auto const now_sec = tr_time();
-            auto const now_msec = tr_time_msec();
-            while (fill_output_buffer(now_sec, now_msec) != 0U)
-            {
-            }
+            fill_output_buffer(tr_time(), tr_time_msec());
         }
         else if (io_->supports_fext())
         {
@@ -613,7 +609,14 @@ private:
     void maybe_send_metadata_requests(time_t now) const;
     [[nodiscard]] size_t add_next_metadata_piece();
     [[nodiscard]] size_t add_next_block(time_t now_sec, uint64_t now_msec);
-    [[nodiscard]] size_t fill_output_buffer(time_t now_sec, uint64_t now_msec);
+
+    [[nodiscard]] size_t fill_output_buffer_impl(time_t now_sec, uint64_t now_msec);
+    void fill_output_buffer(time_t now_sec, uint64_t now_msec)
+    {
+        while (fill_output_buffer_impl(now_sec, now_msec) != 0U)
+        {
+        }
+    }
 
     // ---
 
@@ -1840,14 +1843,7 @@ void tr_peerMsgsImpl::pulse()
     update_desired_request_count();
     maybe_send_block_requests();
     maybe_send_metadata_requests(now_sec);
-
-    for (;;)
-    {
-        if (fill_output_buffer(now_sec, now_msec) == 0U)
-        {
-            break;
-        }
-    }
+    fill_output_buffer(now_sec, now_msec);
 }
 
 void tr_peerMsgsImpl::maybe_send_metadata_requests(time_t now) const
@@ -1907,7 +1903,7 @@ void tr_peerMsgsImpl::check_request_timeout(time_t now)
     }
 }
 
-[[nodiscard]] size_t tr_peerMsgsImpl::fill_output_buffer(time_t now_sec, uint64_t now_msec)
+[[nodiscard]] size_t tr_peerMsgsImpl::fill_output_buffer_impl(time_t now_sec, uint64_t now_msec)
 {
     auto n_bytes_written = size_t{};
 
