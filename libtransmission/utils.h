18bb6ae90dfe5f5601fb75f2cee0b441cc3a4ed6
Yat Ho <lagoho7@gmail.com>
2025-01-10 00:03:38 +0800

    chore: comments about min compiler versions for C++20 features

--- libtransmission/utils.h
+++ libtransmission/utils.h
@@ -114,33 +114,41 @@ template<typename T>
  */
 [[nodiscard]] bool tr_wildmat(char const* text, char const* pattern);
 
+// c++23 (P1679R3), GCC 11.1, clang 12
 template<typename T>
-[[nodiscard]] constexpr bool tr_strv_contains(std::string_view sv, T key) noexcept // c++23
+[[nodiscard]] constexpr bool tr_strv_contains(std::string_view sv, T key) noexcept
 {
     return sv.find(key) != std::string_view::npos;
 }
 
-[[nodiscard]] constexpr bool tr_strv_starts_with(std::string_view sv, char key) // c++20
+// c++20 (P0457R2), GCC 9.1, clang 6
+[[nodiscard]] constexpr bool tr_strv_starts_with(std::string_view sv, char key)
 {
     return !std::empty(sv) && sv.front() == key;
 }
 
-[[nodiscard]] constexpr bool tr_strv_starts_with(std::string_view sv, std::string_view key) // c++20
+// c++20 (P0457R2), GCC 9.1, clang 6
+[[nodiscard]] constexpr bool tr_strv_starts_with(std::string_view sv, std::string_view key)
 {
     return std::size(key) <= std::size(sv) && sv.substr(0, std::size(key)) == key;
 }
 
-[[nodiscard]] constexpr bool tr_strv_starts_with(std::wstring_view sv, std::wstring_view key) // c++20
+// c++20 (P0457R2), GCC 9.1, clang 6
+[[nodiscard]] constexpr bool tr_strv_starts_with(
+    std::wstring_view sv,
+    std::wstring_view key) // c++20 (P0457R2), GCC 9.1, clang 6
 {
     return std::size(key) <= std::size(sv) && sv.substr(0, std::size(key)) == key;
 }
 
-[[nodiscard]] constexpr bool tr_strv_ends_with(std::string_view sv, std::string_view key) // c++20
+// c++20 (P0457R2), GCC 9.1, clang 6
+[[nodiscard]] constexpr bool tr_strv_ends_with(std::string_view sv, std::string_view key)
 {
     return std::size(key) <= std::size(sv) && sv.substr(std::size(sv) - std::size(key)) == key;
 }
 
-[[nodiscard]] constexpr bool tr_strv_ends_with(std::string_view sv, char key) // c++20
+// c++20 (P0457R2), GCC 9.1, clang 6
+[[nodiscard]] constexpr bool tr_strv_ends_with(std::string_view sv, char key)
 {
     return !std::empty(sv) && sv.back() == key;
 }
