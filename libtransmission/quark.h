7c87c1aa0a2f903d9312396879b384d793e14108
Yat Ho <lagoho7@gmail.com>
2025-03-06 11:11:09 +0800

    feat: store `preferred_transports` as array in `settings.json`

--- libtransmission/quark.h
+++ libtransmission/quark.h
@@ -255,7 +255,7 @@ enum // NOLINT(performance-enum-size)
     TR_KEY_port_forwarding_enabled,
     TR_KEY_port_is_open,
     TR_KEY_preallocation,
-    TR_KEY_preferred_transport,
+    TR_KEY_preferred_transports,
     TR_KEY_primary_mime_type,
     TR_KEY_priorities,
     TR_KEY_priority,
