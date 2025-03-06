f0aad0721216ebe11f3959e2b1111de9ecbce6ed
Yat Ho <lagoho7@gmail.com>
2025-03-06 11:11:09 +0800

    feat: store `preferred_transports` as array in `settings.json`

--- libtransmission/quark.cc
+++ libtransmission/quark.cc
@@ -254,7 +254,7 @@ auto constexpr MyStatic = std::array<std::string_view, TR_N_KEYS>{
     "port-forwarding-enabled"sv,
     "port-is-open"sv,
     "preallocation"sv,
-    "preferred-transport"sv,
+    "preferred_transports"sv,
     "primary-mime-type"sv,
     "priorities"sv,
     "priority"sv,
