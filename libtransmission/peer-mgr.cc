4729e20ab8b74fa55cf9882706e0b54bdff7f189
cdowen <hhylx1950@gmail.com>
2025-04-09 19:42:20 +0800

    when receving unexpected padding block

--- libtransmission/peer-mgr.cc
+++ libtransmission/peer-mgr.cc
@@ -904,6 +904,12 @@ private:
                 auto* const tor = s->tor;
                 auto const loc = tor->piece_loc(event.pieceIndex, event.offset);
                 s->cancel_all_requests_for_block(loc.block, peer);
+                auto is_padding_data = tor->piece_is_padded(loc.piece);
+                if (is_padding_data)
+                {
+                    tr_logAddDebugTor(tor, "got padding block");
+                    break;
+                }
                 peer->blocks_sent_to_client.add(tr_time(), 1);
                 peer->blame.set(loc.piece);
                 s->got_block.emit(tor, loc.block); // put this line before calling tr_torrent callback
