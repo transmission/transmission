d42afffd2129d1410fb669ec2b9b8e1766a5d110
Yat Ho <lagoho7@gmail.com>
2025-04-11 16:12:43 +0800

    build: support system small library

--- cmake/FindSmall.cmake
+++ cmake/FindSmall.cmake
@@ -1,9 +1,25 @@
-add_library(small::small INTERFACE IMPORTED)
+find_package(${CMAKE_FIND_PACKAGE_NAME} QUIET NO_MODULE)
 
-target_include_directories(small::small
-    INTERFACE
-        ${TR_THIRD_PARTY_SOURCE_DIR}/small/include)
+include(FindPackageHandleStandardArgs)
+if(${CMAKE_FIND_PACKAGE_NAME}_FOUND)
+    find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME} CONFIG_MODE)
+    return()
+endif()
 
-target_compile_definitions(small::small
-    INTERFACE
-        SMALL_DISABLE_EXCEPTIONS=1)
+find_path(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
+    NAMES small/vector.hpp)
+
+if (${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
+    add_library(small::small INTERFACE IMPORTED)
+
+    target_include_directories(small::small
+        INTERFACE
+            ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
+endif()
+
+set(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIRS ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
+
+find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME}
+    REQUIRED_VARS ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
+
+mark_as_advanced(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
