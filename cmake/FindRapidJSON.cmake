6a5e06ce75f6bbd4b0c4e45d1542ef66b64b94da
Yat Ho <lagoho7@gmail.com>
2025-04-11 11:53:25 +0800

    build: support system rapidjson library

--- cmake/FindRapidJSON.cmake
+++ cmake/FindRapidJSON.cmake
@@ -1,9 +1,36 @@
-add_library(RapidJSON INTERFACE IMPORTED)
+find_package(${CMAKE_FIND_PACKAGE_NAME} QUIET NO_MODULE)
 
-target_include_directories(RapidJSON
-    INTERFACE
-        ${TR_THIRD_PARTY_SOURCE_DIR}/rapidjson/include)
+include(FindPackageHandleStandardArgs)
+if(${CMAKE_FIND_PACKAGE_NAME}_FOUND)
+    find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME} CONFIG_MODE)
+    return()
+endif()
 
-target_compile_definitions(RapidJSON
-    INTERFACE
-        RAPIDJSON_HAS_STDSTRING=1)
+if(UNIX)
+    find_package(PkgConfig QUIET)
+    pkg_check_modules(_RAPIDJSON QUIET RapidJSON)
+endif()
+
+find_path(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
+    NAMES rapidjson/rapidjson.h
+    HINTS ${_RAPIDJSON_INCLUDEDIR})
+
+if (${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
+    add_library(RapidJSON INTERFACE IMPORTED)
+
+    target_include_directories(RapidJSON
+        INTERFACE
+            ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
+endif()
+
+if(_RAPIDJSON_VERSION)
+    set(${CMAKE_FIND_PACKAGE_NAME}_VERSION ${_RAPIDJSON_VERSION})
+endif()
+
+set(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIRS ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
+
+find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME}
+    REQUIRED_VARS ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
+    VERSION_VAR ${CMAKE_FIND_PACKAGE_NAME}_VERSION)
+
+mark_as_advanced(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
