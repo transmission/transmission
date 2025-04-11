13dd4593c7f3fea62360e87efa0b3ad747ca96ad
Yat Ho <lagoho7@gmail.com>
2025-04-11 16:54:31 +0800

    build: support system library WideInteger

--- cmake/FindWideInteger.cmake
+++ cmake/FindWideInteger.cmake
@@ -1,21 +1,25 @@
-add_library(WideInteger::WideInteger INTERFACE IMPORTED)
+find_package(${CMAKE_FIND_PACKAGE_NAME} QUIET NO_MODULE)
 
-target_include_directories(WideInteger::WideInteger
-    INTERFACE
-        ${TR_THIRD_PARTY_SOURCE_DIR}/wide-integer)
+include(FindPackageHandleStandardArgs)
+if(${CMAKE_FIND_PACKAGE_NAME}_FOUND)
+    find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME} CONFIG_MODE)
+    return()
+endif()
 
-set(_INT128_TEST_FILE ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/CheckInt128.c)
-file(WRITE ${_INT128_TEST_FILE}
-    "int main()
-    {
-        unsigned __int128 u;
-        signed __int128 i;
-        return 0;
-    }")
-try_compile(_HAVE_INT128
-    ${CMAKE_BINARY_DIR}
-    ${_INT128_TEST_FILE})
-target_compile_definitions(WideInteger::WideInteger
-    INTERFACE
-        $<$<BOOL:${_HAVE_INT128}>:WIDE_INTEGER_HAS_LIMB_TYPE_UINT64>
-)
+find_path(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
+    NAMES math/wide_integer/uintwide_t.h)
+
+if (${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
+    add_library(WideInteger::WideInteger INTERFACE IMPORTED)
+
+    target_include_directories(WideInteger::WideInteger
+        INTERFACE
+            ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
+endif()
+
+set(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIRS ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
+
+find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME}
+    REQUIRED_VARS ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
+
+mark_as_advanced(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
