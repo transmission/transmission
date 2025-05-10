118ea2c0de83ae1c2f0bc471899662a99da0edb9
Yat Ho <lagoho7@gmail.com>
2025-05-10 11:25:54 +0800

    chore: bump fmt to 11.2.0
    Acquire https://github.com/fmtlib/fmt/commit/6797f0c39a4ef13061cbc3bb850c35af7428fdc4, which fixes compile error on GCC 15.1.
    
    Acquire
    https://github.com/fmtlib/fmt/commit/9f269062a7ba1440d91c12f096a8cf3b91d6fbe9,
    which fixes buffer overflow in NetBSD 10.1 on GCC 10.5.
    
    Replace `fmt::localtime` with `std::localtime`, as `fmt::` is now
    deprecated.

--- utils/remote.cc
+++ utils/remote.cc
@@ -7,6 +7,7 @@
 #include <array>
 #include <cctype> /* isspace */
 #include <cmath> // floor
+#include <chrono>
 #include <cstdint> // int64_t
 #include <cstdio>
 #include <cstdlib>
@@ -883,7 +884,7 @@ template<size_t N>
 std::string_view format_date(std::array<char, N>& buf, time_t now)
 {
     auto begin = std::data(buf);
-    auto end = fmt::format_to_n(begin, N, "{:%a %b %d %T %Y}", fmt::localtime(now)).out;
+    auto end = fmt::format_to_n(begin, N, "{:%a %b %d %T %Y}", *std::localtime(&now)).out;
     return { begin, static_cast<size_t>(end - begin) };
 }
 
