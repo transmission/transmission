c0ef40a52ad2ade4e3f7661ed08b5efa1cc2d478
Geoffrey Bonneville <geoffrey.bonneville@gmail.com>
2025-10-16 15:29:47 +0200

    fix: flush torrent files on piece completed in sequential mode (#7489)
    * fix: flush torrent files on piece completed in sequential mode
    
    * Mark flush_torrent_files as const
    
    * Apply review changes
    
    * docs: Update cache-size-mb description in Editing-Configuration-Files.md
    
    * docs: written on disk -> written to fs
    
    * fix: flush torrent files when switching to sequential download
    
    ---------
    
    Co-authored-by: Charles Kerr <charles@charleskerr.com>

--- libtransmission/torrent.h
+++ libtransmission/torrent.h
@@ -758,6 +758,10 @@ struct tr_torrent
     {
         if (is_sequential != sequential_download_)
         {
+            if (is_sequential)
+            {
+                session->flush_torrent_files(id());
+            }
             sequential_download_ = is_sequential;
             sequential_download_changed_.emit(this, is_sequential);
             set_dirty();
