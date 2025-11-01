a50cb9443e00808924cb6f701f88325f87c2f83f
Yat Ho <lagoho7@gmail.com>
2025-11-01 21:39:17 +0800

    code review: remove TODO comment

--- libtransmission/peer-mgr.cc
+++ libtransmission/peer-mgr.cc
@@ -1859,7 +1859,6 @@ tr_peer_stat* tr_peerMgrPeerStats(tr_torrent const* tor, size_t* setme_count)
     auto const n = std::size(peers);
     auto* const ret = new tr_peer_stat[n];
 
-    // TODO: re-implement as a callback solution (similar to tr_sessionSetCompletenessCallback) in case present call to run_in_session_thread is causing hangs when the peers info window is displayed.
     auto const lock = tor->unique_lock();
     auto const now = tr_time();
     auto const now_msec = tr_time_msec();
