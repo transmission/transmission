54b34c0e21a1d68cd2807ece6335c8e0c15a2df1
reardonia <2925104+reardonia@users.noreply.github.com>
2025-11-09 10:12:40 -0500

    upload_only fixes, check for partial seeds

--- libtransmission/peer-mgr.cc
+++ libtransmission/peer-mgr.cc
@@ -241,7 +241,10 @@ void tr_peer_info::merge(tr_peer_info& that) noexcept
     }
     /* is_connected_ should already be set */
     /* keep is_seed_ as-is */
-    /* keep upload_only_ as-is */
+    /* preserve upload_only_ if TRUE already */
+    if (that.is_upload_only()) {
+        set_upload_only(true);
+    }
 
     if (that.outgoing_handshake_)
     {
@@ -2111,7 +2114,7 @@ void rechokeUploads(tr_swarm* s, uint64_t const now)
     auto salter = tr_salt_shaker{};
     for (auto* const peer : peers)
     {
-        if (peer->is_seed())
+        if (peer->peer_info->is_upload_only())
         {
             /* choke seeds and partial seeds */
             peer->set_choke(true);
@@ -2251,8 +2254,8 @@ auto constexpr MaxUploadIdleSecs = time_t{ 60 * 5 };
     auto const* tor = s->tor;
     auto const& info = peer->peer_info;
 
-    /* disconnect if we're both seeds and enough time has passed for PEX */
-    if (tor->is_done() && peer->is_seed())
+    /* disconnect if we're both seeds or partial seeds and enough time has passed for PEX */
+    if (tor->is_done() && info->is_upload_only())
     {
         return !tor->allows_pex() || info->idle_secs(now).value_or(0U) >= 30U;
     }
