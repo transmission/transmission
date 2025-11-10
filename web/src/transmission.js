c05fd2be34056a6233df4aeab0209e059c183e9a
Ivan Kara <ivanjuan222@gmail.com>
2025-11-10 18:56:17 +0700

    improvements

--- web/src/transmission.js
+++ web/src/transmission.js
@@ -489,10 +489,6 @@ export class Transmission extends EventTarget {
     return Object.values(this._torrents);
   }
 
-  _getFilteredTorrents() {
-    return this._rows.map((row) => row.getTorrent());
-  }
-
   static _getTorrentIds(torrents) {
     return torrents.map((t) => t.getId());
   }
