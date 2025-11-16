8e4bf742d3f10348819b7af5dfd14673a5bae2cc
htmltiger <1429451+htmltiger@users.noreply.github.com>
2025-11-16 21:49:55 +0000

    Update quark.cc

--- libtransmission/quark.cc
+++ libtransmission/quark.cc
@@ -279,7 +279,7 @@ auto constexpr MyStatic = std::array<std::string_view, TR_N_KEYS>{
     "remote-session-password"sv,
     "remote-session-port"sv,
     "remote-session-requres-authentication"sv,
-    "remote-session-rpc-url"sv,
+    "remote_session_rpc_url_path"sv,
     "remote-session-username"sv,
     "removed"sv,
     "rename-partial-files"sv,
