728f3bf5e938d3efb61ef49715192054c73ecb4e
Yat Ho <lagoho7@gmail.com>
2025-11-17 16:24:30 +0800

    code review: Findlibminiupnpc.cmake

--- cmake/Findminiupnpc.cmake
+++ cmake/Findminiupnpc.cmake
@@ -29,23 +29,36 @@ find_library(${CMAKE_FIND_PACKAGE_NAME}_LIBRARY
         libminiupnpc
     HINTS ${_MINIUPNPC_LIBDIR})
 
-if(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR AND ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY)
-    add_library(miniupnpc::miniupnpc INTERFACE IMPORTED)
-    target_include_directories(miniupnpc::miniupnpc
-            INTERFACE
-            ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
-    target_link_libraries(miniupnpc::miniupnpc
-            INTERFACE
-            ${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY})
+if(_MINIUPNPC_VERSION)
+    set(${CMAKE_FIND_PACKAGE_NAME}_VERSION ${_MINIUPNPC_VERSION})
+elseif(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
+    file(STRINGS "${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR}/miniupnpc/miniupnpc.h" _MINIUPNPC_VERSION_STR
+        REGEX "^#define[\t ]+MINIUPNPC_VERSION[\t ]+\"[^\"]+\"")
+    if(_MINIUPNPC_VERSION_STR MATCHES "\"([^\"]+)\"")
+        set(${CMAKE_FIND_PACKAGE_NAME}_VERSION "${CMAKE_MATCH_1}")
+    endif()
 endif()
-
-set(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIRS ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
-set(${CMAKE_FIND_PACKAGE_NAME}_LIBRARIES ${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY})
 
 find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME}
     REQUIRED_VARS
         ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY
-        ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
+        ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
+    VERSION_VAR ${CMAKE_FIND_PACKAGE_NAME}_VERSION)
+
+if(${CMAKE_FIND_PACKAGE_NAME}_FOUND)
+    set(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIRS ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
+    set(${CMAKE_FIND_PACKAGE_NAME}_LIBRARIES ${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY})
+
+    if(NOT TARGET miniupnpc::miniupnpc)
+        add_library(miniupnpc::miniupnpc INTERFACE IMPORTED)
+        target_include_directories(miniupnpc::miniupnpc
+            INTERFACE
+                ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
+        target_link_libraries(miniupnpc::miniupnpc
+            INTERFACE
+                ${${CMAKE_FIND_PACKAGE_NAME}_LIBRARY})
+    endif()
+endif()
 
 mark_as_advanced(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR ${CMAKE_FIND_PACKAGE_NAME}_LIBRARY)
 
