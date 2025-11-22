58dc3500fd658d81aaa5ae148146485fcb17eed4
Charles Kerr <charles@charleskerr.com>
2025-11-22 13:09:17 -0600

    fixup! feat: support dark, light mode when rendering SF Symbol monochrome icons
    fix: fail gracefully on macOS 11

--- qt/NativeIconMac.mm
+++ qt/NativeIconMac.mm
@@ -77,10 +77,15 @@ QPixmap loadSFSymbol(QString const symbol_name, int const pixel_size)
 {
     if (NSImage* image = [NSImage imageWithSystemSymbolName:symbol_name.toNSString() accessibilityDescription:nil])
     {
-        // use whatever color QPalette::ButtonText is using
-        QColor const qfg = qApp->palette().color(QPalette::ButtonText);
-        NSColor* nsfg = [NSColor colorWithCalibratedRed:qfg.redF() green:qfg.greenF() blue:qfg.blueF() alpha:qfg.alphaF()];
-        auto* configuration = [NSImageSymbolConfiguration configurationWithHierarchicalColor:nsfg];
+        auto* configuration = [NSImageSymbolConfiguration configurationWithPointSize:pixel_size];
+        if (@available(macOS 12.0, *)) {
+            // use whatever color QPalette::ButtonText is using
+            // @available check needed for configurationWithHierarchicalColor
+            QColor const qfg = qApp->palette().color(QPalette::ButtonText);
+            NSColor* nsfg = [NSColor colorWithCalibratedRed:qfg.redF() green:qfg.greenF() blue:qfg.blueF() alpha:qfg.alphaF()];
+            auto* colorConfig = [NSImageSymbolConfiguration configurationWithHierarchicalColor:nsfg];
+            configuration = [configuration configurationByApplyingConfiguration:colorConfig];
+        }
         image = [image imageWithSymbolConfiguration:configuration];
 
         // NSImage -> QPixmap
