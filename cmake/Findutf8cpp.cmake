3c201453dea122c331b13f6c643122dc1d4a15d5
Yat Ho <lagoho7@gmail.com>
2025-12-06 11:04:26 +0800

    Revert "build: only accept cmake config package for utf8cpp"
    This reverts commit 2158d631fd90c4a18c3290b7c08c3fc088345ed8.

--- cmake/Findutf8cpp.cmake
+++ cmake/Findutf8cpp.cmake
@@ -12,8 +12,21 @@ if(${CMAKE_FIND_PACKAGE_NAME}_FOUND)
     return()
 endif()
 
-# Always fails
+find_path(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
+    NAMES utf8.h
+    PATH_SUFFIXES utf8cpp)
+
+if (${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
+    add_library(utf8::cpp INTERFACE IMPORTED)
+
+    target_include_directories(utf8::cpp
+        INTERFACE
+            ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
+endif()
+
+set(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIRS ${${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR})
+
 find_package_handle_standard_args(${CMAKE_FIND_PACKAGE_NAME}
-    REQUIRED_VARS
-        ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR
-    VERSION_VAR ${CMAKE_FIND_PACKAGE_NAME}_VERSION)
+    REQUIRED_VARS ${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
+
+mark_as_advanced(${CMAKE_FIND_PACKAGE_NAME}_INCLUDE_DIR)
