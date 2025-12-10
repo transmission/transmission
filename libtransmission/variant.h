899065babf7ed8a80d82f77290a91c825333ce84
Charles Kerr <charles@charleskerr.com>
2025-12-09 20:56:17 -0600

    fixup! feat: add tr_variant::Map::replace_key()
    fix readability-braces-around-statements

--- libtransmission/variant.h
+++ libtransmission/variant.h
@@ -147,11 +147,15 @@ public:
         bool replace_key(tr_quark const old_key, tr_quark const new_key)
         {
             if (contains(new_key))
+            {
                 return false;
+            }
 
             auto iter = find(old_key);
             if (iter == end())
+            {
                 return false;
+            }
 
             iter->first = new_key;
             return true;
