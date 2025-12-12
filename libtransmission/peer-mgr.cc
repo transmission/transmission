1706e008fb4c11f2db1e4583ad4998b588ae9454
Yat Ho <lagoho7@gmail.com>
2025-12-12 22:06:17 +0800

    chore: add logs 6

--- libtransmission/peer-mgr.cc
+++ libtransmission/peer-mgr.cc
@@ -603,6 +603,7 @@ public:
                 s->block_history.try_emplace(loc.block).first->second.emplace_back(
                     now,
                     fmt::format("cancelled to {} [{}]", msgs->display_name(), msgs->user_agent()));
+                s->log_block_history(loc.block);
             }
             break;
 
@@ -650,6 +651,7 @@ public:
                     s->block_history.try_emplace(block).first->second.emplace_back(
                         now,
                         fmt::format("choked by {} [{}]", msgs->display_name(), msgs->user_agent()));
+                    s->log_block_history(block);
                 }
             }
             break;
@@ -886,6 +888,18 @@ private:
                         piece,
                         peer->strikes + 1));
                 add_strike(peer);
+                auto const* const msgs = dynamic_cast<tr_peerMsgs*>(peer);
+                auto const now = std::chrono::system_clock::now();
+                for (auto [block, end] = wishlist_mediator.block_span(piece); block < end; ++block)
+                {
+                    block_history.try_emplace(block).first->second.emplace_back(
+                        now,
+                        fmt::format(
+                            "got bad piece {} [{}]",
+                            peer->display_name(),
+                            msgs != nullptr ? msgs->user_agent() : "webseed"sv));
+                    log_block_history(block);
+                }
             }
         };
 
@@ -981,6 +995,7 @@ private:
                         "rejected by {} [{}]",
                         peer->display_name(),
                         msgs != nullptr ? msgs->user_agent() : "webseed"sv));
+                s->log_block_history(loc.block);
             }
             break;
 
