2c4978384c07bcef9bd71d617570aa85b2f94905
Charles Kerr <charles@charleskerr.com>
2025-12-19 16:00:02 -0600

    fix: disallow get_if<std::string>() and get_if<std::string_view>()

--- libtransmission/variant.h
+++ libtransmission/variant.h
@@ -348,12 +348,18 @@ public:
     template<typename Val>
     [[nodiscard]] constexpr auto* get_if() noexcept
     {
+        static_assert(
+            !std::is_same_v<std::decay_t<Val>, std::string> && !std::is_same_v<std::decay_t<Val>, std::string_view>,
+            "not supported -- use value_if<std::string_view>() instead.");
         return std::get_if<Val>(&val_);
     }
 
     template<typename Val>
     [[nodiscard]] constexpr auto const* get_if() const noexcept
     {
+        static_assert(
+            !std::is_same_v<std::decay_t<Val>, std::string> && !std::is_same_v<std::decay_t<Val>, std::string_view>,
+            "not supported -- use value_if<std::string_view>() instead.");
         return const_cast<tr_variant*>(this)->get_if<Val>();
     }
 
