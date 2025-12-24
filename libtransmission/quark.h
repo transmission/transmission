b682c34426f3519c6196430ac33b0ca0414139a0
Yat Ho <lagoho7@gmail.com>
2025-12-24 11:10:49 +0800

    refactor: move `TR_KEY_peer_is_choked_camel` handling

--- libtransmission/quark.h
+++ libtransmission/quark.h
@@ -376,7 +376,7 @@ enum // NOLINT(performance-enum-size)
     TR_KEY_peer_port_random_low_kebab,
     TR_KEY_peer_port_random_on_start_kebab_APICOMPAT,
     TR_KEY_peer_socket_tos_kebab,
-    TR_KEY_peer_is_choked_camel,
+    TR_KEY_peer_is_choked_camel_APICOMPAT,
     TR_KEY_peer_is_interested_camel,
     TR_KEY_peer_congestion_algorithm,
     TR_KEY_peer_id,
