235fbf2eca489dc1a823e4cb19ef6d3791667d91
Yat Ho <lagoho7@gmail.com>
2025-12-24 11:20:48 +0800

    refactor: move `TR_KEY_downloaded_bytes_camel` handling

--- libtransmission/rpcimpl.cc
+++ libtransmission/rpcimpl.cc
@@ -2284,7 +2284,6 @@ void add_strings_from_var(std::set<std::string_view>& strings, tr_variant const&
     {
         auto stats_map = tr_variant::Map{ 5U };
         stats_map.try_emplace(TR_KEY_downloaded_bytes, stats.downloadedBytes);
-        stats_map.try_emplace(TR_KEY_downloaded_bytes_camel, stats.downloadedBytes);
         stats_map.try_emplace(TR_KEY_files_added, stats.filesAdded);
         stats_map.try_emplace(TR_KEY_files_added_camel, stats.filesAdded);
         stats_map.try_emplace(TR_KEY_seconds_active, stats.secondsActive);
