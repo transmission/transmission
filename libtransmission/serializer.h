005b46ac3b6a3f0be0ce0f5508a66649165375e7
Yat Ho <lagoho7@gmail.com>
2026-01-02 21:37:07 +0800

    fix: reject nested optionals in serializer

--- libtransmission/serializer.h
+++ libtransmission/serializer.h
@@ -433,12 +433,14 @@ bool to_array(tr_variant const& src, C* const ptgt)
 template<typename T>
 tr_variant from_optional(std::optional<T> const& src)
 {
+    static_assert(!is_optional_v<T>);
     return src ? Converters::serialize(*src) : nullptr;
 }
 
 template<typename T>
 bool to_optional(tr_variant const& src, std::optional<T>* ptgt)
 {
+    static_assert(!is_optional_v<T>);
     if (src.index() == tr_variant::NullIndex)
     {
         ptgt->reset();
