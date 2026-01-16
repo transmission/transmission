f200bedd57d2e36c73b27eb0a3a5f32c59f45e07
Charles Kerr <charles@charleskerr.com>
2026-01-16 02:45:49 -0600

    fix: crash on invalid command-line value for `--cache` / `-e` arg (#8147)
    * fix: crash on invalid command-line value for --cache arg
    
    * Update utils/remote.cc
    
    Co-authored-by: Yat Ho <lagoho7@gmail.com>
    
    ---------
    
    Co-authored-by: Yat Ho <lagoho7@gmail.com>

--- utils/remote.cc
+++ utils/remote.cc
@@ -2878,7 +2878,15 @@ int process_args(char const* rpcurl, int argc, char const* const* argv, RemoteCo
                 break;
 
             case 'e':
-                args.insert_or_assign(TR_KEY_cache_size_mib, tr_num_parse<int64_t>(optarg_sv).value());
+                if (auto val = tr_num_parse<int64_t>(optarg_sv))
+                {
+                    args.insert_or_assign(TR_KEY_cache_size_mib, *val);
+                }
+                else
+                {
+                    fmt::print(stderr, "Argument to '-e'/'--cache' should be an integer");
+                    status |= EXIT_FAILURE;
+                }
                 break;
 
             case 910:
