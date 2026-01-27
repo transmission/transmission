eeaa4d74129089bec7d09bfdbe540aca2c5d29c9
Yat Ho <lagoho7@gmail.com>
2026-01-27 23:17:31 +0800

    chore(core): fix `modernize-use-constraints` (#8236)

--- libtransmission/utils.cc
+++ libtransmission/utils.cc
@@ -809,8 +809,9 @@ std::string_view tr_get_mime_type_for_filename(std::string_view filename)
 
 // --- tr_num_parse()
 
-template<typename T, std::enable_if_t<std::is_integral_v<T>, bool>>
+template<typename T>
 [[nodiscard]] std::optional<T> tr_num_parse(std::string_view str, std::string_view* remainder, int base)
+    requires std::is_integral_v<T>
 {
     auto val = T{};
     auto const* const begin_ch = std::data(str);
@@ -839,8 +840,9 @@ template std::optional<unsigned int> tr_num_parse(std::string_view str, std::str
 template std::optional<unsigned short> tr_num_parse(std::string_view str, std::string_view* remainder, int base);
 template std::optional<unsigned char> tr_num_parse(std::string_view str, std::string_view* remainder, int base);
 
-template<typename T, std::enable_if_t<std::is_floating_point_v<T>, bool>>
+template<typename T>
 [[nodiscard]] std::optional<T> tr_num_parse(std::string_view str, std::string_view* remainder)
+    requires std::is_floating_point_v<T>
 {
     auto const* const begin_ch = std::data(str);
     auto const* const end_ch = begin_ch + std::size(str);
