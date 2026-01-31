b7377b2b6cd5ab7d4d5c8fc0b4cc61bdf40a263f
reardonia <2925104+reardonia@users.noreply.github.com>
2026-01-31 08:49:08 -0500

    fixup style

--- libtransmission/peer-io.cc
+++ libtransmission/peer-io.cc
@@ -182,18 +182,20 @@ void tr_peerIo::set_socket(tr_peer_socket socket_in)
 
     if (socket_.is_tcp())
     {
-        event_read_.reset(tr::evhelpers::event_new_pri2(
-            session_->event_base(),
-            socket_.handle.tcp,
-            EV_READ,
-            &tr_peerIo::event_read_cb,
-            this));
-        event_write_.reset(tr::evhelpers::event_new_pri2(
-            session_->event_base(),
-            socket_.handle.tcp,
-            EV_WRITE,
-            &tr_peerIo::event_write_cb,
-            this));
+        event_read_.reset(
+            tr::evhelpers::event_new_pri2(
+                session_->event_base(),
+                socket_.handle.tcp,
+                EV_READ,
+                &tr_peerIo::event_read_cb,
+                this));
+        event_write_.reset(
+            tr::evhelpers::event_new_pri2(
+                session_->event_base(),
+                socket_.handle.tcp,
+                EV_WRITE,
+                &tr_peerIo::event_write_cb,
+                this));
     }
 #ifdef WITH_UTP
     else if (socket_.is_utp())
