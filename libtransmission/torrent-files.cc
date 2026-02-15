e6122f3aefeedfe50679d6c8a22b0494b57b1b9a
reardonia <2925104+reardonia@users.noreply.github.com>
2026-02-15 13:57:12 -0500

    fixup: compact into is_reserved_char()

--- libtransmission/torrent-files.cc
+++ libtransmission/torrent-files.cc
@@ -433,9 +433,10 @@ namespace
 
 // `is_unix_reserved_char` and `is_win32_reserved_char` kept as `maybe_unused`
 // for potential support of different filesystems on the same OS
-[[nodiscard, maybe_unused]] auto constexpr is_unix_reserved_char(unsigned char ch) noexcept
+[[nodiscard, maybe_unused]] auto constexpr is_unix_reserved_char([[maybe_unused]] unsigned char ch) noexcept
 {
-    return ch == '/' || ch <= 31;
+    // TODO: keep this here for future uses
+    return false;
 }
 
 // https://docs.microsoft.com/en-us/windows/desktop/FileIO/naming-a-file
@@ -448,7 +449,6 @@ namespace
     {
     case '"':
     case '*':
-    case '/':
     case ':':
     case '<':
     case '>':
@@ -457,12 +457,17 @@ namespace
     case '|':
         return true;
     default:
-        return ch <= 31;
+        return false;
     }
 }
 
 [[nodiscard]] auto constexpr is_reserved_char(unsigned char ch, bool os_specific) noexcept
 {
+    if (ch <= 31 || ch == '/')
+    {
+        return true;
+    }
+
     if (!os_specific)
     {
         return is_unix_reserved_char(ch) || is_win32_reserved_char(ch);
