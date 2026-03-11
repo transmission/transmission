47398c45aa769feefe4e1ce093eb33a860a92296
Dmitry Lobanov <gaussblurinc@gmail.com>
2026-03-11 11:37:19 +0300

    Moving TorrentCell to layers.

--- macosx/TorrentCell.mm
+++ macosx/TorrentCell.mm
@@ -18,13 +18,72 @@
 {
     [super awakeFromNib];
 
-    self.secondProgress = [[ProgressBarView2 alloc] init];
-    self.secondProgress.translatesAutoresizingMaskIntoConstraints = false;
+    self.wantsLayer = YES;
+    self.layer.masksToBounds = YES;
+
+    if (self.secondProgress == nil)
+    {
+        self.secondProgress = [[ProgressBarView2 alloc] init];
+        self.secondProgress.translatesAutoresizingMaskIntoConstraints = false;
+    }
+}
+
+- (BOOL)wantsUpdateLayer
+{
+    return YES;
+}
+
+- (void)updateLayer
+{
+    __auto_type was = CFAbsoluteTimeGetCurrent();
+    [super updateLayer];
+
+    if (self.fTorrentTableView)
+    {
+        Torrent* torrent = (Torrent*)self.objectValue;
+        if (self.secondProgress.superview == nil)
+        {
+            [self.fTorrentProgressBarView addSubview:self.secondProgress];
+
+            __auto_type view = self.secondProgress;
+            [NSLayoutConstraint activateConstraints:@[
+                [view.leadingAnchor constraintEqualToAnchor:self.fTorrentProgressBarView.leadingAnchor],
+                [view.trailingAnchor constraintEqualToAnchor:self.fTorrentProgressBarView.trailingAnchor],
+                [view.topAnchor constraintEqualToAnchor:self.fTorrentProgressBarView.topAnchor],
+                [view.bottomAnchor constraintEqualToAnchor:self.fTorrentProgressBarView.bottomAnchor],
+            ]];
+        }
+
+        // draw progress bar
+        // NSRect barRect = self.fTorrentProgressBarView.frame;
+        // ProgressBarView* progressBar = [[ProgressBarView alloc] init];
+        [self.secondProgress drawBarInRect:CGRectZero forTableView:self.fTorrentTableView withTorrent:torrent];
+        // [self.secondProgress drawBarIn]
+
+        // set priority icon
+        if (torrent.priority != TR_PRI_NORMAL)
+        {
+            NSColor* priorityColor = self.backgroundStyle == NSBackgroundStyleEmphasized ? NSColor.whiteColor : NSColor.labelColor;
+            NSImage* priorityImage = [[NSImage imageNamed:(torrent.priority == TR_PRI_HIGH ? @"PriorityHighTemplate" : @"PriorityLowTemplate")]
+                imageWithColor:priorityColor];
+
+            self.fTorrentPriorityView.image = priorityImage;
+
+            [self.fStackView setVisibilityPriority:NSStackViewVisibilityPriorityMustHold forView:self.fTorrentPriorityView];
+        }
+        else
+        {
+            [self.fStackView setVisibilityPriority:NSStackViewVisibilityPriorityNotVisible forView:self.fTorrentPriorityView];
+        }
+    }
+    
+    __auto_type spent = CFAbsoluteTimeGetCurrent() - was;
+    NSLog(@"Update happened in %.4f", spent);
 }
 
 - (void)drawRect:(NSRect)dirtyRect
 {
-    __auto_type was = CFAbsoluteTimeGetCurrent();
+    return;
     if (self.fTorrentTableView)
     {
         Torrent* torrent = (Torrent*)self.objectValue;
@@ -66,9 +125,6 @@
     }
 
     [super drawRect:dirtyRect];
-    
-    __auto_type spent = CFAbsoluteTimeGetCurrent() - was;
-    NSLog(@"Update happened in %.7f", spent);
 }
 
 // otherwise progress bar is inverted
