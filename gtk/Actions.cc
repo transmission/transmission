5321167ffc244392ef6a02b451a9a283fb2cc54e
Zan Skamljic <zan.skamljic@gmail.com>
2026-03-18 06:53:15 +0100

    fix: prevent crash when toggling alternate speed limits in GTK client

--- gtk/Actions.cc
+++ gtk/Actions.cc
@@ -70,7 +70,7 @@ void toggle_pref_cb(Gio::SimpleAction& action, gpointer prefs_key)
 
 // action-name, prefs_name
 constexpr auto PrefToggleEntries = std::array<std::pair<std::string_view, tr_quark>, 6>{ {
-    { "alt-speed-enabled"sv, TR_KEY_alt_speed_enabled },
+    { "alt_speed_enabled"sv, TR_KEY_alt_speed_enabled },
     { "compact-view"sv, TR_KEY_compact_view },
     { "show-filterbar"sv, TR_KEY_show_filterbar },
     { "show-statusbar"sv, TR_KEY_show_statusbar },
