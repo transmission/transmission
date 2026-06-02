7ddaae0dc52aedef86c01de0bd7bc0be8f0a4a2d
Charles Kerr <charles@charleskerr.com>
2026-06-01 19:52:35 -0500

    fixup! transmission-qt: fixed issue #7970, remember the last used directory

--- qt/MainWindow.cc
+++ qt/MainWindow.cc
@@ -1307,7 +1307,11 @@ void MainWindow::addTorrents(QStringList const& filenames)
         {
             show_options = b->isChecked();
         }
-        prefs_.set(Prefs::OPEN_DIALOG_FOLDER, file_dialog->directory().absolutePath());
+
+        if (!filenames.isEmpty())
+        {
+            prefs_.set(Prefs::OPEN_DIALOG_FOLDER, QFileInfo{ filenames.first() }.absolutePath());
+        }
     }
 
     for (QString const& filename : filenames)
